d622a735af0464780005b74ebd5143e19d3fe0cc
Tom Trenka <ttrenka@gmail.com>
2007-05-27 18:26:10 +0000

    Fixes #3045, #3014, #2249.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@8763 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ collections/tests/collections.js
@@ -0,0 +1,14 @@
+dojo.provide("dojox.collections.tests.collections");
+
+try{
+	dojo.require("dojox.collections.tests.common");
+	dojo.require("dojox.collections.tests.ArrayList");
+	dojo.require("dojox.collections.tests.BinaryTree");
+	dojo.require("dojox.collections.tests.Dictionary");
+	dojo.require("dojox.collections.tests.Queue");
+	dojo.require("dojox.collections.tests.Set");
+	dojo.require("dojox.collections.tests.SortedList");
+	dojo.require("dojox.collections.tests.Stack");
+}catch(e){
+	doh.debug(e);
+}
