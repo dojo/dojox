0575166221a2072a273cf7e8c987b8ec03439b8c
Alex Russell <slightlyoff@chromium.org>
2007-06-04 09:35:32 +0000

    working WhatWG provider and updated loader. Refs #3252
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@8896 560b804f-0ae3-0310-86f3-f6aa0a117693

--- storage/_common.js
+++ storage/_common.js
@@ -1,5 +1,6 @@
 dojo.provide("dojox.storage._common");
 dojo.require("dojox.storage.Provider");
 dojo.require("dojox.storage.manager");
-// dojo.requireIf(dojo.isGears, "dojox.storage.GearsStorageProvider");
-dojo.require("dojox.storage.GearsStorageProvider");
+dojo.requireIf(dojo.isGears, "dojox.storage.GearsStorageProvider");
+dojo.requireIf(!!globalStorage, "dojox.storage.WhatWGStorageProvider");
+dojo.requireIf(!globalStorage, "dojox.storage.FlashStorageProvider");
