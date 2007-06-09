31a488889a568221c59ac30b4debd3747b4d74eb
Brad Neuberg <BradNeuberg@dojotoolkit.org>
2007-06-09 00:01:50 +0000

    Making dojox.sql public (was dojox._sql for some reason)
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@8981 560b804f-0ae3-0310-86f3-f6aa0a117693

--- sql.js
+++ sql.js
@@ -1,2 +1,2 @@
-dojo.require("dojox._sql.common");
+dojo.require("dojox.sql.common");
 dojo.provide("dojox.sql");
--- storage/GearsStorageProvider.js
+++ storage/GearsStorageProvider.js
@@ -80,7 +80,6 @@ if(dojo.isGears){
 					}else{
 						value = dojo.toJson(value);
 					}
-					// console.debug(value);
 					
 					// try to store the value	
 					try{
