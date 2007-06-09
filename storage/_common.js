54b28ff5650a42ae48090bad2800dcaacdb48f8d
Brad Neuberg <BradNeuberg@dojotoolkit.org>
2007-06-09 00:49:12 +0000

    Fixme added
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@8986 560b804f-0ae3-0310-86f3-f6aa0a117693

--- storage/_common.js
+++ storage/_common.js
@@ -1,6 +1,9 @@
 dojo.provide("dojox.storage._common");
 dojo.require("dojox.storage.Provider");
 dojo.require("dojox.storage.manager");
+// FIXME: These requireIf's need to take into account the
+// djConfig.forceStorageProvider flag to force a particular
+// storage provider even if it may not be available
 dojo.requireIf(dojo.isGears, "dojox.storage.GearsStorageProvider");
 dojo.requireIf(!dojo.isGears, "dojox.storage.FlashStorageProvider");
 dojo.requireIf(!dojo.isGears, "dojox.storage.WhatWGStorageProvider");
