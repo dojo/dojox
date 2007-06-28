8548fff4904a74a34a93fcf0f04739647208f602
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2007-06-28 21:12:57 +0000

    Checking in a prototype HtmlTableStore written by Ben Schell for later use with charting code.  fixes #3560
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@9363 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/tests/module.js
+++ data/tests/module.js
@@ -2,6 +2,7 @@ dojo.provide("dojox.tests.module");
 
 try{
 	dojo.require("dojox.data.tests.stores.CsvStore");
+	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.stores.HtmlTableStore");
 	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.stores.OpmlStore");
 	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.stores.XmlStore");
 	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.dom");
