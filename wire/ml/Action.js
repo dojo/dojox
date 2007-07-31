60e54d0f9af42dfc2d052202d7a03ba1848f4796
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2007-07-31 14:21:05 +0000

    Enhancement to ActionFilter.  fixes #3913
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@9880 560b804f-0ae3-0310-86f3-f6aa0a117693

--- wire/ml/Action.js
+++ wire/ml/Action.js
@@ -123,11 +123,11 @@ dojo.declare("dojox.wire.ml.Action",
 		}
 	},
 
-	_destroy: function(finalize /*boolean*/){
+	uninitialize: function(){
 		//	summary:
-		//		Over-ride of base widget _destroy function to do some connection cleanup.
+		//		Over-ride of base widget unitialize function to do some connection cleanup.
 		this._disconnect();
-		dojox.wire.ml.Action.superclass._destroy.apply(this,arguments);
+		return true;
 	}
 });
 
@@ -143,33 +143,74 @@ dojo.declare("dojox.wire.ml.ActionFilter",
 	//		Subclasses may implement their own filter() method.
 	//	required:
 	//		A property required
+	//	requiredValue:
+	//		Optional.  A specific value the property is required to have.  If this isn't provided
+	//		than any non-false/non-null value of the required propery will cause this filter
+	//		to pass.
+	//	type:
+	//		Optional.  A specific type to compare the values as (if requiredValue is set)
+	//		Valid values for type are boolean, int, string.  Default is string.
 	//	message:
-	//		An error message
+	//		An error message to emit if the filter doesn't execute due to property mismatch.
 	//	error:
-	//		A property to store an error
+	//		A property to store an error due to property mismatch.
 	required: "",
+	requiredValue: "",
+	type: "",
 	message: "",
 	error: "",
 
+
 	filter: function(){
 		//	summary:
-		//		Check if a required property is specified
+		//		Check if a required property is specified.  Also, if provided, check to see 
+		//		if the required property contains a specific value.
 		//	description:
-		//		If a value is undefined for a property, specifieid with
+		//		If a value is undefined for a property, specified with
 		//		'required', this method returns false.
+		//		If the value for a property is defined, but there isn't a requiredValue for it
+		//		then any non-false value will cause the method to return true.
+		//		if requiredValue is set, then filter compares that value with the value from
+		//		the required property and returns true if and only if they match.
+		//		The type option just allows for a way to convert the required property values
+		//		into a proper form for comparison (boolean, number, etc).
 		//		If 'message' is specified, it is set to a proeprty specified
 		//		with 'error' or shown with alert().
 		//		If 'required' starts with "arguments", a property of
 		//		the method arguments are checked.
 		//	returns:
-		//		True if a required property is specified, otherwise false
-		if(!this.required){
+		//		True if a required property is specified (and if requiredValue is specified, 
+		//		that they match), otherwise false
+		if(this.required === ""){
 			return true; //Boolean
+		}else{
+			var value = dojox.wire.ml._getValue(this.required, arguments);
+			if(this.requiredValue === ""){
+				//Just see if there's a value, nothing to compare it to.
+				if(value){
+					return true; //Boolean
+				}
+			}else{
+				//See if we need to type convert.
+				var reqValue = this.requiredValue;
+				if(this.type !== ""){
+					var lType = this.type.toLowerCase();
+					if(lType === "boolean"){
+						if(reqValue.toLowerCase() === "false"){
+							reqValue = false;
+						}else{
+							reqValue = true;
+						}
+					}else if(lType === "number"){
+						reqValue = parseInt(reqValue, 10);
+					}
+				}
+				if(value === reqValue){
+					return true; //boolean
+				}
+			}
 		}
-		var value = dojox.wire.ml._getValue(this.required);
-		if(value){
-			return true; //Boolean
-		}
+		
 		if(this.message){
 			if(this.error){
 				dojox.wire.ml._setValue(this.error, this.message);
