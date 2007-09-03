7ff17842db7037730d4f24cfe708fac37d884f1a
Peter E Higgins <dante@dojotoolkit.org>
2007-09-03 11:57:39 +0000

    fixes #4351 - adding dojox.validate.isbn validation checking courtesy bookstack [cla/soc champ]. adds DOH unit tests, that may or may not be validating properly (refs #4350). tests were ported from 0.4 unit tests, and all 'passed' as expected, so didn't double check forced failure. I must have set up the tests wrong, somehow.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@10414 560b804f-0ae3-0310-86f3-f6aa0a117693

--- validate/tests/validate.js
+++ validate/tests/validate.js
@@ -5,11 +5,32 @@ dojo.require("dojox.validate.check");
 dojo.require("dojox.validate.us");
 dojo.require("dojox.validate.ca"); 
 dojo.require("dojox.validate.web");
+dojo.require("dojox.validate.isbn");
 
 tests.register("dojox.validate.tests.validate",
 	[{
 		name: "isText",
 		runTest: function(tests){
+			tests.t(dojox.validate.isValidISBN('0596007590')); //test string input
+			tests.t(dojox.validate.isValidISBN('0-596-00759-0')); //test string input with dashes
+			tests.f(dojox.validate.isValidISBN(0596007590)); //test numerical input as well
+			tests.t(dojox.validate.isValidISBN("960-425-059-0")); 
+			tests.t(dojox.validate.isValidISBN(9604250590)); //test numerical input as well
+			tests.t(dojox.validate.isValidISBN('0-9752298-0-X')); // test string with X
+			tests.t(dojox.validate.isValidISBN('0-9752298-0-x')); 
+			tests.t(dojox.validate.isValidISBN('097522980x')); 
+			tests.t(dojox.validate.isValidISBN('097522980X')); 
+			tests.f(dojox.validate.isValidISBN(0596007598)); //testing failures
+			tests.f(dojox.validate.isValidISBN('059-600759-X')); //testing failures
+			tests.f(dojox.validate.isValidISBN('059600')); // too short
+
+			tests.t(dojox.validate.isValidISBN('9780596007591'));
+			tests.t(dojox.validate.isValidISBN('978-0-596 00759-1'));
+			tests.t(dojox.validate.isValidISBN(9780596007591));
+			tests.f(dojox.validate.isValidISBN('978059600759X'));
+			tests.f(dojox.validate.isValidISBN('978-3250-596 00759-1 '));
+			tests.f(dojox.validate.isValidISBN('3250-596 00759 '));
+
 			tests.t(dojox.validate.isText('            x'));
 			tests.t(dojox.validate.isText('x             '));
 			tests.t(dojox.validate.isText('        x     '));
