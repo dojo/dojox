101ef077ae93b91c1462882b2fbf0b6395dde2bb
Bryan Forbes <bryan@reigndropsfall.net>
2007-09-06 21:50:53 +0000

    * Added a dependency on dojox.crypto.MD5.   * Changed boundary to be a random string that gets turned into an MD5 sum.   * Added quotes around undefined in the fileName and contentType checks.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@10440 560b804f-0ae3-0310-86f3-f6aa0a117693

--- io/xhrMultiPart.js
+++ io/xhrMultiPart.js
@@ -1,21 +1,25 @@
 dojo.provide("dojox.io.xhrMultiPart");
+
 dojo.require("dojo._base.xhr");
+dojo.require("dojox.crypto.MD5");
 
 dojox.io.xhrMultiPart = function(args){
-	// unique guid as a boundary value for multipart posts
-	var boundary = "45309FFF-BD65-4d50-99C9-36986896A96F";
-
 	if(!args["file"]){
 		throw new Error("file must be provided to dojox.io.xhrMultiPart's arguments");
 	}
+
+	// unique guid as a boundary value for multipart posts
+	var boundary = dojox.crypto.MD5.compute(
+		new Date()+Math.round(Math.random*10000)
+	);
 
 	var d = (dojo.isArray(args.file) ? args.file : [args.file]);
 	var tmp = [];
 
 	for(var i=0; i < d.length; i++){
 		var o = d[i];
-		var fileName = (typeof o["fileName"] != undefined ? o.fileName : o.name);
-		var contentType = (typeof o["contentType"] != undefined ? o.contentType : "application/octet-stream");
+		var fileName = (typeof o["fileName"] != "undefined" ? o.fileName : o.name);
+		var contentType = (typeof o["contentType"] != "undefined" ? o.contentType : "application/octet-stream");
 
 		tmp.push("--" + boundary,
 				 "Content-Disposition: form-data; name=\"" + o.name + "\"; filename=\"" + fileName + "\"",
