e860ab7434259469f42cce121bbb8e1fa7cb7835
Eugene Lazutkin <eugene@lazutkin.com>
2007-10-15 19:24:47 +0000

    Initial check in of dojox.lang.functional. Other JS-related enhancements are pending.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@10831 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ lang/tests/main.js
@@ -0,0 +1,11 @@
+﻿dojo.provide("dojox.lang.tests.test_fun");
+
+try{
+	dojo.require("dojox.lang.tests.listcomp");
+	dojo.require("dojox.lang.tests.lambda");
+	dojo.require("dojox.lang.tests.reduce");
+	dojo.require("dojox.lang.tests.curry");
+	dojo.require("dojox.lang.tests.misc");
+}catch(e){
+	doh.debug(e);
+}
