e54c56a4879d86aabda3e8e6f3c90bd978f86945
Tom Trenka <ttrenka@gmail.com>
2007-10-20 15:06:29 +0000

    Fixed theme tests.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@11017 560b804f-0ae3-0310-86f3-f6aa0a117693

--- charting/tests/Theme.js
+++ charting/tests/Theme.js
@@ -1,14 +1,15 @@
 dojo.provide("dojox.charting.tests.Theme");
 dojo.require("dojox.charting.Theme");
+dojo.require("dojox.charting.themes.PlotKit.blue");
 
 (function(){
 	var dxc=dojox.charting;
-
+	var blue=dxc.themes.PlotKit.blue;
 	tests.register("dojox.charting.tests.Theme", [
 		function testDefineColor(t){
-			var h=45;
-			var args={ hue:h, num:16 };
-			var a=dxc.Theme.defineColors(args);
+			var args={ num:16, cache:false };
+			blue.defineColors(args);
+			var a=blue.colors;
 			var s="<table border=1>";
 			for(var i=0; i<a.length; i++){
 				if(i%8==0){
@@ -20,8 +21,9 @@ dojo.require("dojox.charting.Theme");
 			s+="</tr></table>";
 			doh.debug(s);
 
-			var args={ hue:h, num:32 };
-			var a=dxc.Theme.defineColors(args);
+			var args={ num:32, cache: false };
+			blue.defineColors(args);
+			var a=blue.colors;
 			var s="<table border=1 style=margin-top:12px;>";
 			for(var i=0; i<a.length; i++){
 				if(i%8==0){
@@ -33,8 +35,9 @@ dojo.require("dojox.charting.Theme");
 			s+="</tr></table>";
 			doh.debug(s);
 
-			var args={ hue:h, saturation:20, num:32 };
-			var a=dxc.Theme.defineColors(args);
+			var args={ saturation:20, num:32, cache:false };
+			blue.defineColors(args);
+			var a=blue.colors;
 			var s="<table border=1 style=margin-top:12px;>";
 			for(var i=0; i<a.length; i++){
 				if(i%8==0){
@@ -46,8 +49,9 @@ dojo.require("dojox.charting.Theme");
 			s+="</tr></table>";
 			doh.debug(s);
 
-			var args={ hue:h, low:10, high:90, num:32 };
-			var a=dxc.Theme.defineColors(args);
+			var args={ low:10, high:90, num:32, cache: false };
+			blue.defineColors(args);
+			var a=blue.colors;
 			var s="<table border=1 style=margin-top:12px;>";
 			for(var i=0; i<a.length; i++){
 				if(i%8==0){
--- charting/tests/charting.js
+++ charting/tests/charting.js
@@ -3,8 +3,6 @@ dojo.provide("dojox.charting.tests.charting");
 try{
 	dojo.require("dojox.charting.tests._color");
 	dojo.require("dojox.charting.tests.Theme");
-	dojo.require("dojox.charting.themes.PlotKit.blue");
-	console.log(dojox.charting.themes.PlotKit.blue);
 }catch(e){
 	doh.debug(e);
 }
