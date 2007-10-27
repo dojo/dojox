0b7dd2ae665afa42ac0c2ac6dbfe9e9d4d5e1ed9
Peter E Higgins <dante@dojotoolkit.org>
2007-10-27 03:40:03 +0000

    refs #4901 - cleanups.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@11139 560b804f-0ae3-0310-86f3-f6aa0a117693

--- widget/FisheyeList.js
+++ widget/FisheyeList.js
@@ -5,26 +5,26 @@ dojo.require("dijit._Templated");
 dojo.require("dijit._Container");
 
 dojo.declare("dojox.widget.FisheyeList", [dijit._Widget, dijit._Templated, dijit._Container], {
-	// summary
+	// summary:
 	//	Menu similar to the fish eye menu on the Mac OS
-	// usage
-	//	<div dojoType="FisheyeList"
-	//	itemWidth="40" itemHeight="40"
-	//	itemMaxWidth="150" itemMaxHeight="150"
-	//	orientation="horizontal"
-	//	effectUnits="2"
-	//	itemPadding="10"
-	//	attachEdge="center"
-	//	labelEdge="bottom">
-	//
-	//		<div dojoType="FisheyeListItem"
-	//			id="item1"
-	//			onclick="alert('click on' + this.label + '(from widget id ' + this.widgetId + ')!');"
-	//			label="Item 1"
-	//			iconSrc="images/fisheye_1.png">
-	//		</div>
-	//		...
-	//	</div>
+	// example:
+	// |	<div dojoType="FisheyeList"
+	// |		itemWidth="40" itemHeight="40"
+	// |		itemMaxWidth="150" itemMaxHeight="150"
+	// |		orientation="horizontal"
+	// |		effectUnits="2"
+	// |		itemPadding="10"
+	// |		attachEdge="center"
+	// |		labelEdge="bottom">
+	// |
+	// |		<div dojoType="FisheyeListItem"
+	// |			id="item1"
+	// |			onclick="alert('click on' + this.label + '(from widget id ' + this.widgetId + ')!');"
+	// |			label="Item 1"
+	// |			iconSrc="images/fisheye_1.png">
+	// |		</div>
+	// |		...
+	// |	</div>
 	//
 	constructor: function(){
 		//
@@ -600,7 +600,7 @@ dojo.declare("dojox.widget.FisheyeList", [dijit._Widget, dijit._Templated, dijit
 		dojo.disconnect(this._addChildHandle);
 		if (this.isFixed) { dojo.disconnect(this._onScrollHandle); }
 		dojo.disconnect(this._onResizeHandle); 
-		dojox.widget.FisheyeList.superclass.destroyRecursive.call(this);
+		this.inherited("destroyRecursive",arguments);
 	}
 });
 
