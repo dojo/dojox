f88b1453409d5a10dd4ba117089756337f30ad73
Adam Peller <github@peller.org>
2007-11-06 17:20:22 +0000

    Include MIT license for Oliver Steele's work.  Refs #5026
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@11410 560b804f-0ae3-0310-86f3-f6aa0a117693

--- lang/functional.js
+++ lang/functional.js
@@ -7,10 +7,10 @@ dojo.provide("dojox.lang.functional");
 //	- "reduce" family of functions
 //	- currying and partial functions
 //	- argument pre-processing: mixer and flip
-//	- miscelaneous useful functions
+//	- miscellaneous useful functions
 
 // Acknoledgements:
-//	- parts of this module (most notably lamda, constFun, invoke, pluck, and partial) 
+//	- parts of this module (most notably lambda, constFun, invoke, pluck, and partial) 
 //		are based on work by Oliver Steele (http://osteele.com/sources/javascript/functional/functional.js)
 //		which was published under MIT License
 //	- Simple "maybe" monad was donated by Alex Russell.
@@ -22,7 +22,7 @@ dojo.provide("dojox.lang.functional");
 //	- missing high-level functions are provided with the compatible API: 
 //		foldl, foldl1, scanl, scanl1, foldr, foldr1, scanr, scanr1,
 //		reduce, reduceRight
-//	- lambda() and listcomp() produce functions, which after the comilation step are 
+//	- lambda() and listcomp() produce functions, which after the compilation step are 
 //		as fast as regular JS functions (at least theoretically).
 
 (function(){
@@ -44,9 +44,9 @@ dojo.provide("dojox.lang.functional");
 				args = sects.pop().split(/\s*,\s*|\s+/m);
 				if(sects.length){ sects.push("(function(" + args + "){return (" + s + ")})"); }
 			}
-		} else if(s.match(/\b_\b/)) {
+		}else if(s.match(/\b_\b/)){
 			args = ["_"];
-		} else {
+		}else{
 			var l = s.match(/^\s*(?:[+*\/%&|\^\.=<>]|!=)/m),
 				r = s.match(/[+\-*\/%&|\^\.=<>!]\s*$/m);
 			if(l || r){
@@ -58,7 +58,7 @@ dojo.provide("dojox.lang.functional");
 					args.push("$2");
 					s = s + "$2";
 				}
-			} else {
+			}else{
 				var vars = s.
 					replace(/(?:\b[A-Z]|\.[a-zA-Z_$])[a-zA-Z_$\d]*|[a-zA-Z_$][a-zA-Z_$\d]*:|this|true|false|null|undefined|typeof|instanceof|in|delete|new|void|arguments|decodeURI|decodeURIComponent|encodeURI|encodeURIComponent|escape|eval|isFinite|isNaN|parseFloat|parseInt|unescape|dojo|dijit|dojox|'(?:[^'\\]|\\.)*'|"(?:[^"\\]|\\.)*"/g, "").
 					match(/([a-z_$][a-z_$\d]*)/gi) || [];
