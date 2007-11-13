a7d9a763f449fa36d8035932c3679c95b16570a6
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2007-11-13 21:53:28 +0000

    Carrying fix forward.  refs #5004
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@11508 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/XmlStore.js
+++ data/XmlStore.js
@@ -504,10 +504,9 @@ dojo.declare("dojox.data.XmlStore", null, {
 			}
 			items.push(item);
 		}
-		for(var i in items){
-			var element = items[i].element;
-			element.parentNode.removeChild(element); // make it root
-		}
+		dojo.forEach(items,function(item){ 
+			item.element.parentNode.removeChild(item.element); // make it root
+		},this); 
 		return items;
 	},
 
@@ -803,7 +802,7 @@ dojo.declare("dojox.data.XmlStore", null, {
 		if(!keywordArgs){
 			keywordArgs = {};
 		}
-		for(var i in this._modifiedItems){
+		for(var i = 0; i < this._modifiedItems.length; i++){
 			this._saveItem(this._modifiedItems[i], keywordArgs, "PUT");
 		}
 		for(var i = 0; i < this._newItems.length; i++){
@@ -815,7 +814,7 @@ dojo.declare("dojox.data.XmlStore", null, {
 			}
 			this._saveItem(this._newItems[i], keywordArgs, "POST");
 		}
-		for(var i in this._deletedItems){
+		for(var i = 0; i < this._deletedItems.length; i++){
 			this._saveItem(this._deletedItems[i], keywordArgs, "DELETE");
 		}
 	},
@@ -1016,7 +1015,7 @@ dojo.declare("dojox.data.XmlStore", null, {
 	},
 
 	_getItemIndex: function(items, element){
-		for(var i in items){
+		for(var i = 0; i < items.length; i++){
 			if(items[i].element === element){
 				return i; //int
 			}
@@ -1038,13 +1037,13 @@ dojo.declare("dojox.data.XmlStore", null, {
 	},
 
 	_restoreItems: function(items){
-		for(var i in items){
-			var item = items[i];
+
+		dojo.forEach(items,function(item){ 
 			if(item._backup){
 				item.element = item._backup;
 				item._backup = null;
 			}
-		}
+		},this); 
 	},
 
 	_forgetItem: function(item){
