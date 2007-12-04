d0536438a3563f6a69534c5cb6a177f114bf522c
Peter E Higgins <dante@dojotoolkit.org>
2007-12-04 06:30:52 +0000

    a quick way to supplement the quirky nature of inheritance and document package-level objects (eg: all of dojox, and a things like dijit.form and .layout) ... dojox project owners should grep FIXME _dojox.js to see if your project is missing its info, and please update this file.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@11687 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ resources/_docs/_dojox.js
@@ -0,0 +1,168 @@
+// this file is in place as a quick way to give summaries to all available dojox projects.
+// it is a fallback file, and should never be included in your source or build, ever. really.
+
+dojox.charting = { 
+	// summary: Vector Data Driven graphs and charts 
+};
+
+dojox.collections = { // FIXME:
+
+};
+
+dojox.color = { 
+	// summary: Advanced color methods, including HSV, HSL, CMYK, etc.
+};
+
+dojox.cometd = { 
+	// summary: A comted client written in dojo
+};
+
+dojox.crypto = { 
+	// summary: A cryptography project 
+};	
+
+dojox.data = { 
+	// summary: Additional dojo.data data stores and demos
+};
+
+dojox.date = { 	
+	// summary: Additional date manipulation functions
+};
+
+dojox.dtl = { 
+	// summary: Django Templating Language implementation	
+};
+
+dojox.encoding = { 
+	// summary: Experimental encoding project	
+};
+
+dojox.flash = { // FIXME:
+
+};
+
+dojox.fx = {
+	// summary: Extension animations to the core dojo FX project
+	//
+	// description:
+	//	A package of animations, and FX-related code, extending Dojo Core fx.
+	//	Including this package includes all the Base and Core fx packages.
+	//
+};
+
+dojox.gfx = { 
+	// summary: Cross-browser vector drawing API
+	// description: 
+	//
+	//	dojox.gfx is an advanced API providing normalized vector drawing
+	//	in a variety of browsers. It has individual renderers for SVG, VML,
+	//	Canvas, and Silverlight.
+	//
+};
+
+dojox.gfx3d = { 
+	// summary: A 3d API for dojox.gfx
+};
+
+dojox.grid = { 
+	// summary: An advanced Grid project
+};	
+
+dojox.highlight = { 
+	// summary: A client-side syntax highlighting engine.
+	// description:
+	//	This project parses pre > code nodes, and applies syntax highlighting for
+	//	a wide variety of languages. Simply dojo.require() in all the 
+	//	dojox.highlight.languages you wish to include in your highlighingting,
+	//	and customize colors in the highlight.css.
+	//
+	//	It is provided as a dojo package, contributed under CLA 
+	//	by Ivan Sagalaev and is available origionally from:
+	// 	http://softwaremaniacs.org/soft/highlight/en/	
+	//	
+};
+
+dojox.image = { 
+	// summary: A collection of image related widgets
+};
+
+dojox.io = { 
+	// summary: Extensions to the Core dojo.io transports
+};
+
+dojox.jsonPath = { 
+	// summary: A query system for JavaScript objects
+};
+
+dojox.layout = { 
+	// summary: A Collection of layout related Widgets
+};
+
+dojox.lang = { 
+	// summary: Language specific extensions
+};
+
+dojox.math = { // FIXME:
+
+};
+
+dojox.off = { // summary: the Dojo Offline project
+};
+
+dojox.presentation = {
+	// summary: A simple, experimental PowerPoint-like presentation project
+};
+
+dojox.regexp = { 
+	// summary: Additional pre-made regular expressions for use
+};
+
+dojox.sql = { // FIXME:
+
+};
+
+dojox.storage = { // FIXME:
+
+};
+
+dojox.string = { // FIXME:
+
+};
+
+dojox.timing = { // FIXME:
+
+};
+
+dojox.uuid = { // FIXME:
+
+};
+
+dojox.validate = { 
+	// summary: Additional input validation methods
+	ca : {
+		// summary: Methods specific to the Canadian provinces
+	},
+	creditCard : {
+		// summary: Validate various credit card types  
+	}
+};
+
+dojox.widget = { 
+	// summary: A Collection of un-categorized widgets, or code not requiring it's own package.
+	// 
+	// description: 
+	// 	These are standalone widgets with varying levels of stability. Some are useful, 
+	//	some were created for demonstration purposes, and learning tools. The each maintain
+	//	their own .css file (typically dojox/widget/WidgetName/WidgetName.css)
+	//
+	//	
+};
+
+dojox.wire = { // FIXME:
+
+};
+
+dojox.xml = { // FIXME:
+
+};
+
