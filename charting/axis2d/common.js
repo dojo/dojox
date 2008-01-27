d6ff784542087d8d2e1f8b9b95862ca683cdd7df
Eugene Lazutkin <eugene@lazutkin.com>
2008-01-27 08:14:47 +0000

    gfx: Switching from absolutely positioned labels to relatively positioned ones. It should eliminate all weird problems with labels positioning. Fixes #5168.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@12177 560b804f-0ae3-0310-86f3-f6aa0a117693

--- charting/axis2d/common.js
+++ charting/axis2d/common.js
@@ -5,6 +5,21 @@ dojo.require("dojox.gfx");
 (function(){
 	var g = dojox.gfx;
 	
+	function clearNode(s){
+		s.marginLeft   = "0px";
+		s.marginTop    = "0px";
+		s.marginRight  = "0px";
+		s.marginBottom = "0px";
+		s.paddingLeft   = "0px";
+		s.paddingTop    = "0px";
+		s.paddingRight  = "0px";
+		s.paddingBottom = "0px";
+		s.borderLeftWidth   = "0px";
+		s.borderTopWidth    = "0px";
+		s.borderRightWidth  = "0px";
+		s.borderBottomWidth = "0px";
+	}
+	
 	dojo.mixin(dojox.charting.axis2d.common, {
 		createText: {
 			gfx: function(chart, creator, x, y, align, text, font, fontColor){
@@ -13,40 +28,42 @@ dojo.require("dojox.gfx");
 				}).setFont(font).setFill(fontColor);
 			},
 			html: function(chart, creator, x, y, align, text, font, fontColor){
-				var p = dojo.doc.createElement("div"), s = p.style;
-				s.marginLeft   = "0px";
-				s.marginTop    = "0px";
-				s.marginRight  = "0px";
-				s.marginBottom = "0px";
-				s.paddingLeft   = "0px";
-				s.paddingTop    = "0px";
-				s.paddingRight  = "0px";
-				s.paddingBottom = "0px";
-				s.borderLeftWidth   = "0px";
-				s.borderTopWidth    = "0px";
-				s.borderRightWidth  = "0px";
-				s.borderBottomWidth = "0px";
-				s.position = "absolute";
+				var p = dojo.doc.createElement("div"), s = p.style,
+					wrap = dojo.doc.createElement("div"), w = wrap.style;
+				// setup the text node
+				clearNode(s);
 				s.font = font;
 				p.innerHTML = text;
 				s.color = fontColor;
-				chart.node.appendChild(p);
-				var parent = chart.getCoords(), 
-					box = dojo.marginBox(p),
-					size = g.normalizedLength(g.splitFontString(font).size),
-					top = parent.y + Math.floor(y - size);
+				// measure the size
+				s.position = "absolute";
+				s.left = "-10000px";
+				dojo.body().appendChild(p);
+				var size = g.normalizedLength(g.splitFontString(font).size),
+					box = dojo.marginBox(p);
+				// reset the text node
+				dojo.body().removeChild(p);
+				s.position = "relative";
+				// setup the wrapper node
+				clearNode(w);
+				w.width = "0px";
+				w.height = "0px";
+				wrap.appendChild(p)
+				// insert nodes and setup positions
+				chart.node.insertBefore(wrap, chart.node.firstChild);
 				switch(align){
 					case "middle":
-						dojo.marginBox(p, {l: parent.x + Math.floor(x - box.w / 2), t: top});
+						s.left = Math.floor(x - box.w / 2);
 						break;
 					case "end":
-						dojo.marginBox(p, {l: parent.x + Math.floor(x - box.w), t: top});
+						s.left = Math.floor(x - box.w);
 						break;
 					//case "start":
 					default:
-						dojo.marginBox(p, {l: parent.x + Math.floor(x), t: top});
+						s.left = Math.floor(x);
 						break;
 				}
+				s.top = Math.floor(y - size);
 				return p;
 			}
 		}
