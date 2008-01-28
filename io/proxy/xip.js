c3bdd6446d099815a359747641b73c446b4aa660
James Burke <jrburke@gmail.com>
2008-01-28 06:21:23 +0000

    Refs #5699. Safari 3 is now working, and the general approach is much simpler than before. No need for a third iframe in IE7 now. Still need to fix Opera, but that will be hopefully fairly straight-forward. \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@12181 560b804f-0ae3-0310-86f3-f6aa0a117693

--- io/proxy/xip.js
+++ io/proxy/xip.js
@@ -11,52 +11,90 @@ dojox.io.proxy.xip = {
 	//http://dojotoolkit.org/book/dojo-book-0-4/part-5-connecting-pieces/i-o/cross-domain-xmlhttprequest-using-iframe-proxy
 	//Usage of XHR IFrame Proxying does not work from local disk in Safari.
 
-	xipClientUrl: dojo.config["xipClientUrl"] || dojo.moduleUrl("dojox.io.proxy", "xip_client.html"),
+	/*
+	This code is really focused on just sending one complete request to the server, and
+	receiving one complete response per iframe. The code does not expect to reuse iframes for multiple XHR request/response
+	sequences. This might be reworked later if performance indicates a need for it.
+	
+	xip fragment identifier/hash values have the form:
+	#id:cmd:realEncodedMessage
 
+	id: some ID that should be unique among message fragments. No inherent meaning,
+	        just something to make sure the hash value is unique so the message
+	        receiver knows a new message is available.
+	        
+	cmd: command to the receiver. Valid values are:
+	         - init: message used to init the frame. Sent as the first URL when loading
+	                 the page. Contains some config parameters.
+	         - loaded: the remote frame is loaded. Only sent from xip_client.html to this module.
+	         - ok: the message that this page sent was received OK. The next message may
+	               now be sent.
+	         - start: the start message of a block of messages (a complete message may
+	                  need to be segmented into many messages to get around the limitiations
+	                  of the size of an URL that a browser accepts.
+	         - part: indicates this is a part of a message.
+	         - end: the end message of a block of messages. The message can now be acted upon.
+	                If the message is small enough that it doesn't need to be segmented, then
+	                just one hash value message can be sent with "end" as the command.
+	
+	To reassemble a segmented message, the realEncodedMessage parts just have to be concatenated
+	together.
+	*/
+
+	xipClientUrl: ((dojo.config || djConfig)["xipClientUrl"]) || dojo.moduleUrl("dojox.io.proxy", "xip_client.html"),
+
+
+	//MSIE has the lowest limit for URLs with fragment identifiers,
+	//at around 4K. Choosing a slightly smaller number for good measure.
+	urlLimit: 4000,
+
+	_callbackName: (dojox._scopeName || "dojox") + ".io.proxy.xip.fragmentReceived",
 	_state: {},
 	_stateIdCounter: 0,
+	_isWebKit: navigator.userAgent.indexOf("WebKit") != -1,
 
-	needFrameRecursion: function(){
-		return (dojo.isIE >= 7);
-	},
-
-	send: function(facade){		
+	send: function(facade){
 		var stateId = "XhrIframeProxy" + (this._stateIdCounter++);
 		facade._stateId = stateId;
+		var url = this.xipClientUrl;
 
-
-		var frameUrl = this.xipClientUrl + "#0:init:id=" + stateId + "&server=" 
-			+ encodeURIComponent(facade._ifpServerUrl) + "&fr=false";
-		if(this.needFrameRecursion()){
-			//IE7 hack. Need to load server URL, and have that load the xip_client.html.
-			//Also, this server URL needs to different from the one eventually loaded by xip_client.html
-			//Otherwise, IE7 will not load it. Funky.
-			var fullClientUrl = window.location.href.split("#")[0].split("?")[0];
-			if((this.xipClientUrl + "").charAt(0) == "/"){
-				var endIndex = fullClientUrl.indexOf("://");
-				endIndex = fullClientUrl.indexOf("/", endIndex + 3);
-				fullClientUrl = fullClientUrl.substring(0, endIndex);
-			}else{
-				fullClientUrl = fullClientUrl.substring(0, fullClientUrl.lastIndexOf("/") + 1);
+		//Make sure we are not dealing with javascript urls, just to be safe.
+		if(!url.split(":")[0].match(/javascript/i) && !facade._ifpServerUrl.split(":")[0].match(/javascript/i)){
+			//Make xip_client a full URL.
+			var colonIndex = url.indexOf(":");
+			var slashIndex = url.indexOf("/");
+			if(colonIndex == -1 || slashIndex < colonIndex){
+				//No colon or we are starting with a / before a colon, so we need to make a full URL.
+				var loc = window.location.href;
+				if(slashIndex == 0){
+					//Have a full path, just need the domain.
+					url = loc.substring(0, loc.indexOf("/", 9)) + url; //Using 9 to get past http(s)://
+				}else{
+					url = loc.substring(0, (loc.lastIndexOf("/") + 1)) + url;
+				}
 			}
-			fullClientUrl += this.xipClientUrl;
-		
-			var serverUrl = facade._ifpServerUrl
-				+ (facade._ifpServerUrl.indexOf("?") == -1 ? "?" : "&") + "dojo.fr=1";
-
-			frameUrl = serverUrl + "#0:init:id=" + stateId + "&client=" 
-				+ encodeURIComponent(fullClientUrl) + "&fr=" + this.needFrameRecursion(); //fr is for Frame Recursion
-		}
-
-		this._state[stateId] = {
-			facade: facade,
-			stateId: stateId,
-			clientFrame: dojo.io.iframe.create(stateId, "", frameUrl)
-		};
-		
-		return stateId;
-	},
 	
+			var frameUrl = facade._ifpServerUrl + "#0:init:id=" + stateId + "&client=" 
+				+ encodeURIComponent(url) + "&callback=" + encodeURIComponent(this._callbackName);
+	
+			this._state[stateId] = {
+				facade: facade,
+				stateId: stateId,
+				clientFrame: dojo.io.iframe.create(stateId, "", frameUrl),
+				isSending: false,
+				serverUrl: facade._ifpServerUrl,
+				requestData: null,
+				responseMessage: "",
+				requestParts: [],
+				idCounter: 1,
+				partIndex: 0,
+				serverWindow: null
+			};
+	
+			return stateId;
+		}
+	},
+
 	receive: function(/*String*/stateId, /*String*/urlEncodedData){
 		/* urlEncodedData should have the following params:
 				- responseHeaders
@@ -92,7 +130,7 @@ dojox.io.proxy.xip = {
 			var contentType = facade.getResponseHeader("Content-Type");
 			if(contentType){
 				var mimeType = contentType.split(";")[0];
-				if(mimeType == "application/xml" || mimeType == "text/xml"){
+				if(mimeType.indexOf("application/xml") == 0 || mimeType.indexOf("text/xml") == 0){
 					facade.responseXML = dojox.data.dom.createDocument(response.responseText, contentType);
 				}
 			}
@@ -102,11 +140,11 @@ dojox.io.proxy.xip = {
 		this.destroyState(stateId);
 	},
 
-	clientFrameLoaded: function(/*String*/stateId){
+	frameLoaded: function(/*String*/stateId){
 		var state = this._state[stateId];
 		var facade = state.facade;
 
-		var clientWindow = this.needFrameRecursion() ? window.open("", state.stateId + "_clientEndPoint") : state.clientFrame.contentWindow;
+		var clientWindow = state.clientFrame.contentWindow;
 
 		var reqHeaders = [];
 		for(var param in facade._requestHeaders){
@@ -126,7 +164,7 @@ dojox.io.proxy.xip = {
 			requestData.data = facade._bodyData;
 		}
 
-		clientWindow.send(dojo.objectToQuery(requestData));
+		this.sendRequest(stateId, dojo.objectToQuery(requestData));
 	},
 	
 	destroyState: function(/*String*/stateId){
@@ -146,6 +184,144 @@ dojox.io.proxy.xip = {
 		}else{
 			return dojox.io.proxy.xip._xhrObjOld.apply(dojo, arguments);
 		}
+	},
+	
+	//**** State-bound methods ****
+	sendRequest: function(stateId, encodedData){
+		var state = this._state[stateId];
+		if(!state.isSending){
+			state.isSending = true;
+
+			state.requestData = encodedData || "";
+
+			//Get a handle to the server iframe.
+			state.serverWindow = frames[state.stateId];
+			if (!state.serverWindow){
+				state.serverWindow = document.getElementById(state.stateId).contentWindow;
+			}
+			
+			if(state.serverWindow.contentWindow){
+				state.serverWindow = state.serverWindow.contentWindow;
+			}
+
+			this.sendRequestStart(stateId);
+		}
+	},
+
+	sendRequestStart: function(stateId){
+		//Break the message into parts, if necessary.
+		var state = this._state[stateId];
+		state.requestParts = [];
+		var reqData = state.requestData;
+		var urlLength = state.serverUrl.length;
+		var partLength = this.urlLimit - urlLength;
+		var reqIndex = 0;
+
+		while((reqData.length - reqIndex) + urlLength > this.urlLimit){
+			var part = reqData.substring(reqIndex, reqIndex + partLength);
+			//Safari will do some extra hex escaping unless we keep the original hex
+			//escaping complete.
+			var percentIndex = part.lastIndexOf("%");
+			if(percentIndex == part.length - 1 || percentIndex == part.length - 2){
+				part = part.substring(0, percentIndex);
+			}
+			state.requestParts.push(part);
+			reqIndex += part.length;
+		}
+		state.requestParts.push(reqData.substring(reqIndex, reqData.length));
+		
+		state.partIndex = 0;
+		this.sendRequestPart(stateId);
+
+	},
+	
+	sendRequestPart: function(stateId){
+		var state = this._state[stateId];
+
+		if(state.partIndex < state.requestParts.length){
+			//Get the message part.
+			var partData = state.requestParts[state.partIndex];
+
+			//Get the command.
+			var cmd = "part";
+			if(state.partIndex + 1 == state.requestParts.length){
+				cmd = "end";
+			}else if (state.partIndex == 0){
+				cmd = "start";
+			}
+			
+			this.setServerUrl(stateId, cmd, partData);
+			state.partIndex++;
+		}
+	},
+
+	setServerUrl: function(stateId, cmd, message){
+		var serverUrl = this.makeServerUrl(stateId, cmd, message);
+		var state = this._state[stateId];
+
+		//Safari won't let us replace across domains.
+		if(this._isWebKit){
+			state.serverWindow.location = serverUrl;
+		}else{
+			state.serverWindow.location.replace(serverUrl);
+		}
+	},
+
+	makeServerUrl: function(stateId, cmd, message){
+		var state = this._state[stateId];
+		var serverUrl = state.serverUrl + "#" + (state.idCounter++) + ":" + cmd;
+		if(message){
+			serverUrl += ":" + message;
+		}
+		return serverUrl;
+	},
+
+	fragmentReceived: function(frag){
+		var index = frag.indexOf("#");
+		var stateId = frag.substring(0, index);
+		var encodedData = frag.substring(index + 1, frag.length);
+
+		var msg = this.unpackMessage(encodedData);
+		var state = this._state[stateId];
+
+		switch(msg.command){
+			case "loaded":
+				this.frameLoaded(stateId);
+				break;
+			case "ok":
+				this.sendRequestPart(stateId);
+				break;
+			case "start":
+				state.responseMessage = "" + msg.message;
+				this.setServerUrl(stateId, "ok");
+				break;
+			case "part":
+				state.responseMessage += msg.message;			
+				this.setServerUrl(stateId, "ok");
+				break;
+			case "end":
+				this.setServerUrl(stateId, "ok");
+				state.responseMessage += msg.message;
+				this.receive(stateId, state.responseMessage);
+				break;
+		}
+	},
+	
+	unpackMessage: function(encodedMessage){
+		var parts = encodedMessage.split(":");
+		var command = parts[1];
+		encodedMessage = parts[2] || "";
+
+		var config = null;
+		if(command == "init"){
+			var configParts = encodedMessage.split("&");
+			config = {};
+			for(var i = 0; i < configParts.length; i++){
+				var nameValue = configParts[i].split("=");
+				config[decodeURIComponent(nameValue[0])] = decodeURIComponent(nameValue[1]);
+			}
+		}
+		return {command: command, message: encodedMessage, config: config};
 	}
 }
 
