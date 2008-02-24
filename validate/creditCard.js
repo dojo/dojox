378e5bc10a4a664b3ba97145690b4364194fa759
Adam Peller <github@peller.org>
2008-02-24 07:09:07 +0000

    don't do a dojo.indexOf on a hash. Fixes #5956
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@12675 560b804f-0ae3-0310-86f3-f6aa0a117693

--- validate/creditCard.js
+++ validate/creditCard.js
@@ -9,26 +9,25 @@ dojo.require("dojox.validate._base");
 
 dojox.validate.isValidCreditCard = function(/*String|Int*/value, /*String*/ccType){
 	//Summary:
-	//  checks if type matches the # scheme, and if Luhn checksum is accurate (unless its an Enroute card, the checkSum is skipped)
+	//  checks if ccType matches the # scheme in value, and if Luhn checksum is accurate (unless its an Enroute card, the checkSum is skipped)
 	
 	//Value: Boolean
-	if(value&&ccType&&((ccType.toLowerCase()=='er'||dojox.validate.isValidLuhn(value))&&(dojox.validate.isValidCreditCardNumber(value,ccType.toLowerCase())))){
-			return true; //Boolean
-	}
-	return false; //Boolean
+	return ((ccType.toLowerCase() == 'er' || dojox.validate.isValidLuhn(value)) &&
+			dojox.validate.isValidCreditCardNumber(value,ccType.toLowerCase())); //Boolean
 }
-dojox.validate.isValidCreditCardNumber = function(/*String|Int*/value,/*String?*/ccType) {
-	//Summary:
-	//  checks if the # matches the pattern for that card or any card types if none is specified
-	//  value == CC #, white spaces and dashes are ignored
-	//  ccType is of the values in cardinfo -- if Omitted it it returns a | delimited string of matching card types, or false if no matches found
-	
-	//Value: Boolean
-	
-	if(typeof value!='string'){
-		value = String(value);
-	}
-	value = value.replace(/[- ]/g,''); //ignore dashes and whitespaces
+
+dojox.validate.isValidCreditCardNumber = function(/*String|Int*/value,/*String?*/ccType){
+	// summary:
+	//  checks if value matches the pattern for that card or any card types if none is specified
+	//
+	// value: Boolean
+	// CC #, white spaces and dashes are ignored
+	//
+	// ccType: String?
+	// one of the values in cardinfo -- if Omitted it it returns a | delimited string of matching card types, or false if no matches found
+
+	value = String(value).replace(/[- ]/g,''); //ignore dashes and whitespaces
+
 	/* 	FIXME: not sure on all the abbreviations for credit cards,below is what each stands for atleast to my knowledge
 		mc: Mastercard
 		ec: Eurocard
@@ -40,23 +39,23 @@ dojox.validate.isValidCreditCardNumber = function(/*String|Int*/value,/*String?*
 		jcb: JCB
 		er: Enroute
 	 */
-	var results=[];
 	var cardinfo = {
-		'mc':'5[1-5][0-9]{14}','ec':'5[1-5][0-9]{14}','vi':'4([0-9]{12}|[0-9]{15})',
-		'ax':'3[47][0-9]{13}', 'dc':'3(0[0-5][0-9]{11}|[68][0-9]{12})',
-		'bl':'3(0[0-5][0-9]{11}|[68][0-9]{12})','di':'6011[0-9]{12}',
-		'jcb':'(3[0-9]{15}|(2131|1800)[0-9]{11})','er':'2(014|149)[0-9]{11}'
+		'mc':'5[1-5][0-9]{14}','ec':'5[1-5][0-9]{14}','vi':'4(?:[0-9]{12}|[0-9]{15})',
+		'ax':'3[47][0-9]{13}', 'dc':'3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+		'bl':'3(?:0[0-5][0-9]{11}|[68][0-9]{12})','di':'6011[0-9]{12}',
+		'jcb':'(?:3[0-9]{15}|(2131|1800)[0-9]{11})','er':'2(?:014|149)[0-9]{11}'
 	};
-	if(ccType&&dojo.indexOf(cardinfo,ccType.toLowerCase())){
-		return Boolean(value.match(cardinfo[ccType.toLowerCase()])); // boolean
-	}else{
-		for(var p in cardinfo){
-			if(value.match('^'+cardinfo[p]+'$')!=null){
-				results.push(p);
-			}
+	if(ccType){
+		var expr = cardinfo[ccType.toLowerCase()];
+		return expr ? !!(value.match(cardinfo[ccType.toLowerCase()])) : false; // boolean
+	}
+	var results=[];
+	for(var p in cardinfo){
+		if(value.match('^'+cardinfo[p]+'$')){
+			results.push(p);
 		}
-		return (results.length)?results.join('|'):false; // string | boolean
-	}	
+	}
+	return results.length ? results.join('|') : false; // string | boolean
 }
 
 dojox.validate.isValidCvv = function(/*String|Int*/value, /*String*/ccType) {
@@ -84,8 +83,6 @@ dojox.validate.isValidCvv = function(/*String|Int*/value, /*String*/ccType) {
 	}
 	var flags = {format:format};
 	//FIXME? Why does isNumberFormat take an object for flags when its only parameter is either a string or an array inside the object?
-	if ((value.length == format.length)&&(dojox.validate.isNumberFormat(value, flags))){
-		return true; //Boolean
-	}
-	return false; //Boolean
+	//FIXME: um... just check value.length?
+	return (value.length == format.length && dojox.validate.isNumberFormat(value, flags)); //Boolean
 }
