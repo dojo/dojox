9e7f5022e9b5c0be122fe8db6bbd6648d3842ccd
Peter E Higgins <dante@dojotoolkit.org>
2008-03-11 21:40:37 +0000

    removing potentially questionalable language definitions.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@13041 560b804f-0ae3-0310-86f3-f6aa0a117693

--- highlight/languages/_all.js
+++ highlight/languages/_all.js
@@ -5,11 +5,3 @@ dojo.require("dojox.highlight.languages._static");
 dojo.require("dojox.highlight.languages._dynamic");
 dojo.require("dojox.highlight.languages._www");
 
-/* additional supported language maps */
-dojo.require("dojox.highlight.languages.1c");
-dojo.require("dojox.highlight.languages.axapta");
-dojo.require("dojox.highlight.languages.rib");
-dojo.require("dojox.highlight.languages.rsl");
-dojo.require("dojox.highlight.languages.smalltalk");
-dojo.require("dojox.highlight.languages.sql");
-dojo.require("dojox.highlight.languages.vbscript");
--- highlight/languages/_static.js
+++ highlight/languages/_static.js
@@ -2,5 +2,5 @@ dojo.provide("dojox.highlight.languages._static");
 
 /* common static languages */
 dojo.require("dojox.highlight.languages.cpp")
-dojo.require("dojox.highlight.languages.java");
+// dojo.require("dojox.highlight.languages.java");
 dojo.require("dojox.highlight.languages.delphi");
