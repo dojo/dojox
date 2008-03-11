468f0b9015c7aed1a5d548abfac8fc567281e066
Brad Neuberg <BradNeuberg@dojotoolkit.org>
2008-03-11 19:22:20 +0000

    fixes #5959
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@13035 560b804f-0ae3-0310-86f3-f6aa0a117693

--- storage/_common.js
+++ storage/_common.js
@@ -2,13 +2,16 @@ dojo.provide("dojox.storage._common");
 dojo.require("dojox.storage.Provider");
 dojo.require("dojox.storage.manager");
 
-if(djConfig.offlineProfile){
-  dojo.require("dojox.storage.GearsStorageProvider");
-}else{
-  dojo.require("dojox.storage.GearsStorageProvider");
-  dojo.require("dojox.storage.WhatWGStorageProvider");
-  dojo.require("dojox.storage.FlashStorageProvider");
-}
+/*
+  Note: if you are doing Dojo Offline builds you _must_
+  have offlineProfile=true when you run the build script:
+  ./build.sh action=release profile=offline offlineProfile=true
+*/
+dojo.require("dojox.storage.GearsStorageProvider");
+//>>excludeStart("offlineProfileExclude", kwArgs.offlineProfile == "true");
+dojo.require("dojox.storage.WhatWGStorageProvider");
+dojo.require("dojox.storage.FlashStorageProvider");
+//>>excludeEnd("offlineProfileExclude");
 
 // now that we are loaded and registered tell the storage manager to
 // initialize itself
