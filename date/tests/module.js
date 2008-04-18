07202720a851abb894568d277915b046c5e4f87b
Adam Peller <github@peller.org>
2008-04-18 22:25:35 +0000

    Hebrew Calendar initial implementation, from Hossam Aldin Katory (IBM).  Refs #5074 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@13354 560b804f-0ae3-0310-86f3-f6aa0a117693

--- date/tests/module.js
+++ date/tests/module.js
@@ -1,8 +1,9 @@
 dojo.provide("dojox.date.tests.module");
 
 try{
-	dojo.require("dojox.date.tests.posix");
+	dojo.require("dojox.date.tests.HebrewDate");
 	dojo.require("dojox.date.tests.IslamicDate");
+	dojo.require("dojox.date.tests.posix");
 }catch(e){
 	doh.debug(e);
 }
