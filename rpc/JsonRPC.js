57c1b77c2ed5a2e98960a8fca54166d71659513c
Alex Russell <slightlyoff@chromium.org>
2008-04-24 20:18:48 +0000

    merging Kris Zyp's patch for to bump up the naming of the next JSON-RPC version (CLA and CCLA for SitePen on file). Also fixing some whack whitespace issues while I'm at it. Fixes #6278. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@13395 560b804f-0ae3-0310-86f3-f6aa0a117693

--- rpc/JsonRPC.js
+++ rpc/JsonRPC.js
@@ -1,23 +1,32 @@
 dojo.provide("dojox.rpc.JsonRPC");
 
 dojox.rpc.envelopeRegistry.register(
-	"JSON-RPC-1.0",function(str){return str == "JSON-RPC-1.0"},{
+	"JSON-RPC-1.0",
+	function(str){
+		return str == "JSON-RPC-1.0";
+	},
+	{
 		serialize: function(smd, method, data, options){
-			//not converted to json it self. This  will be done, if appropriate, at the 
-			//transport level
-	                var d = dojox.rpc.toOrdered(method, data);
-					d = dojox.rpc.toJson({id: this._requestId++, method: method.name, params: d});
-	
-	                return {
-	                        data: d,
-	                        contentType: 'application/json',
-	                        transport:"POST"
-	                }
+			//not converted to json it self. This  will be done, if
+			//appropriate, at the transport level
+			var d = dojox.rpc.toOrdered(method, data);
+
+			d = dojox.rpc.toJson({
+				id: this._requestId++,
+				method: method.name,
+				params: d
+			});
+
+			return {
+				data: d,
+				contentType: 'application/json',
+				transport:"POST"
+			};
 		},
 
 		deserialize: function(results){
 			var obj = dojox.rpc.resolveJson(results);
-			if (obj.error) {
+			if(obj.error){
 				var e = new Error(obj.error);
 				e._rpcErrorObject = obj.error;
 				return e;
@@ -28,22 +37,31 @@ dojox.rpc.envelopeRegistry.register(
 );
 
 dojox.rpc.envelopeRegistry.register(
-	"JSON-RPC-1.2",function(str){return str == "JSON-RPC-1.2"},{
+	"JSON-RPC-2.0",
+	function(str){
+		return str == "JSON-RPC-2.0";
+	},
+	{
 		serialize: function(smd, method, data, options){
-	                var trans = method.transport || smd.transport || "POST";
-	                var d = dojox.rpc.toNamed(method, data);
+			var trans = method.transport || smd.transport || "POST";
+			var d = dojox.rpc.toNamed(method, data);
 	
-			d = dojox.rpc.toJson({id: this._requestId++, method: method.name, params: data});
-	                return {
-	                        data: d,
-	                        contentType: 'application/json',
-	                        transport:"POST"
-	                }
+			d = dojox.rpc.toJson({
+				id: this._requestId++, 
+				method: method.name, 
+				params: data
+			});
+
+			return {
+				data: d,
+				contentType: 'application/json',
+				transport:"POST"
+			};
 		},
 	
 		deserialize: function(results){
 			var obj = dojox.rpc.resolveJson(results);
-			if (obj.error) {
+			if(obj.error){
 				var e = new Error(obj.error.message);
 				e._rpcErrorObject = obj.error;
 				return e;
