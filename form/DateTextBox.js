f8487d5bf12282d5e3868b5276e8f2982b496ac0
Nathan Toone <nathan@toonetown.com>
2008-05-09 15:40:44 +0000

    fixes #6528: proxy commit for shane which splits dojox.widget.Calendar into mixins.  Some minor issues still exist - but they are visual issues, and probably should be tracked in separate bugs.  In IE6, the "previous" and "next" buttons are not transparent (this is due to using a different background color in the header).  In Safari, the fisheye effect does not work, and the custom highlighting produces weird results.  However, since these are both minor issues, I am marking this bug as fixed - and separate bugs can be opened for those issues.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@13652 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/DateTextBox.js
+++ form/DateTextBox.js
@@ -10,27 +10,61 @@ dojo.declare(
 		// summary:
 		//		A validating, serializable, range-bound date text box with a popup calendar
 
+		// popupClass: String
+		//  The popup widget to use. In this case, a calendar with Day, Month and Year views.
 		popupClass: "dojox.widget.Calendar",
 		_selector: "date",
-		useMonthOnly: false,
-		useDayOnly: false,
-		useYearOnly: false,
 		
 		_open: function(){
 			this.inherited(arguments);
 			dojo.style(this._picker.domNode.parentNode, "position", "absolute");
+		}
+	}
+);
+
+dojo.declare(
+	"dojox.form.MonthTextBox",
+	dojox.form.DateTextBox, 
+	{
+		// summary:
+		//		A validating, serializable, range-bound date text box with a popup calendar that contains just months.
+		
+		
+		// popupClass: String
+		//  The popup widget to use. In this case, a calendar with just a Month view.
+		popupClass: "dojox.widget.MonthlyCalendar",
+		
+		_open: function(){
+			this.inherited(arguments);
 			
-			var mode = this.useDayOnly ? "day" : (this.useMonthOnly ? "month" : (this.useYearOnly ? "year" : null));
-			if(this.useMonthOnly || this.useYearOnly){
-				this._picker.onValueSelected = dojo.hitch(this, function(value){
-					this.focus(); // focus the textbox before the popup closes to avoid reopening the popup
-					setTimeout(dojo.hitch(this, "_close"), 1); // allow focus time to take
-					dijit.form.TextBox.prototype.setValue.call(this,value, true, value);
-				});
-			}
-			if(mode){
-				this._picker.setMode(mode);
-			}
+			this._picker.onValueSelected = dojo.hitch(this, function(value){
+				this.focus(); // focus the textbox before the popup closes to avoid reopening the popup
+				setTimeout(dojo.hitch(this, "_close"), 1); // allow focus time to take
+				dijit.form.TextBox.prototype.setValue.call(this,value + 1, true, value + 1);
+			});			
+		}
+	}
+);
+
+dojo.declare(
+	"dojox.form.YearTextBox",
+	dojox.form.DateTextBox, 
+	{
+		// summary:
+		//		A validating, serializable, range-bound date text box with a popup calendar that contains only years
+		
+		// popupClass: String
+		//  The popup widget to use. In this case, a calendar with just a Year view.
+		popupClass: "dojox.widget.YearlyCalendar",
+		
+		_open: function(){
+			this.inherited(arguments);
+			
+			this._picker.onValueSelected = dojo.hitch(this, function(value){
+				this.focus(); // focus the textbox before the popup closes to avoid reopening the popup
+				setTimeout(dojo.hitch(this, "_close"), 1); // allow focus time to take
+				dijit.form.TextBox.prototype.setValue.call(this,value, true, value);
+			});						
 		}
 	}
 );
