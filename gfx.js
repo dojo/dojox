e934e1e700237268f95d4d56c0619b74e951ec6b
James Burke <jrburke@gmail.com>
2008-06-02 04:09:25 +0000

    Refs #4462: Get xdomain loading to work for dojox.gfx. Requires a new loader function, dojo.loadInit() for this to work. Not my favorite, but it is the only robust option at this point.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@13852 560b804f-0ae3-0310-86f3-f6aa0a117693

--- gfx.js
+++ gfx.js
@@ -3,31 +3,37 @@ dojo.provide("dojox.gfx");
 dojo.require("dojox.gfx.matrix");
 dojo.require("dojox.gfx._base");
 
-(function(){
-	var renderers = (typeof dojo.config["gfxRenderer"] == "string" ?
-		dojo.config["gfxRenderer"] : "svg,vml,silverlight,canvas").split(",");
-	for(var i = 0; i < renderers.length; ++i){
-		switch(renderers[i]){
-			case "svg":
-				//TODO: need more comprehensive test for SVG
-				if(!dojo.isIE && (navigator.userAgent.indexOf("iPhone") < 0) && (navigator.userAgent.indexOf("iPod") < 0)){ dojox.gfx.renderer = "svg"; }
-				break;
-			case "vml":
-				if(dojo.isIE != 0){ dojox.gfx.renderer = "vml"; }
-				break;
-			case "silverlight":
-				//TODO: need more comprehensive test for Silverlight
-				if(window.Silverlight){ dojox.gfx.renderer = "silverlight"; }
-				break;
-			case "canvas":
-				//TODO: need more comprehensive test for Canvas
-				if(dojo.isIE == 0){ dojox.gfx.renderer = "canvas"; }
-				break;
+dojo.loadInit(function(){
+	//Since loaderInit can be fired before any dojo.provide/require calls,
+	//make sure the dojox.gfx object exists and only run this logic if dojox.gfx.renderer
+	//has not been defined yet.
+	var gfx = dojo.getObject("dojox.gfx", true);
+	if(!gfx.renderer){
+		var renderers = (typeof dojo.config["gfxRenderer"] == "string" ?
+			dojo.config["gfxRenderer"] : "svg,vml,silverlight,canvas").split(",");
+		for(var i = 0; i < renderers.length; ++i){
+			switch(renderers[i]){
+				case "svg":
+					//TODO: need more comprehensive test for SVG
+					if(!dojo.isIE && (navigator.userAgent.indexOf("iPhone") < 0) && (navigator.userAgent.indexOf("iPod") < 0)){ dojox.gfx.renderer = "svg"; }
+					break;
+				case "vml":
+					if(dojo.isIE != 0){ dojox.gfx.renderer = "vml"; }
+					break;
+				case "silverlight":
+					//TODO: need more comprehensive test for Silverlight
+					if(window.Silverlight){ dojox.gfx.renderer = "silverlight"; }
+					break;
+				case "canvas":
+					//TODO: need more comprehensive test for Canvas
+					if(dojo.isIE == 0){ dojox.gfx.renderer = "canvas"; }
+					break;
+			}
+			if(dojox.gfx.renderer){ break; }
 		}
-		if(dojox.gfx.renderer){ break; }
+		console.log("gfx renderer = " + dojox.gfx.renderer);
 	}
-	console.log("gfx renderer = " + dojox.gfx.renderer);
-})();
+});
 
 // include a renderer conditionally
 dojo.requireIf(dojox.gfx.renderer == "svg", "dojox.gfx.svg");
