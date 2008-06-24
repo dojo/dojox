09a0e26edf3aad30dfe80f739fcdd38e3cd0b198
Nathan Toone <nathan@toonetown.com>
2008-06-24 00:07:12 +0000

    Refs: #7033 - first work towards a fancy file browser widget.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@14112 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ widget/FilePicker.js
@@ -0,0 +1,303 @@
+dojo.provide("dojox.widget.FilePicker");
+dojo.experimental("dojox.widget.FilePicker");
+
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+dojo.require("dijit._Container");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.Menu");
+
+dojo.require("dojo.i18n"); 
+dojo.requireLocalization("dojox.widget", "FilePicker"); 
+
+dojo.declare("dojox.widget._FilePickerItemMixin", null, {
+	// summary: a mixin to provide shared functionality between different types
+	//	of pickers
+	
+	// store: dojo.data.api.Read
+	//	a read store
+	store: null,
+	
+	// item: item
+	//	a dojo.data item from teh above store
+	item: null,
+	
+	postMixInProperties: function(){
+		this.label = this.store.getLabel(this.item);
+		this.inherited(arguments);
+	},
+	
+	blur: function(){
+		// summary: blurs this item
+		dojo.removeClass(this.domNode, "dijitMenuItemHover");
+	},
+	
+	postCreate: function(){
+		dojo.addClass(this.domNode, "dojoxPickerItem");
+		if(this.type){
+			dojo.addClass(this.domNode, "dojox" + this.type + "PickerItem");
+		}
+		this.connect(this.focusNode, "blur", "blur");
+		this.inherited(arguments);
+	}
+});
+
+dojo.declare("dojox.widget._FilePickerFileItem",
+	[dijit.MenuItem, dojox.widget._FilePickerItemMixin], {
+	// summary: an item that represents a file
+	
+	// type: String
+	//  The type of this item
+	type: "File"
+});
+
+dojo.declare("dojox.widget._FilePickerDirectoryItem",
+	[dijit.MenuItem, dojox.widget._FilePickerItemMixin], {
+	// summary: an item that represents a directory
+	
+	// type: String
+	//  The type of this item
+	type: "Directory",
+	
+	startup: function(){
+		dojo.style(this.arrowWrapper, "display", "");
+	}
+});
+
+dojo.declare("dojox.widget._FileInfoPane",
+	[dijit._Widget, dijit._Templated, dijit._Contained], {
+	// summary: a pane to display the information for the currently-selected
+	//  file
+	
+	// templatePath: string
+	//  Our template path
+	templatePath: dojo.moduleUrl("dojox.widget", "FilePicker/_FileInfoPane.html"),
+	
+	// store: dojo.data.api.Read
+	//  the read store we must use
+	store: null,
+	
+	// item: item
+	//  the item from the above store for this pane to display
+	item: null,
+	
+	postMixInProperties: function(){
+		this._messages = dojo.i18n.getLocalization("dojox.widget", "FilePicker", this.lang);
+	},
+	
+	postCreate: function(){
+		var store = this.store, item = this.item;
+		this.inherited(arguments);
+		// Set the values of the info for this item
+		this.nameNode.innerHTML = store.getLabel(item);
+		this.pathNode.innerHTML = store.getIdentity(item);
+		this.sizeNode.innerHTML = store.getValue(item, "size");
+	}
+});
+
+dojo.declare("dojox.widget._FilePickerPane", 
+	[dijit.layout.ContentPane, dijit._Templated, dijit._Contained], {
+	// summary: a pane that represents a directory - provides options for
+	//  selecting sub-items.
+	
+	// templateString: string
+	//	our template
+	templateString: '<div class="dojoxPickerPane" dojoAttachPoint="containerNode"></div>',
+	
+	// store: dojo.data.api.Read
+	//  the read store we must use
+	store: null,
+	
+	// query: object
+	//  a query to pass to the datastore.  This is only used if items are null
+	query: null,
+	
+	// queryOptions: object
+	//  query options to be passed to the datastore
+	queryOptions: null,
+	
+	// items: item[]
+	//  an array of (possibly not-yet-loaded) items to display in this.
+	//  If this array is null, then the query and query options are used to
+	//  get the top-level items to use.
+	items: null,
+	
+	// parentWidget: dojox.widget.FilePicker
+	//  Our parent widget
+	parentWidget: null,
+	
+	// class: string
+	//  We override this to dijitInline so things display correctly
+	"class": "dijitInline",
+	
+	_loadCheck: function(/* Boolean? */ forceLoad){
+		// summary: checks that the store is loaded
+		var displayState = this._isShown();
+		if((this.store || this.items) && (forceLoad || (this.refreshOnShow && displayState) || (!this.isLoaded && displayState))){
+			this._doQuery();
+		}
+	},
+	
+	_doQuery: function(){
+		// summary: either runs the query or loads potentially not-yet-loaded items.
+		this.isLoaded = false;
+		this._setContent(this.loadingMessage);
+		if(this.items){
+			var _waitCount = 0, store = this.store, items = this.items;
+			dojo.forEach(items, function(item){ if(!store.isItemLoaded(item)){ _waitCount++; }});
+			if(_waitCount === 0){
+				this._createItems();
+			}else{
+				var onItem = dojo.hitch(this, function(item){
+					_waitCount--;
+					if((_waitCount) === 0){
+						this._createItems();
+					}
+				});
+				dojo.forEach(items, function(item){
+					if(!store.isItemLoaded(item)){
+						store.loadItem({item: item, onItem: onItem});
+					}
+				});
+			}
+		}else{
+			this.store.fetch({query: this.query, onComplete: function(items){
+				this.items = items;
+				this._createItems();
+			}, scope: this});
+		}
+	},
+	
+	onItemClick: function(/* dijit.MenuItem */ item, /* Event */ evt){
+		// summary: Called when a menu item has been clicked.  Updates the 
+		//  panes correctly
+		var store = this.store;
+		if(item.type == "Directory" && !item._paneWidget){
+			item._paneWidget = new dojox.widget._FilePickerPane({store: store,
+																items: store.getValues(item.item, "children"),
+																parentWidget: this.parentWidget});
+		}else if(item.type == "File" && !item._paneWidget){
+			item._paneWidget = new dojox.widget._FileInfoPane({store: store, item: item.item});
+		}
+		if(item._paneWidget){
+			this.parentWidget.pushChild(item._paneWidget, this);
+			if(item._paneWidget.focus){
+				item._paneWidget.focus();
+			}
+		}
+		dojo.forEach(this.menu.getChildren(), function(i){
+			dojo.toggleClass(i.domNode, "dojoxPickerItemSelected", i == item);
+		});
+	},
+	
+	focus: function(){
+		// summary: focuses this widget (or its menu)
+		if(this.menu){
+			this.menu.focus();
+		}else{
+			var conn = this.connect(this, "_createItems", function(){
+				this.disconnect(conn);
+				this.menu.focus();
+			});
+		}
+	},
+	
+	blurAll: function(){
+		if(this.menu){
+			dojo.forEach(this.menu.getChildren(), function(i){
+				i.blur();
+				dojo.removeClass(i.domNode, "dojoxPickerItemSelected");
+			}, this);
+		}
+	},
+	
+	_createItems: function(){
+		// summary: Creates the menu widget and the items to place within it.
+		var self = this;
+		var menu = this.menu = new dijit.Menu({
+			onItemUnhover: function(/*MenuItem*/ item){item.blur();}
+		});
+		var store = this.store;
+		if(this.items.length){
+			dojo.forEach(this.items, function(item){
+				var id = store.getIdentity(item);
+				var w;
+				if(store.getValue(item, "directory")){
+					w = new dojox.widget._FilePickerDirectoryItem({store: store, item: item});
+				}else{
+					w = new dojox.widget._FilePickerFileItem({store: store, item: item});
+				}
+				menu.addChild(w);
+			});
+		}else{
+			var c = new dijit.MenuItem({
+				label: dojo.i18n.getLocalization("dojox.widget", "FilePicker", this.lang).empty,
+				disabled: true
+			});
+			dojo.addClass(c.domNode, "dojoxPickerItem");
+			menu.addChild(c);
+		}
+		this.connect(menu, "onItemClick", "onItemClick");
+		this.setContent(menu.domNode);
+		if(!menu.started){
+			menu.startup();
+		}
+		this.isLoaded = true;
+	}
+});
+
+dojo.declare("dojox.widget.FilePicker",
+	[dijit._Widget, dijit._Templated, dijit._Container], {
+	// summary: a file picking widget that can be tied with a data store (such
+	//  as dojox.data.FileStore)
+	
+	// templateString: string
+	//  our template string to use
+	templateString: '<div class="dojoxFilePicker" dojoAttachPoint="containerNode"></div>',
+	
+	// store: dojo.data.api.Read
+	//  the read store we must use
+	store: null,
+	
+	// query: object
+	//  a query to pass to the datastore.  This is only used if items are null
+	query: null,
+	
+	// queryOptions: object
+	//  query options to be passed to the datastore
+	queryOptions: null,
+
+	pushChild: function(/* dijit._Container */ child, /* dijit._Container? */ popUntil){
+		// summary: pushes the given child to the end of the container.  It pops
+		//  off any children from the end until the given popUntil is encountered.
+		//  If popUntil is null, then nothing is popped off the end.
+		var children, last;
+		while(popUntil && (children = this.getChildren()) !== null && 
+				children.length && (last = children[children.length - 1]) != popUntil){
+			if(last.blurAll){
+				last.blurAll();
+			}
+			this.removeChild(last);
+		}
+		this.addChild(child);
+		if(!child._started){
+			child.startup();
+		}
+		dijit.scrollIntoView(child.domNode);
+	},
+	
+	setStore: function(/* dojo.data.api.Read */ store){
+		// summary: sets the store for this widget */
+		if(store === this.store && this._started){ return; }
+		this.store = store;
+		this.pushChild(new dojox.widget._FilePickerPane({store: this.store, 
+											query: this.query, 
+											queryOptions: this.queryOptions,
+											parentWidget: this}));
+	},
+	
+	startup: function(){
+		this.setStore(this.store);
+		this.inherited(arguments);
+	}
+});
---
+++ widget/nls/FilePicker.js
@@ -0,0 +1,6 @@
+({
+name: "Name",
+path: "Path",
+size: "Size (in bytes)",
+empty: "&lt;EMPTY&gt;"
+})
