21e2e42637e93c2446a411d23e79c85d122c0281
Kris Zyp <kriszyp@gmail.com>
2008-07-04 16:37:29 +0000

    Better handling of partial HTTP messages
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@14266 560b804f-0ae3-0310-86f3-f6aa0a117693

--- io/httpParse.js
+++ io/httpParse.js
@@ -8,8 +8,8 @@ dojox.io.httpParse = function(/*String*/httpStream, /*String?*/topHeaders,/*Bool
 	//		Extra header information to add to each HTTP request (kind of HTTP inheritance)
 	// partial:
 	//		A true value indicates that the stream may not be finished, it may end arbitrarily in mid stream.
-	//		The last XHR object will have a special _lastIndex that indicates the how far along the httpStream
-	//		could be successfully parsed into HTTP messages.
+	//		The last XHR object will have a special property _lastIndex that indicates the how far along 
+	// 		the httpStream could be successfully parsed into HTTP messages.
 	// return:
 	// 		Returns an array of XHR-like object for reading the headers for each message
 	//		 
@@ -24,7 +24,10 @@ dojox.io.httpParse = function(/*String*/httpStream, /*String?*/topHeaders,/*Bool
 		httpStream = httpStream.substring(httpParts[0].length+1);
 		httpParts = httpParts[1];
 		var headerParts = httpStream.match(/([^\n]+\n)*/)[0];
-		httpStream = httpStream.substring(headerParts.length+1);
+		 
+		httpStream = httpStream.substring(headerParts.length);
+		var headerFollowingChar = httpStream.substring(0,1);
+		httpStream = httpStream.substring(1);
 		headerParts = (topHeaders || "") + headerParts;
 		var headerStr = headerParts;
 		headerParts = headerParts.match(/[^:\n]+:[^\n]+\n/g); // parse the containing and contained response headers with the contained taking precedence (by going last)
@@ -56,7 +59,7 @@ dojox.io.httpParse = function(/*String*/httpStream, /*String?*/topHeaders,/*Bool
 		}else if((content = httpStream.match(/(.*)HTTP\/\d\.\d \d\d\d[\w\s]*\n/))){ // assign content
 			// if we spot another HTTP message coming up, we will just assign all the in between text to the content
 			content = content[0];
-		}else if(!partial){
+		}else if(!partial || headerFollowingChar == '\n'){
 			// if we have to finish
 			content = httpStream;
 		}else{
