d192f0fd081c7a7490c727f3ce5493320227ff3c
Eugene Lazutkin <eugene@lazutkin.com>
2008-07-21 21:21:43 +0000

    functional: making object.js mirror array.js functionality. !strict Folds for objects are not implemented yet. Added proper tests.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@14539 560b804f-0ae3-0310-86f3-f6aa0a117693

--- lang/functional/object.js
+++ lang/functional/object.js
@@ -7,21 +7,14 @@ dojo.require("dojox.lang.functional.lambda");
 
 // Defined methods:
 //	- take any valid lambda argument as the functional argument
+//	- skip all attributes that are present in the empty object 
+//		(IE and/or 3rd-party libraries).
 
 (function(){
 	var d = dojo, df = dojox.lang.functional, empty = {};
 
 	d.mixin(df, {
 		// object helpers
-		forIn: function(/*Object*/ obj, /*Function|String|Array*/ f, /*Object?*/ o){
-			// summary: iterates over all object members skipping members, which 
-			//	are present in the empty object (IE and/or 3rd-party libraries).
-			o = o || d.global; f = df.lambda(f);
-			for(var i in obj){
-				if(i in empty){ continue; }
-				f.call(o, obj[i], i, obj);
-			}
-		},
 		keys: function(/*Object*/ obj){
 			// summary: returns an array of all keys in the object
 			var t = [];
@@ -39,6 +32,63 @@ dojo.require("dojox.lang.functional.lambda");
 				t.push(obj[i]);
 			}
 			return	t; // Array
+		},
+		filterIn: function(/*Object*/ obj, /*Function|String|Array*/ f, /*Object?*/ o){
+			// summary: creates new object with all attributes that pass the test 
+			//	implemented by the provided function.
+			o = o || d.global; f = df.lambda(f);
+			var t = {}, v;
+			for(var i in obj){
+				if(i in empty){ continue; }
+				v = obj[i];
+				if(f.call(o, v, i, obj)){ t[i] = v; }
+			}
+			return t;	// Object
+		},
+		forIn: function(/*Object*/ obj, /*Function|String|Array*/ f, /*Object?*/ o){
+			// summary: iterates over all object attributes.
+			o = o || d.global; f = df.lambda(f);
+			for(var i in obj){
+				if(i in empty){ continue; }
+				f.call(o, obj[i], i, obj);
+			}
+			return o;	// Object
+		},
+		mapIn: function(/*Object*/ obj, /*Function|String|Array*/ f, /*Object?*/ o){
+			// summary: creates new object with the results of calling 
+			//	a provided function on every attribute in this object.
+			o = o || d.global; f = df.lambda(f);
+			var t = {};
+			for(var i in obj){
+				if(i in empty){ continue; }
+				t[i] = f.call(o, obj[i], i, obj);
+			}
+			return t;	// Object
+		},
+		everyIn: function(/*Object*/ obj, /*Function|String|Array*/ f, /*Object?*/ o){
+			// summary: tests whether all attributes in the array pass the test 
+			//	implemented by the provided function.
+			o = o || d.global; f = df.lambda(f);
+			for(var i in obj){
+				if(i in empty){ continue; }
+				if(!f.call(o, obj[i], i, obj)){
+					return false;	// Boolean
+				}
+			}
+			return true;	// Boolean
+		},
+		someIn: function(/*Object*/ obj, /*Function|String|Array*/ f, /*Object?*/ o){
+			// summary: tests whether some attribute in the array passes the test 
+			//	implemented by the provided function.
+			if(typeof a == "string"){ a = a.split(""); }
+			o = o || d.global; f = df.lambda(f);
+			for(var i in obj){
+				if(i in empty){ continue; }
+				if(f.call(o, obj[i], i, obj)){
+					return true;	// Boolean
+				}
+			}
+			return false;	// Boolean
 		}
 	});
 })();
--- lang/tests/misc.js
+++ lang/tests/misc.js
@@ -1,11 +1,9 @@
 ﻿dojo.provide("dojox.lang.tests.misc");
 
-dojo.require("dojox.lang.functional.object");
 dojo.require("dojox.lang.functional.zip");
 
 (function(){
-	var df = dojox.lang.functional, fun = df.lambda("100*a + 10*b + c"), result = [];
-	df.forIn({a: 1, b: 2}, function(v, i){ result.push("[" + i + "] = " + v); });
+	var df = dojox.lang.functional, fun = df.lambda("100*a + 10*b + c");
 	
 	tests.register("dojox.lang.tests.misc", [
 		function testZip1(t){ t.assertEqual(df.zip([1, 2, 3], [4, 5, 6]), [[1, 4], [2, 5], [3, 6]]); },
@@ -18,10 +16,6 @@ dojo.require("dojox.lang.functional.zip");
 		function testFlip(t){ t.assertEqual(df.flip(fun)(3, 2, 1), 123); },
 		
 		function testCompose1(t){ t.assertEqual(df.lambda(["+5", "*3"])(8), 8 * 3 + 5); },
-		function testCompose2(t){ t.assertEqual(df.lambda(["+5", "*3"].reverse())(8), (8 + 5) * 3); },
-		
-		function testForIn(t){ t.assertEqual(result.sort().join(", "), "[a] = 1, [b] = 2"); },
-		function testKeys(t){ t.assertEqual(df.keys({a: 1, b: 2, c: 3}), ["a", "b", "c"]); },
-		function testValues(t){ t.assertEqual(df.values({a: 1, b: 2, c: 3}), [1, 2, 3]); }
+		function testCompose2(t){ t.assertEqual(df.lambda(["+5", "*3"].reverse())(8), (8 + 5) * 3); }
 	]);
 })();
