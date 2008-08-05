dcf114a786580b5d2effb6cea84483e18ee1bce2
Mike Wilcox <mwilcox@dojotoolkit.org>
2008-08-05 01:26:12 +0000

    Added ability to allow single or multi file Upload - Added ability to supply custom file mask - fixed Deft pub/sub so it can target one of many SWF movies
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@14705 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/FileInputFlash.js
+++ form/FileInputFlash.js
@@ -8,7 +8,7 @@ dojo.declare("dojox.form.FileInputFlash", null, {
 	//summary: 
 	
 	//	uploadUrl: /* String */
-	// The Url the file will be uploaded
+	// 		The Url the file will be uploaded
 	uploadUrl: "",			
 	
 	//	button: /* dijit.form.Button or a domNode */
@@ -16,15 +16,42 @@ dojo.declare("dojox.form.FileInputFlash", null, {
 	button:null,			
 	
 	// uploadOnChange: /* Boolean */
-	// if true, begins upload immediately
-	// leave false if you wish to display the text of the selection
-	//	and present an "upload" button
+	// 		if true, begins upload immediately
+	// 		leave false if you wish to display the text of the selection
+	//		and present an "upload" button
 	uploadOnChange: false, 	
 	
 	//	fieldName: /* String */
-	//	The form field attribute. This will be needed by the server to get the value.
-	//	If using the ReceiveFile.php test, leave this as-is.
+	//			The form field attribute. This will be needed by the server to get the value.
+	//			If using the ReceiveFile.php test, leave this as-is.
 	// TODO:fieldName:"uploadedfile",
+	
+	//selectMultipleFiles: /* Boolean */
+	// 		Option to restrict to single-file upload, or allow
+	// 		multi-file uploader
+	selectMultipleFiles:true,
+	
+	// fileMask: /* Array[ Array[Description, FileTypes], Array[...]...] */
+	// 		(an array, or an array of arrays)
+	//		Restrict file selection to certain file types
+	// Empty array defaults to "All Files"
+	//
+	// Usage:
+	//	fileMask = ["Images", "*.jpg;*.jpeg;*.gif;*.png"]
+	//	or
+	//	fileMask = [
+	//		["Jpeg File", 	"*.jpg;*.jpeg"],
+	//		["GIF File", 	"*.gif"],
+	//		["PNG File", 	"*.png"],
+	//		["All Images", 	"*.jpg;*.jpeg;*.gif;*.png"],
+	//	]
+	//	NOTE: MacType is not supported, as it does not work very well.
+	//			fileMask will work on a Mac, but differently than 
+	//			Windows. The second example above in Windows will mask
+	//			All but the selected file type, shown in a drop-down at
+	//			the bottom of the system dialog. In Mac, all types in 
+	//			all arrays will be shown and non-types masked.
+	fileMask:[],
 	
 	_swfPath: dojo.moduleUrl("dojox.form", "resources/uploader.swf"),
 	
@@ -32,34 +59,41 @@ dojo.declare("dojox.form.FileInputFlash", null, {
 	flashMovie:null,
 	
 	constructor: function(options){
+		console.log("Flash version detected:", dojox.embed.Flash.available);
+		
 		this.button = options.button;
 		this.uploadUrl = options.uploadUrl;
-		this.uploadOnChange = false;//options.uploadOnChange;
+		this.selectMultipleFiles = (options.selectMultipleFiles===undefined)?this.selectMultipleFiles:options.selectMultipleFiles;
+		this.fileMask = options.fileMask || this.fileMask;
+		this.uploadOnChange = false;
 		this.id = options.id || dijit.getUniqueId("flashuploader");
-		console.warn("FileInputFlash: ", this._swfPath);	
-		console.log("Flash version:", dojox.embed.Flash.available);
-		
+		//console.warn("this.parseFileMask:", this.parseFileMask());
 		var args = {
 			path:this._swfPath.uri,
 			width:1,
 			height:1,
+			// only pass in simple variables - no deep objects
 			vars:{
 				uploadUrl:this.uploadUrl, 
-				uploadOnSelect:this.uploadOnChange, 
-				id:dijit.getUniqueId("flash")
+				uploadOnSelect:this.uploadOnChange,
+				selectMultipleFiles:this.selectMultipleFiles,
+				id:this.id,
+				isDebug:true
 			}
-		}
+		};
+		console.log("VARS:", args.vars)
 		var flashDiv = dojo.doc.createElement("div");
 		dojo.body().appendChild(flashDiv);
 		dojo.style(flashDiv, "position", "absolute");
 		dojo.style(flashDiv, "top", "0");
 		dojo.style(flashDiv, "left", "0");
 		
-		dojo.subscribe("filesSelected", this, "onChange")
-		dojo.subscribe("filesUploaded", this, "onComplete")
+		dojo.subscribe(this.id+"/filesSelected", this, "onChange")
+		dojo.subscribe(this.id+"/filesUploaded", this, "onComplete")
 		this.flashObject = new dojox.embed.Flash(args, flashDiv);
 		this.flashObject.onLoad = dojo.hitch(this, function(mov){
-			this.flashMovie = mov;													
+			this.flashMovie = mov;
+			this.flashMovie.setFileMask(this.fileMask);
 		})
 		dojo.connect(this.button, "onClick", this, "openDialog");
 	},
@@ -81,15 +115,6 @@ dojo.declare("dojox.form.FileInputFlash", null, {
 	
 	onComplete: function(data){
 		console.warn("onComplete:", data);
-	},
-	
-	_registerCallback: function(strThisCallback){
-		var strFunc = this.id+"_"+strThisCallback;
-		var self = this;
-		dojo.global[strFunc] = function(data){ // FIXME: Make this some namespace
-			self[strThisCallback](data);
-		}
-		return strFunc;
 	}
 	
 });
