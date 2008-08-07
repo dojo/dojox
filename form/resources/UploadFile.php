0f7807c22ca08f5d623634fc444bf71c94d7cea6
Mike Wilcox <mwilcox@dojotoolkit.org>
2008-08-07 22:40:06 +0000

    FileUploader - Integrates FileInputOverlay and FileInput Flash into one file, where one object can be created, and uses Flash or degrades to HTML. - Added upload progress from Flash - Added Multi-File upload for HTML (reopening the dialog and adding to the list) - Fixed an IE bug for HTML button - Fixed HTML button alignment on scroll - Greatly improved the test.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@14754 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/resources/UploadFile.php
+++ form/resources/UploadFile.php
@@ -1,23 +1,119 @@
 <?php
+// summary
+//		Test file to handle image uploads (remove the image size check to upload non-images)
+//
+//		This file handles both Flash and HTML uploads
+//
+//		NOTE: This is obviously a PHP file, and thus you need PHP running for this to work
+//		NOTE: Directories must have write permissions
+//		NOTE: This code uses the GD library (to get image sizes), that sometimes is not pre-installed in a 
+//				standard PHP build. 
+//
+require("cLOG.php");
 
-$upload_path = "../resources/"; 	//relative to this file
-$download_path = "../resources/";	// relative to the HTML file
+function trace($txt){
+	//creating a text file that we can log to
+	// this is helpful on a remote server if you don't
+	//have access to the log files
+	//
+	error_log($txt);
+	$log = new cLOG("../resources/upload.txt", false);
+	//$log->clear();
+	$log->write($txt);
+}
+function getImageType($filename){
+	return strtolower(substr(strrchr($filename,"."),1));
+}
+trace("---------------------------------------------------------");
+//
+//
+//	EDIT ME: According to your local directory structure.
+// 	NOTE: Folders must have write permissions
+//
+$upload_path = "../resources/"; 	// where image will be uploaded, relative to this file
+$download_path = "../resources/";	// same folder as above, but relative to the HTML file
 
-move_uploaded_file($_FILES['Filedata']['tmp_name'],  $upload_path . $_FILES['Filedata']['name']);
-$file = $upload_path . $_FILES['Filedata']['name'];
+//
+// 	NOTE: maintain this path for JSON services
+//
+require("../../../dojo/tests/resources/JSON.php");
+$json = new Services_JSON();
 
-$type = exif_imagetype($file);
-list($width, $height) = getimagesize($file);
-//$type2 = exif_imagetype($_FILES['Filedata']['tmp_name']);
 
-error_log("file: " . $file );
-error_log("type: " . $type );
-//error_log("type2: " . $type2 );
+if( isset($_FILES['Filedata'])){
+	//
+	// If the data passed has 'Filedata', then it's Flash. That's the default fieldname used.
+	//
+	$returnFlashdata = true;
+	move_uploaded_file($_FILES['Filedata']['tmp_name'],  $upload_path . $_FILES['Filedata']['name']);
+	$file = $upload_path . $_FILES['Filedata']['name'];
+	$type = getImageType($file);
+	trace("file: " . $file );
+	// 		Flash gets a string back:
+	$data ='file='.$file.',width='.$width.',height='.$height.',type='.$type;
+	if($returnFlashdata){
+		trace("returnFlashdata");
+		echo($data);
+		return $data;
+	}
 
-error_log("temp: " . $_FILES['Filedata']['tmp_name']);
-error_log("file: " . "./images/".$_FILES['Filedata']['name']);
+}elseif( isset($_FILES['uploadedfile']) ){
+	//
+	// 	If the data passed has 'uploadedfile', then it's HTML. 
+	//	There may be better ways to check this, but this is just a test file.$returnFlashdata = false;
+	//
+	move_uploaded_file($_FILES['uploadedfile']['tmp_name'],  $upload_path . $_FILES['uploadedfile']['name']);
+	$file = $upload_path . $_FILES['uploadedfile']['name'];
+	$type = getImageType($file);
+	list($width, $height) = getimagesize($file);
+	trace("file: " . $file );
+	$ar = array(
+		'file' => $file,
+		'width' => $width,
+		'height' => $height,
+		'type'=> $type
+	);
 
-$data ='file='.$file.',width='.$width.',height='.$height;
-echo($data);
+}elseif( isset($_FILES['uploadedfile0']) ){
+	//
+	//	Multiple files have been passed from HTML
+	//
+	$cnt = 0;
+	$ar = array();
+	while(isset($_FILES['uploadedfile'.$cnt])){
+		$moved = move_uploaded_file($_FILES['uploadedfile'.$cnt]['tmp_name'],  $upload_path . $_FILES['uploadedfile'.$cnt]['name']);
+		if($moved){
+			$file = $upload_path . $_FILES['uploadedfile'.$cnt]['name'];
+			$type = getImageType($file);
+			list($width, $height) = getimagesize($file);
+			trace("file: " . $file );
+			$ar[] = array(
+				'file' => $file,
+				'width' => $width,
+				'height' => $height,
+				'type'=> $type
+			);
+		}
+		$cnt++;
+	}
+	
+}else{
+	//
+	//	deleting files
+	//
+	trace("DELETING FILES" . $_GET['rmFiles']);
+	$rmFiles = explode(";", $_GET['rmFiles']);
+	foreach($rmFiles as $f){
+		if($f && file_exists($f)){
+			trace("deleted:" . $f. ":" .unlink($f));
+		}
+	}
+	return;
+}
 
+//HTML gets a json array back:
+$data = $json->encode($ar);
+trace($data);
+// in a text field:
 ?>
+<textarea><?php print $data; ?></textarea>
