8ad27049dbfb2f195618352d3f729384ea8eb289
Peter E Higgins <dante@dojotoolkit.org>
2008-08-25 23:38:10 +0000

    fixes #7525 - nice new flip effect from nicola rizzo (cla on file). awesome work! minor cleanups and obfuscation to limit on-wire-bytes
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@14957 560b804f-0ae3-0310-86f3-f6aa0a117693

--- fx/_base.js
+++ fx/_base.js
@@ -282,4 +282,164 @@ dojox.fx.wipeTo = function(/*Object*/ args){
 
 	var anim = dojo.animateProperty(dojo.mixin({ properties: props }, args));
 	return anim; // dojo._Animation
-}
+};
+
+(function(){
+	
+	// because ShrinkSafe will eat this up:	
+	var borderConst = "border",
+		widthConst = "Width",
+		heightConst = "Height",
+		topConst = "Top",
+		rightConst = "Right",
+		leftConst = "Left",
+		bottomConst = "Bottom"
+	;
+
+	dojox.fx.flip = function(/*Object*/ args){
+		// summary: Animate a node flipping following a specific direction
+		//	
+		// description:
+		//		Returns an animation that will flip the
+		//		node around a central axis:
+		//		if args.dir is "left" or "right" --> y axis
+		//		if args.dir is "top" or "bottom" --> x axis
+		//
+		//		This effect is obtained using a border distorsion applied to a helper node.
+		//
+		//		The user can specify three background colors for the helper node:
+		//		darkColor: the darkest color reached during the animation 
+		//		lightColor: the brightest color
+		//		endColor: the final backgroundColor for the node
+		//
+		//	example:
+		//	|	var anim = dojox.fx.flip({ 
+		//	|		node: dojo.byId("nodeId"),
+		//	|		dir: "top",
+		//	|		darkColor: "#555555",
+		//	|		lightColor: "#dddddd",
+		//	|		endColor: "#666666",
+		//	|		duration:300
+		//	|	  });
+
+		var helperNode = dojo.doc.createElement("div");
+
+		var node = args.node = dojo.byId(args.node), 
+			s = node.style,
+			dims = null, 
+			hs = null, 
+			pn = null,
+			lightColor = args.lightColor || "#dddddd", 
+			darkColor = args.darkColor || "#555555", 
+			endColor = args.endColor || s.backgroundColor,
+			staticProps = {}, 
+			anims = [],
+			duration = args.duration ? args.duration / 2 : 250,
+			dir = args.dir || "left", 
+			pConst = 0.6, 
+			transparentColor = "transparent";
+
+		// IE6 workaround: IE6 doesn't support transparent borders
+		var convertColor = function(color){
+			return ((new dojo.Color(color)).toHex() === "#000000") ? "#000001" : color;
+		};
+
+		if(dojo.isIE < 7){
+			endColor = convertColor(endColor);
+			lightColor = convertColor(lightColor);
+			darkColor = convertColor(darkColor);
+			transparentColor = "black";
+			helperNode.style.filter = "chroma(color='#000000')";
+		}
+
+		var init = (function(n){
+			return function(){
+				var ret = dojo.coords(n, true);
+				dims = {
+					top: ret.y,
+					left: ret.x,
+					width: ret.w,
+					height: ret.h
+				};
+			}
+		})(node);
+		init();
+
+		// helperNode initialization
+		hs = {
+			position: "absolute",
+			top: dims["top"] + "px",
+			left: dims["left"] + "px",
+			height: "0",
+			width: "0",
+			zIndex: args.zIndex || (s.zIndex || 0),
+			border: "0 solid " + transparentColor,
+			fontSize: "0"
+		};
+		dims["endHeight"] = dims["height"] * pConst; 
+		dims["endWidth"] = dims["width"] * pConst; 
+		var props = [ {}, 
+			{
+				top: dims["top"],
+				left: dims["left"]
+			}
+		];
+		var dynProperties = {
+			left: [leftConst, rightConst, topConst, bottomConst, widthConst, heightConst, "end" + heightConst, leftConst],
+			right: [rightConst, leftConst, topConst, bottomConst, widthConst, heightConst, "end" + heightConst, leftConst],
+			top: [topConst, bottomConst, leftConst, rightConst, heightConst, widthConst, "end" + widthConst, topConst],
+			bottom: [bottomConst, topConst, leftConst, rightConst, heightConst, widthConst, "end" + widthConst, topConst] 
+		}
+		// property names
+		pn = dynProperties[dir];
+
+		staticProps[pn[5].toLowerCase()] = dims[pn[5].toLowerCase()] + "px";
+		staticProps[pn[4].toLowerCase()] = "0";
+		staticProps[borderConst + pn[1] + widthConst] = dims[pn[4].toLowerCase()] + "px";
+		staticProps[borderConst + pn[1] + "Color"] = s.backgroundColor;
+
+		var p0 = props[0];
+		p0[borderConst + pn[1] + widthConst] = 0; 
+		p0[borderConst + pn[1] + "Color"] = darkColor; 
+		p0[borderConst + pn[2] + widthConst] = dims[pn[6]] / 2;
+		p0[borderConst + pn[3] + widthConst] = dims[pn[6]] / 2;
+		p0[pn[2].toLowerCase()] = dims[pn[2].toLowerCase()] - dims[pn[6]] / 8;
+		p0[pn[7].toLowerCase()] = dims[pn[7].toLowerCase()] + dims[pn[4].toLowerCase()] / 2;
+		p0[pn[5].toLowerCase()] = dims[pn[6]];
+
+		var p1 = props[1];
+		p1[borderConst + pn[0] + "Color"] = { start: lightColor, end: endColor };
+		p1[borderConst + pn[0] + widthConst] = dims[pn[4].toLowerCase()];
+		p1[borderConst + pn[2] + widthConst] = 0;
+		p1[borderConst + pn[3] + widthConst] = 0;
+		p1[pn[5].toLowerCase()] = { start: dims[pn[6]], end: dims[pn[5].toLowerCase()] };
+
+		dojo.mixin(hs, staticProps);
+		dojo.style(helperNode, hs);
+		dojo.body().appendChild(helperNode);
+
+		var finalize = function(){
+			helperNode.parentNode.removeChild(helperNode);
+			s.visibility = "visible";
+		};
+
+		anims[1] = dojo.animateProperty({
+			node: helperNode, 
+			duration: duration,
+			properties: p1,
+			onEnd: finalize
+		});
+		anims[0] = dojo.animateProperty({
+			node: helperNode, 
+			duration: duration,
+			properties: p0
+		});
+
+		// hide the original node
+		dojo.connect(anims[0], "play", function(){ s.visibility = "hidden"; });
+
+		return dojo.fx.chain(anims); // dojo._Animation
+
+	}
+		
+})();
