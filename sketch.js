8a09e729641bf72a27ae07f7d0ab673f9546008d
Peter E Higgins <dante@dojotoolkit.org>
2008-09-05 15:59:22 +0000

    removing code duplicated in _base/html.js
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@15142 560b804f-0ae3-0310-86f3-f6aa0a117693

--- sketch.js
+++ sketch.js
@@ -1,8 +1,4 @@
 dojo.provide("dojox.sketch");
-try{
-	// fix IE image caching issue
-	document.execCommand("BackgroundImageCache", false, true);
-}catch(e){ }
 dojo.require("dojox.xml.DomParser");
 dojo.require("dojox.sketch.UndoStack");
 dojo.require("dojox.sketch.Figure");
