b0fc3fe1b6e007dbfd77a115ddb0ae2318aeaef1
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2008-11-10 21:09:13 +0000

    Minor fix for XmlStore.  \!strict fixes #8045
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.2/dojox@15632 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/XmlStore.js
+++ data/XmlStore.js
@@ -1122,12 +1122,11 @@ dojo.declare("dojox.data.XmlItem", null, {
 		// 	returns:
 		//		a value of the first text child of the element
 		var str = "";
-		if (this.element) {
-			for (var i = 0; i < this.element.childNodes.length; i++) {
+		if(this.element){
+			for(var i = 0; i < this.element.childNodes.length; i++){
 				var node = this.element.childNodes[i];
-				if (node.nodeType === 3 || node.nodeType === 4) {
-					str = node.nodeValue;
-					break;
+				if(node.nodeType === 3 || node.nodeType === 4){
+					str += node.nodeValue;
 				}
 			}
 		}
