4e00007b2e1d1b2cf9d1ca653b7393b89872f0a2
Heng Liu <liucougar@gmail.com>
2008-12-16 22:28:09 +0000

    typo
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@16096 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dtl/_HtmlTemplated.js
+++ dtl/_HtmlTemplated.js
@@ -27,7 +27,7 @@ dojox.dtl._HtmlTemplated = {
 			this.render();
 			this.domNode = this._template.getRootNode();
  			if(this.srcNodeRef && this.srcNodeRef.parentNode){
- 				dojo._destoryElement(this.srcNodeRef);
+ 				dojo._destroyElement(this.srcNodeRef);
  			}
 		},
 		preCreate: function(){
