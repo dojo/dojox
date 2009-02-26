e01c891a5ce3585e54315437e3dd8accb4fd79c3
Tom Trenka <ttrenka@gmail.com>
2009-02-26 17:57:34 +0000

    Added in a simple AES implementation; taken and modified from the AES implementation first ported by Brad Neuberg for dojox.sql.  It is not an efficient implementation nor will it (likely) play nicely with any other AES implementations; this makes it useful for storage purposes but little else.  !strict. Refs #1395.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@16835 560b804f-0ae3-0310-86f3-f6aa0a117693

--- encoding/tests/crypto/_base.js
+++ encoding/tests/crypto/_base.js
@@ -3,6 +3,7 @@ dojo.require("dojox.encoding.crypto.Blowfish");
 
 try{
 	dojo.require("dojox.encoding.tests.crypto.Blowfish");
+	dojo.require("dojox.encoding.tests.crypto.SimpleAES");
 }catch(e){
 	doh.debug(e);
 }
