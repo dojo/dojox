2c4670619884856ff36eca212f775f8aef6f04c9
Adam Peller <github@peller.org>
2009-04-02 02:28:42 +0000

    rename dojox.date modules in preparation for refactor. Refs #5074
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17170 560b804f-0ae3-0310-86f3-f6aa0a117693

--- date/tests/module.js
+++ date/tests/module.js
@@ -1,7 +1,7 @@
 dojo.provide("dojox.date.tests.module");
 
 try{
-	dojo.require("dojox.date.tests.HebrewDate");
+	dojo.require("dojox.date.tests.hebrew.Date");
 	dojo.require("dojox.date.tests.IslamicDate");
 	dojo.require("dojox.date.tests.posix");
 }catch(e){
