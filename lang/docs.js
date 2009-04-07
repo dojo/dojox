b7a07c3d2d772f2b11b0f354b7a6fa262889062b
Kris Zyp <kriszyp@gmail.com>
2009-04-07 23:32:12 +0000

    Even more gracefully handles missing api.xml file
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17252 560b804f-0ae3-0310-86f3-f6aa0a117693

--- lang/docs.js
+++ lang/docs.js
@@ -2,77 +2,82 @@ dojo.provide("dojox.lang.docs");
 
 // Extracts information from the API docs to apply a schema representation to dojo classes.
 // This can be utilized for runtime metadata retrieval and type checking
-dojo.xhrGet({
-	sync:true,
-	url: dojo.baseUrl + '../util/docscripts/api.xml',
-	handleAs: 'xml'
-}).addCallbacks(function(api){
-	// TODO: find the existing classes and patch them as well
-	var defaultDeclare = dojo.declare;
-	function tagValue(parent, tag){
-		var element = parent.getElementsByTagName(tag)[0];
-		return element && element.firstChild.nodeValue;
-	}
-	function getType(element, propertyDef){
-		var typeDef = element.getAttribute("type").toLowerCase();
-		typeDef = typeDef.replace(/\?/, function(){
-			propertyDef.optional = true;
-			return '';
-		});
-		return (typeDef == 'string' || typeDef == 'number' || typeDef == 'boolean' || typeDef == 'object' ||typeDef == 'array' || typeDef == 'integer') && 
-			typeDef;
-	}
-	dojo.declare = function(name){
-		var clazz = defaultDeclare.apply(this, arguments);
-		var docForClass = dojo.query("object[location='" + name + "']", api)[0];
-		if(docForClass){
-			clazz.description = tagValue(docForClass, "description");
-			// translate the properties to JSON Schema
-			clazz.properties = {};
-			var properties = docForClass.getElementsByTagName("property");
-			for(var i = 0; i < properties.length; i++){
-				var propertyDef = clazz.properties[properties[i].getAttribute("name")] = {};
-				var typeDef = getType(properties[i], propertyDef);
-				if(typeDef){ 
-					propertyDef.type = typeDef;
-				}
-				propertyDef.description = tagValue(properties[i], "description");
-			}
-			// translate the methods to JSON Schema
-			clazz.methods = {};
-			var methods = docForClass.getElementsByTagName("method");
-			for(i = 0; i < methods.length; i++){
-				name = methods[i].getAttribute("name");
-				if(name){
-					var methodDef = clazz.methods[name] = {};
-					methodDef.description = tagValue(methods[i], "description");
-					var parameters = methods[i].getElementsByTagName("parameter");
-					methodDef.parameters = [];
-					for(var j = 0; j < parameters.length; j++){
-						var paramElement = parameters[j];
-						methodDef.parameters[j] = {
-							name: paramElement.getAttribute("name"),
-							type: getType(paramElement, methodDef),
-							optional: "optional" == paramElement.getAttribute("usage")
-						};
-					}
-					var returnElement = methods[i].getElementsByTagName("return-type")[0];
-					if(returnElement){
-						methodDef.returns = { 
-							type: getType(returnElement, methodDef)
-						};
-					}
-				}
-			}
-			var superclass = docForClass.getAttribute("superclass");
-			if(superclass){
-				clazz["extends"] = dojo.getObject(superclass);
-			}
-		}
-		return clazz;
-	};
-	dojo.mixin(dojo.declare, defaultDeclare);
-}, function(error){
+dojox.lang.docs.error = function(error){
 	console.log("Warning, the API docs must be available at ../util/docscripts/api.xml "+ 
-	"in order for dojox.lang.docs to supply schema information, but it could not be loaded: " + e);
-});
+	"in order for dojox.lang.docs to supply schema information, but it could not be loaded: " + error);
+}
+try{
+	dojo.xhrGet({
+		sync:true,
+		url: dojo.baseUrl + '../util/docscripts/api.xml',
+		handleAs: 'xml'
+	}).addCallbacks(function(api){
+		// TODO: find the existing classes and patch them as well
+		var defaultDeclare = dojo.declare;
+		function tagValue(parent, tag){
+			var element = parent.getElementsByTagName(tag)[0];
+			return element && element.firstChild.nodeValue;
+		}
+		function getType(element, propertyDef){
+			var typeDef = element.getAttribute("type").toLowerCase();
+			typeDef = typeDef.replace(/\?/, function(){
+				propertyDef.optional = true;
+				return '';
+			});
+			return (typeDef == 'string' || typeDef == 'number' || typeDef == 'boolean' || typeDef == 'object' ||typeDef == 'array' || typeDef == 'integer') && 
+				typeDef;
+		}
+		dojo.declare = function(name){
+			var clazz = defaultDeclare.apply(this, arguments);
+			var docForClass = dojo.query("object[location='" + name + "']", api)[0];
+			if(docForClass){
+				clazz.description = tagValue(docForClass, "description");
+				// translate the properties to JSON Schema
+				clazz.properties = {};
+				var properties = docForClass.getElementsByTagName("property");
+				for(var i = 0; i < properties.length; i++){
+					var propertyDef = clazz.properties[properties[i].getAttribute("name")] = {};
+					var typeDef = getType(properties[i], propertyDef);
+					if(typeDef){ 
+						propertyDef.type = typeDef;
+					}
+					propertyDef.description = tagValue(properties[i], "description");
+				}
+				// translate the methods to JSON Schema
+				clazz.methods = {};
+				var methods = docForClass.getElementsByTagName("method");
+				for(i = 0; i < methods.length; i++){
+					name = methods[i].getAttribute("name");
+					if(name){
+						var methodDef = clazz.methods[name] = {};
+						methodDef.description = tagValue(methods[i], "description");
+						var parameters = methods[i].getElementsByTagName("parameter");
+						methodDef.parameters = [];
+						for(var j = 0; j < parameters.length; j++){
+							var paramElement = parameters[j];
+							methodDef.parameters[j] = {
+								name: paramElement.getAttribute("name"),
+								type: getType(paramElement, methodDef),
+								optional: "optional" == paramElement.getAttribute("usage")
+							};
+						}
+						var returnElement = methods[i].getElementsByTagName("return-type")[0];
+						if(returnElement){
+							methodDef.returns = { 
+								type: getType(returnElement, methodDef)
+							};
+						}
+					}
+				}
+				var superclass = docForClass.getAttribute("superclass");
+				if(superclass){
+					clazz["extends"] = dojo.getObject(superclass);
+				}
+			}
+			return clazz;
+		};
+		dojo.mixin(dojo.declare, defaultDeclare);
+	}, dojox.lang.docs.error);
+}catch(e){
+	dojox.lang.docs.error(e);
+}
