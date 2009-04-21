f7eb2eb56f7d77b0d18a0bebd8f20b08a3739a71
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-04-21 17:56:24 +0000

    Fixing regression caused by src being set to effectively a cross-domain file which causes the functions of the iframe to be inaccessible.  Also fixed an indent tabs/spaces problem.  fixes #9046
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17354 560b804f-0ae3-0310-86f3-f6aa0a117693

--- html/metrics.js
+++ html/metrics.js
@@ -101,18 +101,18 @@ dojo.provide("dojox.html.metrics");
 		fs.width = "5em";
 		fs.height = "10em";
 		fs.top = "-10000px";
-		f.src = dojo.config["dojoBlankHtmlUrl"] || dojo.moduleUrl("dojo", "resources/blank.html");
+		f.setAttribute("src", "javascript: '<html><body></body></html>'");
 		dojo.body().appendChild(f);
 
 		if(dojo.isIE){
 			f.onreadystatechange = function(){
 				if(f.contentWindow.document.readyState == "complete"){
-                    f.onresize = f.contentWindow.parent[dojox._scopeName].html.metrics._fontresize;
+					f.onresize = f.contentWindow.parent[dojox._scopeName].html.metrics._fontresize;
 				}
 			};
 		}else{
 			f.onload = function(){
-                f.contentWindow.onresize = f.contentWindow.parent[dojox._scopeName].html.metrics._fontresize;
+				f.contentWindow.onresize = f.contentWindow.parent[dojox._scopeName].html.metrics._fontresize;
 			};
 		}
 		dhm.initOnFontResize = function(){};
