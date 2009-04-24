59ce207648c00b2819d985daab57855afa56f997
Bryan Forbes <bryan@reigndropsfall.net>
2009-04-24 18:24:01 +0000

    * Changed dojox.lang.docs to check for broken up docs before loading api.json (fixes #9201 !strict). * Changed dojox.lang.docs to fall back to api.json rather than api.xml.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17419 560b804f-0ae3-0310-86f3-f6aa0a117693

--- lang/docs.js
+++ lang/docs.js
@@ -4,14 +4,94 @@ dojo.provide("dojox.lang.docs");
 // This can be utilized for runtime metadata retrieval and type checking
 (function(){
 	function error(error){
-		console.log("Warning, the API docs must be available at ../util/docscripts/api.xml "+ 
+		console.log("Warning, the API docs must be available at ../util/docscripts/api.json "+ 
+		"or ../util/docscripts/api/*.json "+
 		"in order for dojox.lang.docs to supply schema information, but it could not be loaded: " + error);
 	}
+
 	var declaredClasses = {};
+	var requiredModules = [];
+	var _docs = dojox.lang.docs._loadedDocs = {};
+
 	var schemifyClass = function(clazz, name){
 		// initial implementation records classes until they are ready
 		declaredClasses[name] = clazz;
-	}
+	};
+	var getType = function(type, propDef){
+		var typeDef = type || '';
+		typeDef = typeDef.toLowerCase();
+		typeDef = typeDef.replace(/\?/, function(){
+			propDef.optional = true;
+			return '';
+		});
+		if(typeDef.match(/ string/)){
+			// HTML String and other "types" of strings are really just strings
+			typeDef = "string";
+		}
+		return (typeDef == 'string' || typeDef == 'number' ||
+				typeDef == 'boolean' || typeDef == 'object' ||
+				typeDef == 'array' || typeDef == 'integer')
+			&& typeDef;
+	};
+	var actualSchemifyClass = function(clazz, name){
+		var docForClass = _docs[name];
+		if(docForClass){
+			clazz.description = docForClass.description;
+			clazz.properties = {};
+			clazz.methods = {};
+
+			if(docForClass.properties){
+				var props = docForClass.properties;
+				for(var i=0, l=props.length; i<l; i++){
+					var propDef = clazz.properties[props[i].name] = {};
+					var typeDef = getType(props[i].type, propDef);
+					if(typeDef){
+						propDef.type = typeDef;
+					}
+					propDef.description = props[i].summary;
+				}
+			}
+
+			// translate the methods to JSON Schema
+			if(docForClass.methods){
+				var methods = docForClass.methods;
+				for(i=0, l=methods.length; i<l; i++){
+					var name = methods[i].name;
+					if(name){
+						var methodDef = clazz.methods[name] = {};
+						methodDef.description = methods[i].summary;
+						var parameters = methods[i].parameters;
+						if(parameters){
+							methodDef.parameters = [];
+							for(var j=0, k=parameters.length; j<k; j++){
+								var param = parameters[j];
+								methodDef.parameters[j] = {
+									name: param.name,
+									type: getType(param.type, methodDef),
+									optional: "optional" == param.usage
+								};
+							}
+						}
+						var ret = methods[i]['return-types'];
+						if(ret){
+							methodDef.returns = {
+								type: getType(ret[0].type, methodDef)
+							};
+						}
+					}
+				}
+			}
+
+			var superclass = docForClass.superclass;
+			if(superclass){
+				clazz["extends"] = dojo.getObject(superclass);
+			}
+		}
+	};
+	var requireDocs = function(moduleName){
+		requiredModules.push(moduleName);
+	};
+
 	// hook into all declared classes
 	var defaultDeclare = dojo.declare;
 	dojo.declare = function(name){
@@ -21,6 +101,15 @@ dojo.provide("dojox.lang.docs");
 	};
 	dojo.mixin(dojo.declare, defaultDeclare);
 	var initialized;
+
+	// hook into dojo.require
+	var defaultRequire = dojo.require;
+	dojo.require = function(moduleName){
+		requireDocs(moduleName);
+		var module = defaultRequire.apply(this, arguments);
+		return module;
+	};
+
 	dojox.lang.docs.init = function(/*Boolean*/async){
 		// summary:
 		//		Loads the documentation and applies it to the previously defined classes 
@@ -33,83 +122,65 @@ dojo.provide("dojox.lang.docs");
 			return null;
 		}
 		initialized = true;
-		try{
+
+		var getSplitDocs = function(moduleName, sync){
 			dojo.xhrGet({
-				sync:!async,
-				url: dojo.baseUrl + '../util/docscripts/api.xml',
-				handleAs: 'xml'
-			}).addCallbacks(function(api){
-				function tagValue(parent, tag){
-					var element = parent.getElementsByTagName(tag)[0];
-					return element && element.firstChild.nodeValue;
-				}
-				function getType(element, propertyDef){
-					var typeDef = element.getAttribute("type").toLowerCase();
-					typeDef = typeDef.replace(/\?/, function(){
-						propertyDef.optional = true;
-						return '';
-					});
-					if(typeDef.match(/ string/)){
-						// HTML String and other "types" of strings are really just strings
-						typeDef = "string";
-					}
-					return (typeDef == 'string' || typeDef == 'number' || typeDef == 'boolean' || typeDef == 'object' ||typeDef == 'array' || typeDef == 'integer') && 
-						typeDef;
-				}
-				schemifyClass = function(clazz, name){
-					var docForClass = dojo.query("object[location='" + name + "']", api)[0];
-					if(docForClass){
-						clazz.description = tagValue(docForClass, "description");
-						// translate the properties to JSON Schema
-						clazz.properties = {};
-						var properties = docForClass.getElementsByTagName("property");
-						for(var i = 0; i < properties.length; i++){
-							var propertyDef = clazz.properties[properties[i].getAttribute("name")] = {};
-							var typeDef = getType(properties[i], propertyDef);
-							if(typeDef){ 
-								propertyDef.type = typeDef;
-							}
-							propertyDef.description = tagValue(properties[i], "description");
-						}
-						// translate the methods to JSON Schema
-						clazz.methods = {};
-						var methods = docForClass.getElementsByTagName("method");
-						for(i = 0; i < methods.length; i++){
-							name = methods[i].getAttribute("name");
-							if(name){
-								var methodDef = clazz.methods[name] = {};
-								methodDef.description = tagValue(methods[i], "description");
-								var parameters = methods[i].getElementsByTagName("parameter");
-								methodDef.parameters = [];
-								for(var j = 0; j < parameters.length; j++){
-									var paramElement = parameters[j];
-									methodDef.parameters[j] = {
-										name: paramElement.getAttribute("name"),
-										type: getType(paramElement, methodDef),
-										optional: "optional" == paramElement.getAttribute("usage")
-									};
-								}
-								var returnElement = methods[i].getElementsByTagName("return-type")[0];
-								if(returnElement){
-									methodDef.returns = { 
-										type: getType(returnElement, methodDef)
-									};
-								}
-							}
-						}
-						var superclass = docForClass.getAttribute("superclass");
-						if(superclass){
-							clazz["extends"] = dojo.getObject(superclass);
-						}
+				sync: sync||!async,
+				url: dojo.baseUrl + '../util/docscripts/api/' + moduleName + '.json',
+				handleAs: 'json'
+			}).addCallback(function(obj){
+				for(var clazz in obj){
+					if(!_docs[clazz]){
+						_docs[clazz] = obj[clazz];
 					}
 				}
-				for(var i in declaredClasses){
-					schemifyClass(declaredClasses[i], i);
+			});
+		};
+		var firstMod = requiredModules.shift();
+		try{
+			getSplitDocs(firstMod, true);
+
+			requireDocs = function(moduleName){
+				if(!_docs[moduleName]){
+					try{
+						getSplitDocs(moduleName);
+					}catch(e){
+						_docs[moduleName] = {};
+					}
 				}
-				declaredClasses = null;
-			}, error);
+			};
+			//console.log(requiredModules);
+			dojo.forEach(requiredModules, function(mod){
+				requireDocs(mod);
+			});
+			requiredModules = null;
+
+			schemifyClass = actualSchemifyClass;
+
+			for(i in declaredClasses){
+				schemifyClass(declaredClasses[i], i);
+			}
+			declaredClasses = null;
 		}catch(e){
-			error(e);
+			dojo.require = defaultRequire;
+			requiredModules = null;
+			try{
+				dojo.xhrGet({
+					sync:!async,
+					url: dojo.baseUrl + '../util/docscripts/api.json',
+					handleAs: 'json'
+				}).addCallbacks(function(obj){
+					_docs = obj;
+					schemifyClass = actualSchemifyClass;
+
+					for(var i in declaredClasses){
+						schemifyClass(declaredClasses[i], i);
+					}
+					declaredClasses = null;
+				}, error);
+			}catch(e){
+				error(e);
+			}
 		}
 		return null;
 	}
