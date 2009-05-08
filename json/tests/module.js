b929c6bcbf9658b3e3c7bdb7a3dbd09a844c3998
Kris Zyp <kriszyp@gmail.com>
2009-05-08 13:42:09 +0000

    Add all package unit tests to main unit test module
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17506 560b804f-0ae3-0310-86f3-f6aa0a117693

--- json/tests/module.js
+++ json/tests/module.js
@@ -2,6 +2,8 @@ dojo.provide("dojox.json.tests.module");
 
 try{
 	dojo.require("dojox.json.tests.ref");
+	dojo.require("dojox.json.tests.schema");
+	dojo.require("dojox.json.tests.query");
 }catch(e){
 	doh.debug(e);
 }
