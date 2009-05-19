3451ed10a179598f34ddacb65e6be7c7bbd82795
Adam Peller <github@peller.org>
2009-05-19 14:55:08 +0000

    Experimental buddhist date implementation, written by Aphichit Hanbanchong and Nattapong Sirilappanich (IBM, CCLA)  Fixes #9212  !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17540 560b804f-0ae3-0310-86f3-f6aa0a117693

--- date/tests/module.js
+++ date/tests/module.js
@@ -3,6 +3,7 @@ dojo.provide("dojox.date.tests.module");
 try{
 	dojo.require("dojox.date.tests.hebrew.Date");
 	dojo.require("dojox.date.tests.IslamicDate");
+	dojo.require("dojox.date.tests.buddhist.Date");
 	dojo.require("dojox.date.tests.posix");
 }catch(e){
 	doh.debug(e);
