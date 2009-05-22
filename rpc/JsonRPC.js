3893ed9eaa1c908e3b09489c38a137dde0047a06
Kris Zyp <kriszyp@gmail.com>
2009-05-22 00:11:44 +0000

    Didn't mean to commit this file
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17571 560b804f-0ae3-0310-86f3-f6aa0a117693

--- rpc/JsonRPC.js
+++ rpc/JsonRPC.js
@@ -5,6 +5,8 @@ dojo.require("dojox.rpc.Service");
 	function jsonRpcEnvelope(version){
 		return {
 			serialize: function(smd, method, data, options){
+				//not converted to json it self. This  will be done, if
+				//appropriate, at the transport level
 	
 				var d = {
 					id: this._requestId++,
@@ -50,7 +52,4 @@ dojo.require("dojox.rpc.Service");
 		},
 		jsonRpcEnvelope("2.0")
 	);
-	dojox.rpc.JsonRPC = function(){
-		return new dojox.rpc.Service({});
-	}
 })();
