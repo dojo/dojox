5d6c0110fbb9e71f2c230822e1af4fefb19b8dba
Nathan Toone <nathan@toonetown.com>
2009-06-05 14:37:25 +0000

    Update module.js to be a bit more clean moving forward.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17734 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/tests/performance/module.js
+++ grid/tests/performance/module.js
@@ -3,21 +3,20 @@ dojo.provide("dojox.grid.tests.performance.module");
 doh.register("dojox.grid.tests.performance.module", []);
 
 try{
-	var numRows = 100;
-	
-	// 5-minute timeout on each of these - since they can take quite a while...
-	doh.registerUrl("Grid Creation - " + numRows + " Rows, Single Layout", 
-		dojo.moduleUrl("dojox.grid.tests.performance", "creation.html") + "?rows=" + numRows + "&layout=single&rowSelector=false",
-		300000);
-	doh.registerUrl("Grid Creation - " + numRows + " Rows, Dual Layout", 
-		dojo.moduleUrl("dojox.grid.tests.performance", "creation.html") + "?rows=" + numRows + "&layout=dual&rowSelector=false",
-		300000);
-	doh.registerUrl("Grid Creation - " + numRows + " Rows, Single Layout w/ selector", 
-		dojo.moduleUrl("dojox.grid.tests.performance", "creation.html") + "?rows=" + numRows + "&layout=single&rowSelector=true",
-		300000);
-	doh.registerUrl("Grid Creation - " + numRows + " Rows, Dual Layout w/ selector", 
-		dojo.moduleUrl("dojox.grid.tests.performance", "creation.html") + "?rows=" + numRows + "&layout=dual&rowSelector=true",
-		300000);
+	var numRows = [100];
+	var layouts = ["Single", "Dual"];
+	var selectors = [false, true];
+	dojo.forEach(numRows, function(r){
+		dojo.forEach(layouts, function(l){
+			dojo.forEach(selectors, function(s){
+				// 5-minute timeout on each of these - since they can take quite a while...
+				doh.registerUrl("Grid Creation - " + r + " Rows, " + l + " Layout" + (s ? "w/ selector" : ""), 
+					dojo.moduleUrl("dojox.grid.tests.performance", "creation.html") + 
+									"?rows=" + r + "&layout=" + l.toLowerCase() + "&rowSelector=" + s,
+					300000);
+			});
+		});
+	});
 }catch(e){
 	doh.debug(e);
 }
