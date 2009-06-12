f218a04638d79107c39de3a61e0890b157f1e365
Nathan Toone <nathan@toonetown.com>
2009-06-12 00:08:41 +0000

    Refs #9392 - add in support for html markup in ellipsis-ized nodes
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17909 560b804f-0ae3-0310-86f3-f6aa0a117693

--- html/ellipsis.js
+++ html/ellipsis.js
@@ -31,12 +31,91 @@ dojo.require("dojo.behavior");
 			n.innerHTML = '';
 			n.appendChild(x);
 		};
+		
+		// Create our iframe elements for cloning later
+		var create = d.create;
+		var dd = d.doc;
+		var dp = d.place;
+		var iFrame = create("iframe", {className: "dojoxEllipsisIFrame",
+					src: "javascript:'<html><head><script>if(\"loadFirebugConsole\" in window){window.loadFirebugConsole();}</script></head><body></body></html>'"});
+		var rollRange = function(/* W3C Range */ r){
+			// Summary:
+			//		Rolls the given range back one character from the end
+			if(r.collapsed){
+				// Do nothing - we are already collapsed
+				return;
+			}
+			if(r.endContainer.nodeType == 3 && r.endOffset > 0){
+				r.setEnd(r.endContainer, r.endOffset - 1);
+			}else if(r.endContainer.nodeType == 3){
+				r.setEndBefore(r.endContainer);
+				rollRange(r);
+				return;
+			}else if(r.endOffset && r.endContainer.childNodes.length >= r.endOffset){
+				var nCont = r.endContainer.childNodes[r.endOffset - 1];
+				if(nCont.nodeType == 3){
+					r.setEnd(nCont, nCont.length - 1);
+				}else if(nCont.childNodes.length){
+					r.setEnd(nCont, nCont.childNodes.length);
+					rollRange(r);
+					return;
+				}else{
+					r.setEndBefore(nCont);
+					rollRange(r);
+					return;
+				}
+			}else{
+				r.setEndBefore(r.endContainer);
+				rollRange(r);
+				return;
+			}
+		};
+		var createIFrameEllipsis = function(/* Node */ n){
+			// Summary:
+			//		Given a node, it creates an iframe and and ellipsis div and
+			//		sets up the connections so that they will work correctly.
+			//		This function is used when createXULEllipsis is not able
+			//		to be used (because there is markup within the node) - it's
+			//		a bit slower, but does the trick
+			var c = create("div", {className: "dojoxEllipsisContainer"});
+			var e = create("div", {className: "dojoxEllipsisShown", style: {display: "none"}});
+			n.parentNode.replaceChild(c, n);
+			c.appendChild(n);
+			c.appendChild(e);
+			var i = iFrame.cloneNode(true);
+			var ns = n.style;
+			var es = e.style;
+			var ranges;
+			var resizeNode = function(){
+				ns.display = "";
+				es.display = "none";
+				if(n.scrollWidth <= n.offsetWidth){ return; }
+				var r = dd.createRange();
+				r.selectNodeContents(n);
+				ns.display = "none";
+				es.display = "";
+				do{
+					dp(r.cloneContents(), e, "only");
+					rollRange(r);
+				}while(!r.collapsed && e.scrollWidth > e.offsetWidth);
+			};
+			i.onload = function(){
+				i.contentWindow.onresize = resizeNode;
+				resizeNode();
+			};
+			c.appendChild(i);
+		};
 
 		// Add our behavior
 		var b = d.behavior;
 		b.add({
 			".dojoxEllipsis": function(n){
-				createXULEllipsis(n);
+				if(n.textContent == n.innerHTML){
+					// We can do the faster XUL version, instead of calculating
+					createXULEllipsis(n);
+				}else{
+					createIFrameEllipsis(n);
+				}
 			}
 		});
 		
@@ -48,7 +127,7 @@ dojo.require("dojo.behavior");
 			
 			// Connect to the modified function so that we can catch
 			// future changes
-			dojo.connect(dojo.body(), "DOMSubtreeModified", function(){
+			d.connect(d.body(), "DOMSubtreeModified", function(){
 				if(running){ 
 					// We are in the process of applying - so we just return
 					return;
