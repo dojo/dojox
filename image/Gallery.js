e3167e4e113d52607724f3763b06a9cae946899f
Shane O'Sullivan <shaneosullivan1@gmail.com>
2009-06-14 19:15:37 +0000

    Refs #7843 Fixes the issue where the Gallery calls a non-existent method "toggleSlideshow" on the SlideShow widget.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@17957 560b804f-0ae3-0310-86f3-f6aa0a117693

--- image/Gallery.js
+++ image/Gallery.js
@@ -19,53 +19,54 @@ dojo.declare("dojox.image.Gallery",
 	[dijit._Widget, dijit._Templated],
 	{
 	// summary:
-	//	Gallery widget that wraps a dojox.image.ThumbnailPicker and dojox.image.SlideShow widget
+	//		Gallery widget that wraps a dojox.image.ThumbnailPicker and dojox.image.SlideShow widget
 	//
 	// imageHeight: Number
-	//	Maximum height of an image in the SlideShow widget
+	//		Maximum height of an image in the SlideShow widget
 	imageHeight: 375,
 	
 	// imageWidth: Number
-	//	Maximum width of an image in the SlideShow widget
+	//		Maximum width of an image in the SlideShow widget
 	imageWidth: 500,
 		
 	// pageSize: Number
-	//	The number of records to retrieve from the data store per request.
+	//		The number of records to retrieve from the data store per request.
 	pageSize: dojox.image.SlideShow.prototype.pageSize,
 	
 	// autoLoad: Boolean
-	//	If true, images are loaded before the user views them. If false, an
-	//	image is loaded when the user displays it.
+	//		If true, images are loaded before the user views them. If false, an
+	//		image is loaded when the user displays it.
 	autoLoad: true,
 	
 	// linkAttr: String
-	//	Defines the name of the attribute to request from the store to retrieve the
-	//	URL to link to from an image, if any.
+	//		Defines the name of the attribute to request from the store to retrieve the
+	//		URL to link to from an image, if any.
 	linkAttr: "link",
 	
 	// imageThumbAttr: String
-	//	Defines the name of the attribute to request from the store to retrieve the
-	//	URL to the thumbnail image.
+	//		Defines the name of the attribute to request from the store to retrieve the
+	//		URL to the thumbnail image.
 	imageThumbAttr: "imageUrlThumb",
 	
 	// imageLargeAttr: String
-	//	Defines the name of the attribute to request from the store to retrieve the
-	//	URL to the image.
+	//		Defines the name of the attribute to request from the store to retrieve the
+	//		URL to the image.
 	imageLargeAttr: "imageUrl",
 	
 	// titleAttr: String
-	//	Defines the name of the attribute to request from the store to retrieve the
-	//	title of the picture, if any.
+	//		Defines the name of the attribute to request from the store to retrieve the
+	//		title of the picture, if any.
 	titleAttr: "title",
  
 	// slideshowInterval: Integer
-	//	time in seconds, between image changes in the slide show.
+	//		Time, in seconds, between image changes in the slide show.
 	slideshowInterval: 3,
 	
 	templatePath: dojo.moduleUrl("dojox.image", "resources/Gallery.html"), 
 
 	postCreate: function(){
-		// summary: Initializes the widget, creates the ThumbnailPicker and SlideShow widgets
+		// summary: 
+		//		Initializes the widget, creates the ThumbnailPicker and SlideShow widgets
 		this.widgetid = this.id;
 		this.inherited(arguments)
 		
@@ -94,12 +95,7 @@ dojo.declare("dojox.image.Gallery",
 		//When an image is shown in the Slideshow, make sure it is visible
 		//in the ThumbnailPicker
 		dojo.subscribe(this.slideShow.getShowTopicName(), function(packet){
-			//if(packet.index < _this.thumbPicker._thumbIndex
-			//   || packet.index > _this.thumbPicker._thumbIndex + _this.thumbPicker.numberThumbs -1){
-			//if(!_this.thumbPicker.isVisible(packet.index)){
-				//var index = packet.index - (packet.index % _this.thumbPicker.numberThumbs);
-				_this.thumbPicker._showThumbs(packet.index);
-			//}
+			_this.thumbPicker._showThumbs(packet.index);
 		});	
 		//When the user clicks a thumbnail, show that image
 		dojo.subscribe(this.thumbPicker.getClickTopicName(), function(evt){
@@ -119,52 +115,63 @@ dojo.declare("dojox.image.Gallery",
 	},
 	  
   	setDataStore: function(dataStore, request, /*optional*/paramNames){
-		// summary: Sets the data store and request objects to read data from.
+		// summary: 
+		//		Sets the data store and request objects to read data from.
 		// dataStore:
-		//	An implementation of the dojo.data.api.Read API. This accesses the image
-		//	data.
+		//		An implementation of the dojo.data.api.Read API. This accesses the image
+		//		data.
 		// request:
-		//	An implementation of the dojo.data.api.Request API. This specifies the
-		//	query and paging information to be used by the data store
+		//		An implementation of the dojo.data.api.Request API. This specifies the
+		//		query and paging information to be used by the data store
 		// paramNames:
-		//	An object defining the names of the item attributes to fetch from the
-		//	data store.  The four attributes allowed are 'linkAttr', 'imageLargeAttr',
-		//	'imageThumbAttr' and 'titleAttr'
+		//		An object defining the names of the item attributes to fetch from the
+		//		data store.  The four attributes allowed are 'linkAttr', 'imageLargeAttr',
+		//		'imageThumbAttr' and 'titleAttr'
 		this.thumbPicker.setDataStore(dataStore, request, paramNames);
 		this.slideShow.setDataStore(dataStore, request, paramNames);
   	},
   
   	reset: function(){
-		// summary: Resets the widget to its initial state
+		// summary:
+		//		Resets the widget to its initial state
 		this.slideShow.reset();
 		this.thumbPicker.reset();
   	},
   
 	showNextImage: function(inTimer){
-		// summary: Changes the image being displayed in the SlideShow to the next
-		//	image in the data store
+		// summary: 
+		//		Changes the image being displayed in the SlideShow to the next
+		//		image in the data store
 		// inTimer: Boolean
-		//	If true, a slideshow is active, otherwise the slideshow is inactive.
+		//		If true, a slideshow is active, otherwise the slideshow is inactive.
 		this.slideShow.showNextImage();
 	},
 
 	toggleSlideshow: function(){
-		// summary: Switches the slideshow mode on and off.
-		this.slideShow.toggleSlideshow();
+		dojo.deprecated("dojox.widget.Gallery.toggleSlideshow is deprecated.  Use toggleSlideShow instead.", "", "2.0");
+		this.toggleSlideShow();
+	},
+
+	toggleSlideShow: function(){
+		// summary:
+		//		Switches the slideshow mode on and off.
+		this.slideShow.toggleSlideShow();
 	},
 
 	showImage: function(index, /*optional*/callback){
-		// summary: Shows the image at index 'idx'.
+		// summary:
+		//		Shows the image at index 'idx'.
 		// idx: Number
-		//	The position of the image in the data store to display
+		//		The position of the image in the data store to display
 		// callback: Function
-		//	Optional callback function to call when the image has finished displaying.
+		//		Optional callback function to call when the image has finished displaying.
 		this.slideShow.showImage(index, callback);
 	},
 	
 	_centerChildren: function() {
-		// summary: Ensures that the ThumbnailPicker and the SlideShow widgets
-		//	are centered.
+		// summary: 
+		//		Ensures that the ThumbnailPicker and the SlideShow widgets
+		//		are centered.
 		var thumbSize = dojo.marginBox(this.thumbPicker.outerNode);
 		var slideSize = dojo.marginBox(this.slideShow.outerNode);
 		
