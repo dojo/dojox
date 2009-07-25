bc65abd8f62b87262864140c80fbbb01871bd429
Kris Zyp <kriszyp@gmail.com>
2009-07-25 14:16:01 +0000

    Revert back to separate onNew and onSet events (less code)
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@19097 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/JsonRestStore.js
+++ data/JsonRestStore.js
@@ -123,10 +123,10 @@ dojo.declare("dojox.data.JsonRestStore",
 			// wrap the service with so it goes through JsonRest manager 
 			this.service._store = this;
 			this.schema._idAttr = this.idAttribute;
-			var constructor = this._rawConstructor = dojox.rpc.JsonRest.getConstructor(this.service);
+			var constructor = dojox.rpc.JsonRest.getConstructor(this.service);
 			var self = this;
-			this._constructor = function(){
-				constructor.apply(this, arguments);
+			this._constructor = function(data){
+				constructor.call(this, data);
 				self.onNew(this);
 			}
 			this._constructor.prototype = constructor.prototype;
@@ -147,16 +147,15 @@ dojo.declare("dojox.data.JsonRestStore",
 			//
 			//	data: /* object */
 			//		The data to be added in as an item.
-			data = new this._rawConstructor(data);
+			data = new this._constructor(data);
 			if(parentInfo){
 				// get the previous value or any empty array
-				var values = parentInfo.oldValue = 
-					this.getValue(parentInfo.item = parentInfo.parent, parentInfo.attribute, []);
+				var values = this.getValue(parentInfo.parent,parentInfo.attribute,[]);
 				// set the new value
-				values = data.__parent = parentInfo.newValue = values.concat([data]);
+				values = values.concat([data]);
+				data.__parent = values;
 				this.setValue(parentInfo.parent, parentInfo.attribute, values);
 			}
-			this.onNew(data, parentInfo);
 			return data;
 		},
 		deleteItem: function(item){
