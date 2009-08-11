a39fe76197597cef73b78785e32eb720a547fd14
Heng Liu <liucougar@gmail.com>
2009-08-11 19:21:02 +0000

    changed sketch.slider minimal from 20% to 10% fixed slider resetting problem when sketch reloads
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@19742 560b804f-0ae3-0310-86f3-f6aa0a117693

--- sketch/Slider.js
+++ sketch/Slider.js
@@ -4,7 +4,7 @@ dojo.require("dijit.form.HorizontalSlider");
 
 dojo.declare("dojox.sketch.Slider",dojox.sketch._Plugin,{
 	_initButton: function(){
-		this.slider=new dijit.form.HorizontalSlider({minimum:20,maximum:200,style:"width:200px;float:right"});
+		this.slider=new dijit.form.HorizontalSlider({minimum:10,maximum:200,style:"width:200px;float:right"});
 		this.slider._movable.node.title='Double Click to "Zoom to Fit"'; //I18N
 		this.connect(this.slider,'onChange','_setZoom');
 		this.connect(this.slider.sliderHandle,'ondblclick','_zoomToFit');
@@ -14,15 +14,20 @@ dojo.declare("dojox.sketch.Slider",dojox.sketch._Plugin,{
 		this.slider.attr('value',this.slider.maximum<r?this.slider.maximum:(this.slider.minimum>r?this.slider.minimum:r));
 	},
 	_setZoom: function(v){
-		if(this.figure){
+		if(v && this.figure){
 			this.figure.zoom(v);
 		}
+	},
+	reset: function(){
+		//reset slider to NaN so that onChange will be fired no matter what value is set in _zoomToFit
+		this.slider.attr('value',NaN);
+		this._zoomToFit();
 	},
 	setToolbar: function(t){
 		t.addChild(this.slider);
 		if(!t._reset2Zoom){
 			t._reset2Zoom=true;
-			this.connect(t,'reset','_zoomToFit');
+			this.connect(t,'reset','reset');
 			if(t.figure && t.figure.surface){
 				//already loaded, do a _zoomToFit
 				this._zoomToFit();
