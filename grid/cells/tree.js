f793c473afed87c8a35b5b4ec7b78dcee026f7ac
Bryan Forbes <bryan@reigndropsfall.net>
2009-08-13 13:47:46 +0000

    * Added the cell to the argument list of the tree cell formatter. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@19837 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/cells/tree.js
+++ grid/cells/tree.js
@@ -6,7 +6,7 @@ dojox.grid.cells.TreeCell = {
 	formatAggregate: function(inItem, level, inRowIndexes){
 		var f, g=this.grid, i=g.edit.info, 
 			d=g.aggregator ? g.aggregator.getForCell(this, level, inItem, level === this.level ? "cnt" : this.parentCell.aggregate) : (this.value || this.defaultValue);
-		return this._defaultFormat(d, [d, level - this.level, inRowIndexes]);
+		return this._defaultFormat(d, [d, level - this.level, inRowIndexes, this]);
 	},
 	formatIndexes: function(inRowIndexes, inItem){
 		var f, g=this.grid, i=g.edit.info, 
@@ -14,7 +14,7 @@ dojox.grid.cells.TreeCell = {
 		if(this.editable && (this.alwaysEditing || (i.rowIndex==inRowIndexes[0] && i.cell==this))){
 			return this.formatEditing(d, inRowIndexes[0], inRowIndexes);
 		}else{
-			return this._defaultFormat(d, [d, inRowIndexes[0], inRowIndexes]);
+			return this._defaultFormat(d, [d, inRowIndexes[0], inRowIndexes, this]);
 		}
 	},
 	getOpenState: function(itemId){
