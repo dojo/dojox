8ef3aee2f0688e9ba8815509bb75d6e6017ceeda
Bryan Forbes <bryan@reigndropsfall.net>
2009-09-08 22:37:54 +0000

    * Changed dojo.cache back to dojo.moduleUrl until we're ready to release 1.4 for backwards compatibility. !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@20056 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/_View.js
+++ grid/_View.js
@@ -30,7 +30,7 @@ dojo.require("dojo.dnd.Manager");
 		// 		Width for the view, in valid css unit
 		viewWidth: "",
 
-		templateString: dojo.cache("dojox.grid","resources/View.html"),
+		templatePath: dojo.moduleUrl("dojox.grid","resources/View.html"),
 		
 		themeable: false,
 		classTag: 'dojoxGrid',
