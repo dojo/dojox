bb4300f2a14c9b16f5966eaab2fd9b313b5872ee
Bill Keese <bill@dojotoolkit.org>
2009-09-18 05:58:30 +0000

    Rename DropDownSelect to Select, to match the term used in native HTML (although admittedly the native HTML <select> has a multiple=true option, which we've implemented as another widget: MultiSelect/CheckedMultiSelect).  Refs #9356 !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@20139 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/DropDownSelect.js
+++ form/DropDownSelect.js
@@ -1,6 +1,6 @@
-dojo.deprecated("dojox.form.DropDownSelect", "Use dijit.form.DropDownSelect instead", "2.0");
+dojo.deprecated("dojox.form.DropDownSelect", "Use dijit.form.Select instead", "2.0");
 
 dojo.provide("dojox.form.DropDownSelect");
-dojo.require("dijit.form.DropDownSelect");
+dojo.require("dijit.form.Select");
 
-dojo.setObject("dojox.form.DropDownSelect", dijit.form.DropDownSelect);
+dojo.setObject("dojox.form.DropDownSelect", dijit.form.Select);
--- form/DropDownStack.js
+++ form/DropDownStack.js
@@ -1,10 +1,10 @@
 dojo.provide("dojox.form.DropDownStack");
 
-dojo.require("dijit.form.DropDownSelect");
+dojo.require("dijit.form.Select");
 dojo.require("dojox.form._SelectStackMixin");
 
 dojo.declare("dojox.form.DropDownStack",
-	[ dijit.form.DropDownSelect, dojox.form._SelectStackMixin ], {
+	[ dijit.form.Select, dojox.form._SelectStackMixin ], {
 	// summary: A dropdown-based select stack.
 	
 });
