9aef047abfe213f9f3940f2473c18613d278d7cd
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-09-22 17:34:23 +0000

    Adding in validators for brazilian CNPJ and CPF.  fixes #9940
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@20199 560b804f-0ae3-0310-86f3-f6aa0a117693

--- validate/tests/module.js
+++ validate/tests/module.js
@@ -3,6 +3,7 @@ dojo.provide("dojox.validate.tests.module");
 try{
 	dojo.require("dojox.validate.tests.creditcard");
 	dojo.require("dojox.validate.tests.validate"); 
+	dojo.require("dojox.validate.tests.br"); 
 
 }catch(e){
 	doh.debug(e);
