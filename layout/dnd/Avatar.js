81c1594b26e5d19a72f799172850d37eb2206ff7
Peter E Higgins <dante@dojotoolkit.org>
2009-11-05 03:40:33 +0000

    fixes #10256 applies patch [cla] from moogle, small cleanup here. GridContainer seems slightly broken atm overall, but there are other patches to review in this series so applying this seemingly harmless/low delta cleanup patch. thanks moogle!
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@20719 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/dnd/Avatar.js
+++ layout/dnd/Avatar.js
@@ -1,48 +1,46 @@
 dojo.provide("dojox.layout.dnd.Avatar");
+
+dojo.require("dojo.dnd.Avatar");
 dojo.require("dojo.dnd.common");
 
-dojox.layout.dnd.Avatar = function(manager,opacity){
-	// summary: 
-	// 		An Object, which represents the object being moved in a GridContainer
-	this.manager = manager;
-	this.construct(opacity);
-};
+dojo.declare("dojox.layout.dnd.Avatar", dojo.dnd.Avatar, {
+	// summary:
+	//      An Object, which represents the object being moved in a GridContainer
+	constructor: function(manager, opacity){
+		this.opacity = opacity || 0.9;
+	},
 
-dojo.extend(dojox.layout.dnd.Avatar, {
-	construct: function(/*int*/ opacity){
+	construct: function(){
 		// summary:
-		//		A constructor function. it is separate so it can be (dynamically) 
+		//		A constructor function. it is separate so it can be (dynamically)
 		//		overwritten in case of need.
 		
-		var source = this.manager.source;
-		var node = (source.creator)?
-		// create an avatar representation of the node
-		source._normalizedCreator(source.getItem(this.manager.nodes[0].id).data, "avatar").node :
-		// or just clone the node and hope it works
-		this.manager.nodes[0].cloneNode(true); 
-		node.id = dojo.dnd.getUniqueId();
+		var source = this.manager.source,
+			node = source.creator ?
+				// create an avatar representation of the node
+				source._normalizedCreator(source.getItem(this.manager.nodes[0].id).data, "avatar").node :
+				// or just clone the node and hope it works
+				this.manager.nodes[0].cloneNode(true)
+		;
+
 		dojo.addClass(node, "dojoDndAvatar");
+		
+		node.id = dojo.dnd.getUniqueId();
 		node.style.position = "absolute";
 		node.style.zIndex = 1999;
-		node.style.margin = "0px";
-		node.style.width = dojo.marginBox(source.node).w + "px";
-		dojo.style(node,"opacity",opacity);
+		node.style.margin = "0px"
+		node.style.width = dojo.marginBox(source.node).w + "px"
+		
+		// add contructor object params to define it
+		dojo.style(node, "opacity", this.opacity);
 		this.node = node;
 	},
-	destroy: function(){
-		// summary: Destroy this avatar instance
-		dojo.destroy(this.node);
-		this.node = false;
-	},
-	/*Function: update
-		Updates the avatar to reflect the current DnD state.*/
+
 	update: function(){
-		
-		dojo[(this.manager.canDropFlag ? "add" : "remove") + "Class"](this.node, "dojoDndAvatarCanDrop");
+		// summary: Updates the avatar to reflect the current DnD state.
+		dojo.toggleClass(this.node, "dojoDndAvatarCanDrop", this.manager.canDropFlag);
 	},
-	/*Function: _generateText*/
-	_generateText: function(){
-		//Nothing to do
-	}
-	
+
+	_generateText: function(){ /* nada. */ }
+
 });
