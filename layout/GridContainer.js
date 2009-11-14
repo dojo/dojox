2c07fe27d024691336cfdacddb97e05efe72ba1e
Peter E Higgins <dante@dojotoolkit.org>
2009-11-14 13:29:43 +0000

    fixes #10294 - getZones simplification. thanks Moogle! \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@20804 560b804f-0ae3-0310-86f3-f6aa0a117693

--- layout/GridContainer.js
+++ layout/GridContainer.js
@@ -127,9 +127,7 @@ dojo.declare("dojox.layout.GridContainer",
 			this.gridNode.appendChild(space);
 		}
 
-		this.cell = [];
-		var i = 0;
-		while(i < this.nbZones){
+		for(var i = 0; i < this.nbZones; i++){
 			var node = dojo.create("td", {
 				id: this.id + "_dz" + i,
 				className: "gridContainerZone",
@@ -137,9 +135,6 @@ dojo.declare("dojox.layout.GridContainer",
 					width: this._getColWidth(i) + "%"
 				}
 			}, this.gridNode);
-
-			this.cell[i] = node;
-			i++;
 		}
 	},
 	startup:function(){
@@ -183,7 +178,11 @@ dojo.declare("dojox.layout.GridContainer",
 			child.resize && child.resize();
 		});
 	},
-
+	getZones: function(){
+		// summary:
+		//   return array of zone (domNode) 
+		return dojo.query(".gridContainerZone",  this.containerNode);
+	},
 	getNewChildren: function(){
 		//TODO call Container getChildren()
 		return dojo.query("> [widgetId]",  this.containerNode).map(dijit.byNode);
@@ -253,7 +252,7 @@ dojo.declare("dojox.layout.GridContainer",
 		// first:
 
 		if(typeof(service) == "undefined" )return;
-		var zone = this.cell[z];
+		var zone = this.getZones()[z];
 
 		var kidsZone = zone.childNodes.length;
 		if(typeof(p) == "undefined" || p > kidsZone){ p = kidsZone; }
@@ -320,7 +319,7 @@ dojo.declare("dojox.layout.GridContainer",
 		var grid = [];
 		var i = 0;
 		while(i < this.nbZones){
-			var plottedZone = this._createZone(this.cell[i]);
+			var plottedZone = this._createZone(this.getZones()[i]);
 			if(this.hasResizableColumns && i != (this.nbZones-1)){
 				this._createGrip(plottedZone);
 			}
@@ -725,24 +724,20 @@ dojo.declare("dojox.layout.GridContainer",
 					this.grid[this.grid.length-1].node.parentNode.insertBefore(node,this.grid[this.grid.length-1].node);
 					dz = this._createZone(node);
 					this.grid.splice(this.grid.length-1,0,dz);
-					this.cell.splice(this.cell.length-1,0,node);
 				}else{
 					var zone = this.gridNode.appendChild(node);
 					dz = this._createZone(node);
 					this.grid.push(dz);
-					this.cell.push(node);
 				}
 			}else{
 				if(this.isLeftFixed){
 					(this.grid.length == 1) ? this.grid[0].node.parentNode.appendChild(node,this.grid[0].node) : this.grid[1].node.parentNode.insertBefore(node,this.grid[1].node);
 					dz = this._createZone(node);
 					this.grid.splice(1,0,dz);
-					this.cell.splice(1,0,node);
 				}else{
 					this.grid[this.grid.length-this.nbZones].node.parentNode.insertBefore(node,this.grid[this.grid.length-this.nbZones].node);
 					dz = this._createZone(node);
 					this.grid.splice(this.grid.length-this.nbZones,0,dz);
-					this.cell.splice(this.cell.length-this.nbZones,0,node);
 				}
 			}
 			if(this.hasResizableColumns){
@@ -808,13 +803,11 @@ dojo.declare("dojox.layout.GridContainer",
 					dojo.disconnect(this.handleDndStart[idx]);
 				}
 				this.grid.splice(idx, 1);
-				this.cell.splice(idx, 1);
 			}else{
 				if(this.hasResizableColumns){
 					dojo.disconnect(this.handleDndStart[idx - nbDelZones]);
 				}
 				this.grid.splice(idx - nbDelZones, 1);
-				this.cell.splice(idx - nbDelZones, 1);
 			}
 			this.nbZones--;
 			nbDelZones++;
