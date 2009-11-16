440bb2d1ac4bb7aef897521a0a79d76f919cf8fc
Kris Zyp <kriszyp@gmail.com>
2009-11-16 16:24:12 +0000

    Fixes simplified query option in JsonQuery module and adds docs
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@20825 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/util/JsonQuery.js
+++ data/util/JsonQuery.js
@@ -2,7 +2,13 @@ dojo.provide("dojox.data.util.JsonQuery");
 // this is a mixin to convert object attribute queries to 
 // JSONQuery/JSONPath syntax to be sent to the server.
 dojo.declare("dojox.data.util.JsonQuery", null, {
+	// summary:
+	// 		Indicates that full paths should be used when referencing other objects in filters 
 	useFullIdInQueries: false,
+	// summary:
+	// 		Indicates that simplified queries should be generated. Simplified queries are of the form:
+	//		?name='value'  (instead of [?(@.name='value')])
+	simplifiedQuery: false,
 	_toJsonQuery: function(args, jsonQueryPagination){
 		var first = true;
 		var self = this;
@@ -17,7 +23,9 @@ dojo.declare("dojox.data.util.JsonQuery", null, {
 			for(var i in query){
 				// iterate through each property, adding them to the overall query
 				var value = query[i];
-				var newPath = path + (/^[a-zA-Z_][\w_]*$/.test(i) ? '.' + i : '[' + dojo._escapeString(i) + ']');
+				var newPath = self.simplifiedQuery ?
+					((path ? "." : "") + encodeURIComponent(i)) :
+					(path + (/^[a-zA-Z_][\w_]*$/.test(i) ? '.' + i : '[' + dojo._escapeString(i) + ']'));
 				if(value && typeof value == "object"){
 					buildQuery(newPath, value);
 				}else if(value!="*"){ // full wildcards can be ommitted
@@ -31,15 +39,16 @@ dojo.declare("dojox.data.util.JsonQuery", null, {
 		// performs conversion of Dojo Data query objects and sort arrays to JSONQuery strings
 		if(args.query && typeof args.query == "object"){
 			// convert Dojo Data query objects to JSONQuery
-			var jsonQuery = "[?(";
-			buildQuery("@", args.query);
+			var jsonQuery = self.simplifiedQuery ? "" : "[?(";
+			buildQuery((self.simplifiedQuery ? "" : "@"), args.query);
 			if(!first){
 				// use ' instead of " for quoting in JSONQuery, and end with ]
-				jsonQuery += ")]"; 
+				jsonQuery += self.simplifiedQuery ? "" : ")]"; 
 			}else{
 				jsonQuery = "";
 			}
-			args.queryStr = jsonQuery.replace(/\\"|"/g,function(t){return t == '"' ? "'" : t;});
+			args.queryStr = (self.simplifiedQuery ? "?" : "") + 
+						jsonQuery.replace(/\\"|"/g,function(t){return t == '"' ? "'" : t;});
 		}else if(!args.query || args.query == '*'){
 			args.query = "";
 		}
@@ -47,10 +56,10 @@ dojo.declare("dojox.data.util.JsonQuery", null, {
 		var sort = args.sort;
 		if(sort){
 			// if we have a sort order, add that to the JSONQuery expression
-			args.queryStr = args.queryStr || (typeof args.query == 'string' ? args.query : ""); 
+			args.queryStr = args.queryStr || (typeof args.query == 'string' ? args.query : (self.simplifiedQuery ? "?" : "")); 
 			first = true;
 			for(i = 0; i < sort.length; i++){
-				args.queryStr += (first ? '[' : ',') + (sort[i].descending ? '\\' : '/') + "@[" + dojo._escapeString(sort[i].attribute) + "]";
+				args.queryStr += (first ? '[' : ',') + (sort[i].descending ? '\\' : '/') + self.simplifiedQuery ? encodeURIComponent(sort[i].attribute) : ("@[" + dojo._escapeString(sort[i].attribute) + "]");
 				first = false; 
 			}
 			if(!first){
@@ -60,7 +69,7 @@ dojo.declare("dojox.data.util.JsonQuery", null, {
 		// this is optional because with client side paging JSONQuery doesn't yield the total count
 		if(jsonQueryPagination && (args.start || args.count)){
 			// pagination
-			args.queryStr = (args.queryStr || (typeof args.query == 'string' ? args.query : "")) +
+			args.queryStr = (args.queryStr || (typeof args.query == 'string' ? args.query : (self.simplifiedQuery ? "?" : ""))) +
 				'[' + (args.start || '') + ':' + (args.count ? (args.start || 0) + args.count : '') + ']'; 
 		}
 		if(typeof args.queryStr == 'string'){
