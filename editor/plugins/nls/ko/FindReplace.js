ae3c0d0bfa3ce2a9ff0664d9150dbd00d5f742d6
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-12-08 21:03:29 +0000

    Adding translations to 1.4.  refs #10413
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dojox@21007 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ editor/plugins/nls/ko/FindReplace.js
@@ -0,0 +1,11 @@
+({
+	"findLabel": "찾을 내용:",
+	"replaceLabel": "바꿀 내용:",
+	"findReplace": "찾기/바꾸기 토글",
+	"matchCase": "대소문자 구분", 
+	"backwards": "뒷방향으로",
+	"replaceAll": "모두 바꾸기", 
+	"findButton": "찾기",
+	"replaceButton": "바꾸기",
+	"replaceDialogText": "${0} 개의 내용이 변경되었습니다."
+})
