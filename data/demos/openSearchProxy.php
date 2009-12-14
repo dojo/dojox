2c9cedfec8df53a503caf5133a8ceb79e6785e93
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-12-14 18:47:52 +0000

    Fix for potential reflected XSS in some dojox test php files.  Only affects php servers and deployments of dojo that include the tests.  Fix is escapement of user input to avoid script tag insertion.  Also limits opensearch demo php file to redirect only to noted services.  refs #10478
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dojox@21038 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/demos/openSearchProxy.php
+++ data/demos/openSearchProxy.php
@@ -4,20 +4,30 @@
 if(!$_GET['url']){ return; }
 
 $url = str_replace(array(';;;;', '%%%%'), array('?', '&'), $_GET['url']);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$results = curl_exec($ch);
-header('HTTP/1.1 ' . curl_getinfo($ch, CURLINFO_HTTP_CODE) . ' OK');
-if($_GET['osd'] === 'true'){
-	$xml = new SimpleXMLElement($results);
-	if($xml->Url){
-		foreach($xml->Url as $url){
-			$url['template'] = $_SERVER['SCRIPT_NAME'].'?url='.str_replace(array('?', '&'), array(';;;;', '%%%%'), $url['template']);
+if(stripos($url, "http://intertwingly.net/") === 0 || 
+	stripos($url, "http://www.intertwingly.net/") === 0 || 
+	stripos($url, "http://www.shutterpoint.com/") === 0 || 
+	stripos($url, "http://technorati.com/") === 0){
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$results = curl_exec($ch);
+	header('HTTP/1.1 ' . curl_getinfo($ch, CURLINFO_HTTP_CODE) . ' OK');
+	if($_GET['osd'] === 'true'){
+		$xml = new SimpleXMLElement($results);
+		if($xml->Url){
+			foreach($xml->Url as $url){
+				$url['template'] = $_SERVER['SCRIPT_NAME'].'?url='.str_replace(array('?', '&'), array(';;;;', '%%%%'), $url['template']);
+			}
+			header('Content-Type: text/xml');
+			print $xml->asXML();
 		}
-		header('Content-Type: text/xml');
-		print $xml->asXML();
+	}else{
+		header('Content-Type: '.curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
+		print $results;
 	}
 }else{
-	header('Content-Type: '.curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
-	print $results;
+	header("HTTP/1.0 403 Forbidden");
+	header("Status: 403 Forbidden");
+	print "Provided URL not allowed by this demo proxy.";
 }
+
