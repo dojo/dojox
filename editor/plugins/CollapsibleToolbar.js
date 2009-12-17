2d9a57e3479d5295161187c6675131bd0a2697d0
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2009-12-17 17:50:34 +0000

    Worked on A11y compliance, including high contrast mode and click/keyoard handling refs #10523
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@21067 560b804f-0ae3-0310-86f3-f6aa0a117693

--- editor/plugins/CollapsibleToolbar.js
+++ editor/plugins/CollapsibleToolbar.js
@@ -1,8 +1,59 @@
 dojo.provide("dojox.editor.plugins.CollapsibleToolbar");
 
+dojo.require("dijit._Widget")
+dojo.require("dijit._Templated");
 dojo.require("dijit._editor._Plugin");
 
 dojo.requireLocalization("dojox.editor.plugins", "CollapsibleToolbar");
+
+dojo.declare("dojox.editor.plugins._CollapsibleToolbarButton", [dijit._Widget, dijit._Templated], {
+	// summary:
+	//		Simple internal widget for representing a clickable button for expand/collapse
+	//		with A11Y support.
+	// tags:
+	//		private
+	templateString: "<div tabindex='0'roll='button'></div>",
+
+	// title [public] String
+	//		The text to read by a screen reader that gets button focus.
+	title: "",
+
+	// buttonClass [public] String
+	//		The classname to apply to the expand/collapse button.
+	buttonClass: "",
+
+	// text [public] String
+	//		The text to use as expand/collapse in A11Y mode.
+	text: "",
+	
+	// textClass [public] String
+	//		The classname to apply to the expand/collapse text.
+	textClass: "",
+
+	postCreate: function(){
+		// summary:
+		//		Over-ride of post create to attach in all the titles, text, etc.
+		this.connect(this.domNode, "ondijitclick", "onClick");
+		if(this.title){
+			dojo.attr(this.domNode, "title", this.title);
+		}
+		if(this.buttonClass){
+			dojo.addClass(this.domNode, this.buttonClass);
+		}
+		if(this.text){
+			var tn = dojo.create("span", {innerHTML: this.text}, this.domNode);
+			if(this.textClass){
+				dojo.addClass(tn, this.textClass); 
+			}
+		}
+	},
+
+	onClick: function(e){
+		// summary:
+		//		Simple synthetic event to listen for dijit click events (mouse or keyboard)
+	}
+});
+
 
 dojo.declare("dojox.editor.plugins.CollapsibleToolbar",dijit._editor._Plugin,{
 	// summary:
@@ -37,17 +88,20 @@ dojo.declare("dojox.editor.plugins.CollapsibleToolbar",dijit._editor._Plugin,{
 		var menuTd = dojo.create("td", {style: { width: "100%" }, tabindex: -1}, row);
 		var m = dojo.create("span", {style: { width: "100%" }, tabindex: -1}, menuTd);
 
-		// Put in actual clickable containers in the open/close holders.
-		openTd.appendChild(dojo.create("div", {
-			"class": "dojoxCollapsibleToolbarCollapse", 
-			tabindex: "0", 
-			role: "button", 
-			title: strings.collapse}));
-		closeTd.appendChild(dojo.create("div", {
-			"class": "dojoxCollapsibleToolbarExpand", 
-			tabindex: "0", 
-			role: "button", 
-			title: strings.expand}));
+		var collapseButton = new dojox.editor.plugins._CollapsibleToolbarButton({
+			buttonClass: "dojoxCollapsibleToolbarCollapse",
+			title: strings.collapse,
+			text: "-",
+			textClass: "dojoxCollapsibleToolbarCollapseText"
+		});
+		dojo.place(collapseButton.domNode, openTd);
+		var expandButton = new dojox.editor.plugins._CollapsibleToolbarButton({
+			buttonClass: "dojoxCollapsibleToolbarExpand",
+			title: strings.expand,
+			text: "+",
+			textClass: "dojoxCollapsibleToolbarExpandText"
+		});
+		dojo.place(expandButton.domNode, closeTd);
 
 		// Attach everything in now.
 		dojo.style(closeTd, "display", "none");
@@ -59,76 +113,41 @@ dojo.declare("dojox.editor.plugins.CollapsibleToolbar",dijit._editor._Plugin,{
 		this.menu = m;
 
 		// Establish the events to handle open/close.
-		this.connect(openTd.firstChild, "onclick", "_onClose");
-		this.connect(closeTd.firstChild, "onclick", "_onOpen");
-		this.connect(openTd.firstChild, "onkeydown", "_keyClose");
-		this.connect(closeTd.firstChild, "onkeydown", "_keyOpen");
+		this.connect(collapseButton, "onClick", "_onClose");
+		this.connect(expandButton, "onClick", "_onOpen");
 
 		// Set up some focus handlers so hilighting appears on IE.  Focus box needed 
 		// to be A11Y compliant.
 		if(dojo.isIE){
-			this.connect(openTd.firstChild, "onfocus", function(){
-				dojo.addClass(openTd.firstChild, "dojoxCollapsibleToolbarButtonFocus");
+			this.connect(collapseButton.domNode, "onfocus", function(){
+				dojo.addClass(collapseButton.domNode, "dojoxCollapsibleToolbarButtonFocus");
 			});
-			this.connect(openTd.firstChild, "onblur", function(){
-				dojo.removeClass(openTd.firstChild, "dojoxCollapsibleToolbarButtonFocus");
+			this.connect(collapseButton.domNode, "onblur", function(){
+				dojo.removeClass(collapseButton.domNode, "dojoxCollapsibleToolbarButtonFocus");
 			});
-			this.connect(closeTd.firstChild, "onfocus", function(){
-				dojo.addClass(closeTd.firstChild, "dojoxCollapsibleToolbarButtonFocus");
+			this.connect(expandButton.domNode, "onfocus", function(){
+				dojo.addClass(expandButton.domNode, "dojoxCollapsibleToolbarButtonFocus");
 			});
-			this.connect(closeTd.firstChild, "onblur", function(){
-				dojo.removeClass(closeTd.firstChild, "dojoxCollapsibleToolbarButtonFocus");
+			this.connect(expandButton.domNode, "onblur", function(){
+				dojo.removeClass(expandButton.domNode, "dojoxCollapsibleToolbarButtonFocus");
 			});
-		}
-	},
-
-	_keyOpen: function(e){
-		// summary:
-		//		Internal function for handling a key event that will open the toolbar.
-		// e:
-		//		The key event.
-		// tags:
-		//		private
-		if(e.keyCode == dojo.keys.SPACE || e.keyCode == dojo.keys.ENTER){
-			dojo.stopEvent(e);
-			this._onOpen();
-			// To be IE safe, focus outside of event handler.
-			setTimeout(dojo.hitch(this, function(){
-				dijit.focus(this.openTd.firstChild);
-			}), 0);
-		}
-	},
-
-	_keyClose: function(e){
-		// summary:
-		//		Internal function for handling a key event that will close the toolbar.
-		// e:
-		//		The key event.
-		// tags:
-		//		private
-		if(e.keyCode == dojo.keys.SPACE || e.keyCode == dojo.keys.ENTER){
-			dojo.stopEvent(e);
-			this._onClose();
-			// To be IE safe, focus outside of event handler.
-			setTimeout(dojo.hitch(this, function(){
-				dijit.focus(this.closeTd.firstChild);
-			}), 0);
 		}
 	},
 
 	_onClose: function(e){
-		 // summary:
-		 //		Internal function for handling a click event that will close the toolbar.
-		 // e:
-		 //		The click event.
-		 // tags:
-		 //		private
-		 if(e){ dojo.stopEvent(e); }
-		 var size = dojo.marginBox(this.editor.domNode);
-		 dojo.style(this.openTd, "display", "none");
-		 dojo.style(this.closeTd, "display", "");
-		 dojo.style(this.menu, "display", "none");
-		 this.editor.resize({h: size.h});
+		// summary:
+		//		Internal function for handling a click event that will close the toolbar.
+		// e:
+		//		The click event.
+		// tags:
+		//		private
+		if(e){ dojo.stopEvent(e); }
+		var size = dojo.marginBox(this.editor.domNode);
+		dojo.style(this.openTd, "display", "none");
+		dojo.style(this.closeTd, "display", "");
+		dojo.style(this.menu, "display", "none");
+		this.editor.resize({h: size.h});
+		dijit.focus(this.closeTd.firstChild);
 	},
 
 	_onOpen: function(e) {
@@ -144,6 +163,7 @@ dojo.declare("dojox.editor.plugins.CollapsibleToolbar",dijit._editor._Plugin,{
 		 dojo.style(this.openTd, "display", "");
 		 dojo.style(this.menu, "display", "");
 		 this.editor.resize({h: size.h});
+		 dijit.focus(this.openTd.firstChild);
 	}
 });
 
