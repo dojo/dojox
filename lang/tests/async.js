a1938101f3579feb2183fb186578d253cd6f4e31
Eugene Lazutkin <eugene@lazutkin.com>
2010-01-18 08:37:37 +0000

    dojox.lang.async: adding unit tests for select(), ifThen(), and loop(), removing unneeded spaces, implementing is() to compare objects, !strict, refs #10625.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@21180 560b804f-0ae3-0310-86f3-f6aa0a117693

--- lang/tests/async.js
+++ lang/tests/async.js
@@ -9,7 +9,7 @@ dojo.require("dojox.lang.async.topic");
 		wait = async.timeout.from,
 		QUANT     = 50, // ms
 		MAX_TICKS = 5;
-		
+
 	function randomTimes(n){
 		var a = [];
 		for(var i = 0; i < n; ++i){
@@ -17,31 +17,99 @@ dojo.require("dojox.lang.async.topic");
 		}
 		return a;
 	}
-	
+
 	function getMin(a){
 		return Math.min.apply(Math, a);
 	}
-	
+
 	function getMax(a){
 		return Math.max.apply(Math, a);
 	}
-	
+
 	function waitAndExpect(expected, ms){
 		return function(value){
 			console.log("waitAndExpect: ", value, ", expected: ", expected);
 			if(expected !== value){
+				console.log("ERROR: unexpected value");
 				throw new Error("Unexpected value");
 			}
 			return wait(ms)();
 		}
 	}
-	
+
+	function identity(x){
+		return x;
+	}
+
+	function is(a, b, visited){
+		if(a === b){
+			return true;
+		}
+		if(typeof a != typeof b){
+			return false;
+		}
+		if(Object.prototype.toString.call(a) != Object.prototype.toString.call(b)){
+			return false;
+		}
+		if(Object.prototype.toString.call(a) == "[object Function]"){
+			return false;
+		}
+		if(Object.prototype.toString.call(a) == "[object Array]"){
+			if(a.length !== b.length){
+				return false;
+			}
+			for(var i = 0; i < a.length; ++i){
+				if(!is(a[i], b[i], visited)){
+					return false;
+				}
+				return true;
+			}
+		}
+		if(typeof a == "object"){
+			if(visited){
+				for(var i = 0; i < visited.length; ++i){
+					if(visited[i] === a || visited[i] === b){
+						return true;
+					}
+				}
+				visited.push(a, b);
+			}else{
+				visited = [a, b];
+}
+			var akeys = [];
+			for(var i in a){
+				akeys.push(i);
+			}
+			var bkeys = [];
+			for(var i in b){
+				bkeys.push(i);
+			}
+			akeys.sort();
+			bkeys.sort();
+			if(!is(akeys, bkeys)){
+				return false;
+			}
+			for(var i = 0; i < akeys.length; ++i){
+				if(!is(a[akeys[i]], b[bkeys[i]])){
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	var waitFor0 = waitAndExpect(0, 20),
+		waitFor1 = waitAndExpect(1, 20),
+		waitFor2 = waitAndExpect(2, 20);
+
 	tests.register("dojox.lang.tests.async", [
 		function smokeTest(){
 			var a = randomTimes(1),
 				r = new dojo.Deferred();
 			wait(a[0])().addCallback(function(x){
 				if(r == a[0]){
+					console.log("ERROR: smokeTest: wrong result");
 					throw new Error("smokeTest: wrong result");
 				}
 				r.callback();
@@ -55,6 +123,7 @@ dojo.require("dojox.lang.async.topic");
 				});
 			return async.seq.apply(async, fs)(0).addCallback(function(value){
 				if(a[a.length - 1] !== value){
+					console.log("ERROR: testSeq: wrong time");
 					throw new Error("testSeq: wrong time");
 				}
 			});
@@ -66,7 +135,8 @@ dojo.require("dojox.lang.async.topic");
 				});
 			return async.par.apply(async, fs)(0).addCallback(function(value){
 				console.log(a, " - ", value);
-				if(a !== value){
+				if(!is(a, value)){
+					console.log("ERROR: testPar: wrong time");
 					throw new Error("testPar: wrong time");
 				}
 			});
@@ -80,18 +150,67 @@ dojo.require("dojox.lang.async.topic");
 			return async.any.apply(async, fs)(0).addCallback(function(value){
 				console.log(min, " - ", value);
 				if(min !== value){
+					console.log("ERROR: testAny: wrong time");
 					throw new Error("testAny: wrong time");
 				}
 			});
 		},
-		function testSelect(){
-			
+		function testSelect0(){
+			return async.select(
+				identity,
+				waitFor0,
+				waitFor1,
+				waitFor2
+			)(0);
 		},
-		function testIfThen(){
-			
+		function testSelect1(){
+			return async.select(
+				identity,
+				waitFor0,
+				waitFor1,
+				waitFor2
+			)(1);
+		},
+		function testSelect2(){
+			return async.select(
+				identity,
+				waitFor0,
+				waitFor1,
+				waitFor2
+			)(2);
+		},
+		function testIfThenT(){
+			return async.ifThen(
+				identity,
+				waitFor1,
+				waitFor0
+			)(1);
+		},
+		function testIfThenF(){
+			return async.ifThen(
+				identity,
+				waitFor1,
+				waitFor0
+			)(0);
 		},
 		function testLoop(){
-			
+			var counter = 0;
+			return async.seq(
+				async.loop(
+					identity,
+					function(ms){
+						++counter;
+						return wait(ms - 10)();
+					}
+				),
+				function(){
+					console.log(counter, " - ", 3);
+					if(counter !== 3){
+						console.log("ERROR: testLoop: wrong number of iterations");
+						throw new Error("testLoop: wrong number of iterations");
+					}
+				}
+			)(30);
 		}
 	]);
 })();
