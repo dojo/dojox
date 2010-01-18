bada81e907564ee7fd57c1abb59f06a6b38c4ea3
Adam Peller <github@peller.org>
2010-01-18 15:45:10 +0000

    Add jsdoc for dojox.html.ellipsis based on what's on dojocampus.  Fixes #10469
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dojox@21183 560b804f-0ae3-0310-86f3-f6aa0a117693

--- html/ellipsis.js
+++ html/ellipsis.js
@@ -1,5 +1,16 @@
 dojo.provide("dojox.html.ellipsis");
 
+/*=====
+dojox.html.ellipsis = {
+	// summary: offers cross-browser support for text-overflow: ellipsis
+	//
+	// description: Add "dojoxEllipsis" on any node that you want to ellipsis-ize. In order to function properly,
+	//	the node with the dojoxEllipsis class set on it should be a child of a node with a defined width.
+	//	It should also be a block-level element (i.e. <div>) - it will not work on td elements.
+	//	NOTE: When using the dojoxEllipsis class within tables, the table needs to have the table-layout: fixed style
+}
+=====*/
+
 (function(d){
 	if(d.isMoz){
 		// The delay (in ms) to wait so that we don't keep querying when many 
