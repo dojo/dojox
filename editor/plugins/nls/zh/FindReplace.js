a385dc26efc4eb66d9324769e3a5c73c0f8d845d
Adam Peller <github@peller.org>
2010-02-16 18:12:00 +0000

    translations of 1.4 dojox.editor resources. Refs #10470
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@21368 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ editor/plugins/nls/zh/FindReplace.js
@@ -0,0 +1,12 @@
+({
+	"findLabel": "查找对象：",
+	"replaceLabel": "替换为：",
+	"findReplace": "切换“查找/替换”",
+	"matchCase": "大小写匹配", 
+	"backwards": "向后",
+	"replaceAll": "所有出现位置", 
+	"findButton": "查找",
+	"replaceButton": "替换",
+	"replaceDialogText": "已替换 ${0} 个出现位置。"
+})
+
