adbdae7bb584b25ded4b1d4402fa7e92fe44cfa9
Adam Peller <github@peller.org>
2010-02-16 19:18:40 +0000

    translations of 1.4 resources for dojox.grid for 1.4 branch. Refs #10470
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dojox@21372 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/enhanced/nls/zh/EnhancedGrid.js
+++ grid/enhanced/nls/zh/EnhancedGrid.js
@@ -1,8 +1,8 @@
 ({
-	//Only for testing purpose	
-	singleSort: "简单排序",
+	singleSort: "单一排序",
 	nestedSort: "嵌套排序",
 	ascending: "升序",
 	descending: "降序",
-	unsorted: "取消排序"
+	unsorted: "不要对此列排序"
 })
+
