668c1f3bdd52b16915bc3e7ab5214f5a0f0da836
Bill Keese <bill@dojotoolkit.org>
2010-02-17 06:12:53 +0000

    Convert dijit.getDocumentWindow() calls to dojo.window.get().  Also fixing spurious Menu test failure on FF3.6/mac because context-menu keyboard event defines pageX/pageY.
    Refs #7028 !strict.
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@21381 560b804f-0ae3-0310-86f3-f6aa0a117693

--- sketch/Figure.js
+++ sketch/Figure.js
@@ -145,7 +145,7 @@ dojo.require("dojox.sketch.UndoStack");
 			self._ctr=dojo.position(self.node);
 			//	figure out the coordinates taking scroll into account
 			var scroll={x:self.node.scrollLeft,y:self.node.scrollTop};
-			//var win = dijit.getDocumentWindow(self.node.ownerDocument);
+			//var win = dojo.window.get(self.node.ownerDocument);
 			//var scroll=dojo.withGlobal(win,dojo._docScroll);
 			self._ctr={x:self._ctr.x-scroll.x, y:self._ctr.y-scroll.y};
 			var X=e.clientX-self._ctr.x, Y=e.clientY-self._ctr.y;
