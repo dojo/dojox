83d7716140c970322e2b3e2678e7824e27bbfe66
Adam Peller <github@peller.org>
2010-02-23 17:57:27 +0000

    One more correction from IBM TV. Refs #10470
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.4/dojox@21418 560b804f-0ae3-0310-86f3-f6aa0a117693

--- editor/plugins/nls/ko/FindReplace.js
+++ editor/plugins/nls/ko/FindReplace.js
@@ -1,11 +1,11 @@
 ({
-	"findLabel": "찾을 문자열:",
-	"replaceLabel": "바꿀 대상:",
-	"findReplace": "토글 찾기/바꾸기",
-	"matchCase": "대소문자 일치", 
-	"backwards": "뒤로",
-	"replaceAll": "모두 바꾸기", 
-	"findButton": "찾기",
-	"replaceButton": "바꾸기",
-	"replaceDialogText": "${0} 개를 바꿨습니다."
+	"findLabel": "Find what:",
+	"replaceLabel": "Replace with:",
+	"findReplace": "Toggle Find/Replace",
+	"matchCase": "Match Case", 
+	"backwards": "Backwards",
+	"replaceAll": "All Occurances", 
+	"findButton": "Find",
+	"replaceButton": "Replace",
+	"replaceDialogText": "Replaced ${0} occurances."
 })
