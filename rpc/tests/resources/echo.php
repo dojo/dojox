0078d39a7fdc2dd7090d04f1da80948807a577dc
James Burke <jrburke@gmail.com>
2010-03-04 06:19:38 +0000

    Refs #10478
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.1/dojox@21471 560b804f-0ae3-0310-86f3-f6aa0a117693

--- rpc/tests/resources/echo.php
+++ rpc/tests/resources/echo.php
@@ -2,6 +2,7 @@
 	if (!$_REQUEST["message"]){
 		print "ERROR: message property not found";
 	}else{
-		print $_REQUEST["message"];
+		//Escape it, just to be safe
+		print htmlentities($_REQUEST["message"]);
 	}
 ?>
