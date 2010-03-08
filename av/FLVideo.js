ee6909f4e52a7751ab7be3fe378ab6c9ad2f66be
James Burke <jrburke@gmail.com>
2010-03-08 05:09:45 +0000

    Refs #10478
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.3/dojox@21499 560b804f-0ae3-0310-86f3-f6aa0a117693

--- av/FLVideo.js
+++ av/FLVideo.js
@@ -33,6 +33,12 @@ dojo.declare("dojox.av.FLVideo", [dijit._Widget, dojox.av._Media], {
 	_swfPath: dojo.moduleUrl("dojox.av", "resources/video.swf"),
 	//
 	//
+	constructor: function(/*Object*/options){
+		// Provide this function for the SWF to ensure that the it is playing
+		// in HTML. 
+		dojo.global.swfIsInHTML = function(){ return true; }
+	},
+	
 	postCreate: function(){
 		// summary:
 		// Initialize the media.
@@ -42,10 +48,6 @@ dojo.declare("dojox.av.FLVideo", [dijit._Widget, dojox.av._Media], {
 		this._cons = [];
 		this.mediaUrl = this._normalizeUrl(this.mediaUrl);
 		this.initialVolume = this._normalizeVolume(this.initialVolume);
-		
-		// Provide this function for the SWF to ensure that the it is playing
-		// in HTML. 
-		dojo.global.swfIsInHTML = function(){ return true; }
 		
 		var args = {
 			path:this._swfPath.uri,
