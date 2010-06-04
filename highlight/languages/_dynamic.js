e8c1b291b162bf1bc7571b7592f22f4f1a26b844
Douglas Hays <doughays@dojotoolkit.org>
2010-06-04 17:27:58 +0000

    Fixes #11264.  Added groovy,js, java,js, xquery,js to dojox/highlight/languages/ and added associated tests.  No php.js for the moment.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@22292 560b804f-0ae3-0310-86f3-f6aa0a117693

--- highlight/languages/_dynamic.js
+++ highlight/languages/_dynamic.js
@@ -5,3 +5,5 @@ dojo.require("dojox.highlight.languages.python");
 // dojo.require("dojox.highlight.languages.perl");
 // dojo.require("dojox.highlight.languages.php");
 // dojo.require("dojox.highlight.languages.ruby");
+dojo.require("dojox.highlight.languages.xquery");
+dojo.require("dojox.highlight.languages.groovy");
