dfbe876f6bb577a56d0d4c97086650825363781e
Bob Remeika <bob.remeika@gmail.com>
2010-06-05 11:39:51 +0000

    Better support for content types; Now handling data-method; Added CSRF protection; Now handling emote links with post, put and delete http methods \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@22295 560b804f-0ae3-0310-86f3-f6aa0a117693

--- rails.js
+++ rails.js
@@ -3,7 +3,9 @@ dojo.require("dojo.NodeList-traverse");
 
 dojo.ready((function(d, dr, dg){
 	return function() {
-		var q = d.query;
+		var q = d.query,
+				csrfToken = q("meta[name=csrf-token]").attr("content"),
+		    csrfParam = q("meta[name=csrf-param]").attr("content");
 
 		var live = function(selector, evtName, fn){
 			d.connect(d.body(), evtName, function(evt){
@@ -14,45 +16,18 @@ dojo.ready((function(d, dr, dg){
 			});
 		};
 
-		var handleConfirm = function(evt){
-			var proceed = dg.confirm(d.attr(evt.target, "data-confirm"));
-			if (!proceed){
-				evt.preventDefault();
-				return false;
-			}
-			return true;
+		var createFormForLink = function(url, method){
+			var form = '<form style="display:none" method="POST" action="'+ url +'">' +
+				'<input type="hidden" name="_method" value="'+ method +'" />' +
+				'<input type="hidden" name="'+ csrfParam +'" value="'+ csrfToken +'" />' +
+				'</form>';
+			return dojo.place(form, dojo.body());
 		};
 
-		var handleRemote = function(evt){
-			evt.preventDefault();
-
-			var method, url, content;
-			var el = evt.target;
-
-			if (el.tagName.toLowerCase() == "form"){
-				url = d.attr(el, "action");
-				method = (d.attr(el, "method") || "POST").toUpperCase();
-				content = d.formToObject(el);
-			}else{
-				url = d.attr(el, "href");
-				method = (d.attr(el, "data-method") || "GET").toUpperCase();
-				content = {};
-			}
-
-			// data-type is toolkit specific, rails doesn't actually spit this out unless specifically included
-			var type = d.attr(el, "data-type") || "javascript";
-
-			// TODO: Implement ajax:loading, ajax:loaded, and ajax:interactive as dojo.Deferred callbacks
-			d.publish("ajax:before", [el]);
-			var deferred = d.xhr(method, {
-				url: url,
-				handleAs: type,
-				content: content,
-				load:		function(response, ioArgs) { d.publish("ajax:success",	[el, response, ioArgs]); },
-				error:	function(response, ioArgs) { d.publish("ajax:failure",	[el, response, ioArgs]); },
-				handle: function(response, ioArgs) { d.publish("ajax:complete", [el, response, ioArgs]); }
-			});
-			d.publish("ajax:after", [el]);
+		var handleConfirm = function(evt){
+			var proceed = dg.confirm(d.attr(evt.target, "data-confirm"));
+			if (!proceed){evt.preventDefault();}
+			return proceed;
 		};
 
 		var disable = function(elements){
@@ -68,9 +43,42 @@ dojo.ready((function(d, dr, dg){
 			});
 		};
 
+		var handleRemote = function(evt){
+			var el = evt.target, type = d.attr(el, "data-type") || "javascript";
+
+			var method, url, content;
+			if (el.tagName.toUpperCase() == "FORM"){
+				url = d.attr(el, "action");
+				method = (d.attr(el, "method") || "POST").toUpperCase();
+				content = d.formToObject(el);
+			}else{
+				url = d.attr(el, "href");
+				method = (d.attr(el, "data-method") || "GET").toUpperCase();
+				content = {};
+				if (method != "GET"){
+					el = createFormForLink(url, method);
+					method = "POST";
+				}
+			}
+
+			// ajax:loading, ajax:loaded, and ajax:interactive are not supported
+			d.publish("ajax:before", [el]);
+			evt.preventDefault();
+			var deferred = d.xhr(method, {
+				url:      url,
+				headers:  { "Accept": "text/"+type },
+				content:  content,
+				handleAs: type,
+				load:		  function(response, ioArgs) { d.publish("ajax:success",	[el, response, ioArgs]); },
+				error:	  function(response, ioArgs) { d.publish("ajax:failure",	[el, response, ioArgs]); },
+				handle:   function(response, ioArgs) { d.publish("ajax:complete", [el, response, ioArgs]); }
+			});
+			d.publish("ajax:after", [el]);
+		};
+
+
 		var handleEnable	= function(el){
 			q("*[data-disable-with][disabled]", el).forEach(function(node){
-				console.debug("found node", node);
 				var attr = node.tagName.toLowerCase() == "input" ? "value" : "innerHTML";
 				var value = d.attr(node, "data-original-value");
 				d.attr(node, "disabled", false);
@@ -79,17 +87,19 @@ dojo.ready((function(d, dr, dg){
 			});
 		};
 
-		var handleFormSubmit = function(evt){
-			console.debug("handleFormSubmit", evt.target);
+		var handleDataMethod = function(evt){
+			var el = evt.target, form = createFormForLink(el.href, dojo.attr(el, "data-method"));
 			evt.preventDefault();
-			var el = evt.target;
-			var disableElements = q("*[data-disable-with]", el);
-			if (disableElements.length){ disable(el); }
+			form.submit();
+		};
+
+		var handleFormSubmit = function(evt){
+			var el = evt.target, disableElements = q("*[data-disable-with]", el);
+			if (disableElements.length){ disable(disableElements); }
 			if (d.attr(el, "data-remote")){
 				evt.preventDefault();
 				handleRemote(evt);
 			}else{
-				alert("submitting");
 				el.submit();
 			}
 		};
@@ -106,6 +116,7 @@ dojo.ready((function(d, dr, dg){
 
 		// Register data-remote elements
 		live("a[data-remote]", "click", handleRemote);
+		live("a[data-method]:not([data-remote])", "click", handleDataMethod);
 
 		// Handle form submits
 		live("form", "submit", handleFormSubmit);
