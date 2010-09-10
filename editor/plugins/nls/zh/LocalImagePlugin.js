fa0273316f814d3a56c90a8be13f1b55508e7d43
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2010-09-10 18:48:13 +0000

    Adding in CCLA LocalImage plugin.  fixes #11283
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@22854 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ editor/plugins/nls/zh/LocalImagePlugin.js
@@ -0,0 +1,10 @@
+({
+	insertImageTitle: "插入图片",
+	url: "图片",
+	browse: "浏览...",
+	text: "描述",
+	set: "插入",
+	invalidMessage: "非法图片格式",
+	prePopuTextUrl: "输入图片URL",
+	prePopuTextBrowse: "或者单击浏览插入本地图像"
+})
