96d5eea4240c50140b8a01c8216080f83a16d4bf
Kris Zyp <kriszyp@gmail.com>
2010-10-08 17:32:07 +0000

    Improved handling of send(), first request indication
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@22975 560b804f-0ae3-0310-86f3-f6aa0a117693

--- socket.js
+++ socket.js
@@ -23,6 +23,8 @@ dojox.socket = function(/*dojo.__XhrArgs*/ argsOrUrl){
 	//		|    var message = event.data;
 	//		|    // do something with the message
 	//		| });
+	//		| // send something
+	//		| socket.send("hi there");
 	//		| whenDone(function(){
 	//		|   socket.close();
 	//		| });
@@ -63,55 +65,87 @@ dojox.socket.LongPoll = function(/*dojo.__XhrArgs*/ args){
 	//		| dojox.socket.LongPoll.add();
 	//		| var socket = dojox.socket({url:"/comet"});
 
-	var deferred, cancelled = false, transport = args.transport || dojo.xhrPost;
+var cancelled = false,
+		first = true,
+		transport = args.transport || dojo.xhrPost,
+		connections = [];
+	
 	// create the socket object
 	var socket = {
 		send: function(data){
-			// send some data, just using another XHR request
+			// summary:
+			// 		Send some data using XHR or provided transport
 			args.postData = data;
-			return transport(args).then(null, onError);
+			var deferred = first ? (first = false) || socket.firstRequest(args, transport) :
+				transport(args);
+			connections.push(deferred);
+			deferred.then(function(response){
+				// got a response
+				// remove the current connection
+				connections.splice(dojo.indexOf(connections, deferred), 1);
+				// reconnect to listen for the next message if there are no active connections, 
+				// we queue it up in case one of the onmessage handlers has a message to send
+				if(!connections.length){
+					setTimeout(connect);
+				}
+				if(response){
+					// now send the message along to listeners
+					fire("message", deferred, {data: response});
+				}
+			}, function(error){
+				connections.splice(dojo.indexOf(connections, deferred), 1);
+				// an error occurred, fire the appropriate event listeners
+				if(!cancelled){
+					fire("error", deferred, {error:error});
+					if(!connections.length){
+						fire("close", deferred, {wasClean:false});
+					}
+				}
+			});
+			return deferred;
 		},
 		close: function(){
-			// close the connection
+			// summary:
+			// 		Close the connection
 			cancelled = true;
-			deferred.cancel();
+			for(var i = 0; i < connections.length; i++){
+				connections[i].cancel();
+			}
+			fire("close", deferred, {wasClean:true});
+		},
+		firstRequest: function(args, transport){
+			// summary:
+			// 		This allows for special handling for the first request. This is useful for
+			//		providing information to disambiguate between the first request and
+			//		subsequent long-poll requests so the server can properly setup a 
+			// 		connection on the first connection or reject a request for an expired 
+			// 		connection if the request is not expecting to be the first for a connection.
+			//		This method can be overriden. The default behavior is to include a Pragma
+			//		header with a value of "start-long-poll"
+			var headers = (args.headers || (args.headers = {}));
+			headers.Pragma = "start-long-poll";
+			try{
+				return transport(args);
+			}finally{
+				// cleanup the header so it is not used on subsequent requests
+				delete headers.Pragma;
+			}
 		}
 	};
 	function connect(){
 		// make the long-poll connection, to wait for response from the server
-		deferred = transport(args);
-		deferred.then(function(response){
-			// got a response, reconnect right away, to listen for the next message
-			connect();
-			// now send the message along to listeners
-			setTimeout(function(){
-				if(socket.onmessage){
-					socket.onmessage({
-						name: "message",
-						data: response,
-						ioArgs: deferred.ioArgs
-					});
-				}
-			});
-		}, function(error){
-			// an error occurred, fire the appropriate event listeners
-			if(socket.onerror && !cancelled){
-				socket.onerror({
-					name: "error",
-					error: error,
-					ioArgs: deferred.ioArgs
-				});
-			}
-			if(socket && socket.onclose){
-				socket.onclose({
-					name: "close",
-					wasClean: cancelled,
-					ioArgs: deferred.ioArgs
-				});
-			}
-		});
+		if(!connections.length){
+			socket.send();
+		}
+	}
+	function fire(event, deferred, object){
+		if(socket["on" + event]){
+			object.name = event;
+			object.ioArgs = deferred && deferred.ioArgs;
+			socket["on" + event](object);
+		}
 	}
 	// do the initial connection
-	connect();
+	setTimeout(connect);
 	return socket;
 };
