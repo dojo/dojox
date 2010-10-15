bb11ccb40f52b0529cabeff885c019ab335dfdb5
Jared Jurkiewicz <jaredj@dojotoolkit.org>
2010-10-15 19:50:29 +0000

    Adding in AutoSave plugin.  fixes #11790
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@23021 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ editor/plugins/nls/zh/AutoSave.js
@@ -0,0 +1,13 @@
+({
+	"saveLabel": "保存",
+	"saveSettingLabelOn": "设置自动保存时间间隔...",
+	"saveSettingLabelOff": "关闭自动保存",
+	"saveSettingdialogTitle": "自动保存",
+	"saveSettingdialogDescription": "指定自动保存时间间隔",
+	"saveSettingdialogParamName": "自动保存时间间隔",
+	"saveSettingdialogParamLabel": "分钟",
+	"saveSettingdialogButtonOk": "设置时间间隔",
+	"saveSettingdialogButtonCancel": "取消",
+	"saveMessageSuccess": "于 ${0} 保存",
+	"saveMessageFail": "于 ${0} 保存失败"
+})
