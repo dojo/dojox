8df65ea8fed192abb065370dbc41b4921b535a77
Eugene Lazutkin <eugene@lazutkin.com>
2011-02-01 01:09:39 +0000

    gfx: added text to the canvas renderer, thx Patrick Ruzand (IBM, CCLA) and edchat (IBM, CCLA)!, !strict, fixes #12158.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@23682 560b804f-0ae3-0310-86f3-f6aa0a117693

--- gfx/canvas.js
+++ gfx/canvas.js
@@ -289,11 +289,75 @@ dojo.experimental("dojox.gfx.canvas");
 		}
 	});
 
+	var canvasTextAPI = typeof (document.createElement("canvas").getContext('2d').fillText) == 'function';
+	
 	dojo.declare("dojox.gfx.Text", gs.Text, {
-		// summary: a text shape (Canvas)
-		_renderShape: function(/* Object */ ctx){
-			var s = this.shape;
-			// nothing for the moment
+		_setFont:function(){
+			if (this.fontStyle){
+				this.canvasFont = dojox.gfx.makeFontString(this.fontStyle);
+			} else {
+				delete this.canvasFont;
+			}
+		},
+		
+		getTextWidth: function() {
+			// summary: get the text width in pixels
+			if (!canvasTextAPI) {
+				return 0;
+			}
+			var s = this.shape, w = 0, ctx;
+			if (s.text && s.text.length > 0) {
+				ctx = this.surface.rawNode.getContext("2d");
+				ctx.save();
+				this._renderTransform(ctx);
+				this._renderFill(ctx, false);
+				this._renderStroke(ctx, false);
+				if (this.canvasFont)
+					ctx.font = this.canvasFont;
+				w = ctx.measureText(s.text).width;
+				ctx.restore();
+			}
+			return w;
+		},
+		
+		// override to apply first fill and stroke (
+		// the base implementation is for path-based shape that needs to first define the path then to fill/stroke it.
+		// Here, we need the fillstyle or strokestyle to be set before calling fillText/strokeText.
+		_render: function(/* Object */ctx) {
+			// summary: render the shape
+			// ctx : Object: the drawing context.
+			ctx.save();
+			this._renderTransform(ctx);
+			this._renderFill(ctx, false);
+			this._renderStroke(ctx, false);
+			this._renderShape(ctx);
+			ctx.restore();
+		},
+		
+		_renderShape: function(ctx) {
+			// summary: a text shape (Canvas)
+			// ctx : Object: the drawing context.
+			if (!canvasTextAPI) {
+				return;
+			}
+			var ta, s = this.shape;
+			if (!s.text || s.text.length == 0) {
+				return;
+			}
+			// text align
+			ta = s.align === 'middle' ? 'center' : s.align;
+			ctx.textAlign = ta;
+			if (this.canvasFont) {
+				ctx.font = this.canvasFont;
+			}
+			if (this.canvasFill) {
+				ctx.fillText(s.text, s.x, s.y);
+			}
+			if (this.strokeStyle) {
+				ctx.beginPath(); // fix bug in FF3.6. Fixed in FF4b8
+				ctx.strokeText(s.text, s.x, s.y);
+				ctx.closePath();
+			}
 		}
 	});
 	modifyMethod(g.Text, "setFont");
