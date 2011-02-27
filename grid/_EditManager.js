89b90037a3004f05bde391a7545436ee77c351f1
Evan Huang <evanhuangwei@gmail.com>
2011-02-27 13:51:49 +0000

    "Applying changes in editing when grid gets blurred" - this should be only effective for !IE, refs #11386
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@23903 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/_EditManager.js
+++ grid/_EditManager.js
@@ -9,9 +9,10 @@ dojo.declare("dojox.grid._EditManager", null, {
 		// inGrid: dojox.Grid
 		//		The dojox.Grid this editor should be attached to
 		this.grid = inGrid;
-		this.connections = dojo.isIE <= 7 ? [] : [dojo.connect(this.grid, 'onBlur', this, 'apply')];
 		if(dojo.isIE){
-			this.connections.push(dojo.connect(document.body, "onfocus", dojo.hitch(this, "_boomerangFocus")));
+			this.connections = [dojo.connect(document.body, "onfocus", dojo.hitch(this, "_boomerangFocus"))];
+		}else{
+			this.connections = [dojo.connect(this.grid, 'onBlur', this, 'apply')];
 		}
 	},
 	
