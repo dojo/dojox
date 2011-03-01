b33c481b524d02a8bd9dfb1b5d920bed74882354
Douglas Hays <doughays@dojotoolkit.org>
2011-03-01 20:41:32 +0000

    Fixes #12368. Refactor dijit.form.TextBox by splitting out the core function into _TextBoxMixin.js.  Also split out most of _FormWidget.js into _FormWidgetMixin.js to remove the _Templated dependency.  Combine these mixin's to create dojox.mobile.TextBox.  Added a basic automated test TextBoxMixin.html.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@23945 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ mobile/TextBox.js
@@ -0,0 +1,29 @@
+dojo.provide("dojox.mobile.TextBox");
+dojo.experimental("dojox.mobile.TextBox");
+
+dojo.require("dijit.form._TextBoxMixin");
+dojo.require("dijit.form._FormWidgetMixin");
+
+dojo.declare(
+	"dojox.mobile.TextBox", 
+	[dijit._WidgetBase, dijit.form._FormValueMixin, dijit.form._TextBoxMixin], {
+		baseClass: "mblTextBox",
+
+		// Map widget attributes to DOMNode attributes.
+		_mapPlaceHolderAttr: "textbox",
+
+		buildRendering: function(){
+			if(!this.srcNodeRef){
+				this.srcNodeRef = dojo.create("input", {});
+			}
+			this.inherited(arguments);
+			this.textbox = this.focusNode = this.domNode;
+		},
+
+		postCreate: function(){
+			this.inherited(arguments);
+			this.connect(this.textbox, "onfocus", "_onFocus");
+			this.connect(this.textbox, "onblur", "_onBlur");
+		}
+	}
+);
