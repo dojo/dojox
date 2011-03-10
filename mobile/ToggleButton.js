39dfd4b77009a2e007c3d1f1b14d528f52c6279d
Douglas Hays <doughays@dojotoolkit.org>
2011-03-10 00:44:45 +0000

    Fixes #12419.  Refactor dijit.form.ToggleButton by splitting out the function into _ToggleButtonMixin.js.  Use this mixin to create dojox.mobile.ToggleButton that does not use a template. Added a basic automated test ToggleButtonMixin.html.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@23989 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ mobile/ToggleButton.js
@@ -0,0 +1,25 @@
+define("dojox/mobile/ToggleButton", ["dojo", "dijit", "dojox/mobile/Button", "dijit/form/_ToggleButtonMixin"], function(dojo, dijit) {
+
+dojo.declare("dojox.mobile.ToggleButton", [dojox.mobile.Button, dijit.form._ToggleButtonMixin], {
+	// summary:
+	//		A non-templated button widget that can be in two states (checked or not).
+	//		Can be base class for things like tabs or checkbox or radio buttons
+
+	baseClass: "mblToggleButton",
+
+	_setCheckedAttr: function(){
+		this.inherited(arguments);
+		var button = this.focusNode || this.domNode;
+		var newStateClasses = (this.baseClass+' '+this["class"]).split(" ");
+		newStateClasses = dojo.map(newStateClasses, function(c){ return c+"Checked"; });
+		if(this.checked){
+			dojo.addClass(button, newStateClasses);
+		}else{
+			dojo.removeClass(button, newStateClasses);
+		}
+	}
+
+});
+
+return dojox.mobile.ToggleButton;
+});
