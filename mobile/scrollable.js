c51dc82a125536afc36c3225a4142a94af39171f
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2011-03-19 13:41:47 +0000

    Refs #12482 !strict. Added the default drag threshold
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24067 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/scrollable.js
+++ mobile/scrollable.js
@@ -74,7 +74,7 @@ dojox.mobile.scrollable = function(){
 	this.weight = 0.6; // frictional drag
 	this.fadeScrollBar = true;
 	this.disableFlashScrollBar = false;
-	this.threshold = 0; // drag threshold value in pixels
+	this.threshold = 4; // drag threshold value in pixels
 
 //>>excludeStart("dojo", true);
 	if(!dojo.version){ // seems running in a non-dojo environment
@@ -254,7 +254,7 @@ dojox.mobile.scrollable = function(){
 		var dim = this._dim;
 
 		if(this._time.length == 1){ // the first TouchMove after TouchStart
-			if(dx < this.threshold && dy < this.threshold){ return; }
+			if(Math.abs(dx) < this.threshold && Math.abs(dy) < this.threshold){ return; }
 			this.addCover();
 			this.showScrollBar();
 		}
