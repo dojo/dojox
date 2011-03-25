271f568039e55d64274ed1dbe4063617d3eef373
Adam Peller <github@peller.org>
2011-03-25 16:48:48 +0000

    validate format strings \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24112 560b804f-0ae3-0310-86f3-f6aa0a117693

--- string/tests/BidiEngine/module.js
+++ string/tests/BidiEngine/module.js
@@ -2,6 +2,8 @@ dojo.provide("dojox.string.tests.BidiEngine.module");
 
 try{
    	dojo.require("dojox.string.tests.BidiEngine.BidiEngine");
+   	
+   	dojo.require("dojox.string.tests.BidiEngine.BidiEngineTestLayouts");
 
 }catch(e){
      doh.debug(e);
