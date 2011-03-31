f378f4b7161c080fb1411081677bfebc06991b85
Adam Peller <github@peller.org>
2011-03-31 19:21:27 +0000

    Patch from BryanForbes to undeprecate ellipsis.  I was too hasty.  There's a non-XUL branch.  Also, apply to specified node.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24179 560b804f-0ae3-0310-86f3-f6aa0a117693

--- html/ellipsis.js
+++ html/ellipsis.js
@@ -1,5 +1,4 @@
 dojo.provide("dojox.html.ellipsis");
-dojo.deprecated("dojox.html.ellipsis", "Does not work with Firefox>4.  There is no replacement.  Use text-overflow:ellipsis for other browsers.");
 
 /*=====
 dojox.html.ellipsis = {
@@ -13,7 +12,6 @@ dojox.html.ellipsis = {
 =====*/
 
 (function(d){
-    try{
 	if(d.isMoz){ //TODO: feature detect text-overflow in computed style?
 		// The delay (in ms) to wait so that we don't keep querying when many
 		// changes happen at once - set config "dojoxFFEllipsisDelay" if you
@@ -25,24 +23,28 @@ dojox.html.ellipsis = {
 				delay = 1;
 			}
 		}
-		// Create our stub XUL elements for cloning later
-		// NOTE: this no longer works as of FF 4.0:
-		// https://developer.mozilla.org/En/Firefox_4_for_developers#Remote_XUL_support_removed
-		var sNS = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
-		var xml = document.createElementNS(sNS, 'window');
-		var label = document.createElementNS(sNS, 'description');
-		label.setAttribute('crop', 'end');
-		xml.appendChild(label);
-		
-		var createXULEllipsis = function(/* Node */ n){
-			// Summary:
-			//		Given a node, it creates the XUL and sets its
-			//		content so that it will have an ellipsis
-			var x = xml.cloneNode(true);
-			x.firstChild.setAttribute('value', n.textContent);
-			n.innerHTML = '';
-			n.appendChild(x);
-		};
+		try{
+			var createXULEllipsis = (function(){
+				// Create our stub XUL elements for cloning later
+				// NOTE: this no longer works as of FF 4.0:
+				// https://developer.mozilla.org/En/Firefox_4_for_developers#Remote_XUL_support_removed
+				var sNS = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
+				var xml = document.createElementNS(sNS, 'window');
+				var label = document.createElementNS(sNS, 'description');
+				label.setAttribute('crop', 'end');
+				xml.appendChild(label);
+
+				return function(/* Node */ n){
+					// Summary:
+					//		Given a node, it creates the XUL and sets its
+					//		content so that it will have an ellipsis
+					var x = xml.cloneNode(true);
+					x.firstChild.setAttribute('value', n.textContent);
+					n.innerHTML = '';
+					n.appendChild(x);
+				};
+			})();
+		}catch(e){}
 		
 		// Create our iframe elements for cloning later
 		var create = d.create;
@@ -159,7 +161,7 @@ dojox.html.ellipsis = {
 			d.forEach(s[fn].apply(s, [opt]), function(n){
 				if(!n || n._djx_ellipsis_done){ return; }
 				n._djx_ellipsis_done = true;
-				if(n.textContent == n.innerHTML && !hc(n, "dojoxEllipsisSelectable")){
+				if(createXULEllipsis && n.textContent == n.innerHTML && !hc(n, "dojoxEllipsisSelectable")){
 					// We can do the faster XUL version, instead of calculating
 					createXULEllipsis(n);
 				}else{
@@ -190,7 +192,4 @@ dojox.html.ellipsis = {
 			connFx();
 		});
 	}
-    }catch(e){
-	console.warn("dojox.html.ellipsis does not work with Firefox >= 4");
-    }
 })(dojo);
