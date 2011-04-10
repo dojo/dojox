3e149d70befb5f80f052189bfb20e14d2b61965d
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2011-04-10 05:47:54 +0000

    Porting [24003] and [24248] to honor name attribute in Uploader, to 1.6 branch. Fixes #12411
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.6/dojox@24252 560b804f-0ae3-0310-86f3-f6aa0a117693

--- form/uploader/plugins/HTML5.js
+++ form/uploader/plugins/HTML5.js
@@ -92,8 +92,8 @@ dojo.declare("dojox.form.uploader.plugins.HTML5", [], {
 
 		var fd = new FormData();
 		dojo.forEach(this.inputNode.files, function(f, i){
-			fd.append("uploadedfiles[]", f);
-		});
+			fd.append(this.name+"s[]", f);
+		}, this);
 
 		if(data){
 			for(var nm in data){
@@ -166,7 +166,7 @@ dojo.declare("dojox.form.uploader.plugins.HTML5", [], {
 
 		var filesInError = [];
 		dojo.forEach(this.inputNode.files, function(f, i){
-			var fieldName = "uploadedfiles[]";//+i;
+			var fieldName = this.name+"s[]";//+i;
 			var fileName  = this.inputNode.files[i].fileName;
 			var binary;
 
