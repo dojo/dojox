0c6d0ef7b9bce84b1c41389af47e403cbad3a8dd
Chris Mitchell <ccmitchellusa@gmail.com>
2011-05-03 16:29:11 +0000

    fixes #12406 Experimental OpenLayers map library initial checkin !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24611 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ geo/openlayers/LineString.js
@@ -0,0 +1,28 @@
+// dojo.provide allows pages to use all of the types declared in this resource.
+dojo.provide("dojox.geo.openlayers.LineString");
+dojo.experimental("dojox.geo.openlayers.LineString");
+dojo.require("dojox.geo.openlayers.Geometry");
+
+dojo.declare("dojox.geo.openlayers.LineString", dojox.geo.openlayers.Geometry, {
+	// summary:
+  //   The `dojox.geo.openlayers.LineString` geometry. This geometry holds an array
+	// of coordinates.
+	
+	// summary:
+	//   Sets the points for this geometry.
+	// p : Array
+	//   An array of {x, y} objects
+	setPoints : function(p) {
+		this.coordinates = p;
+	},
+	
+	// summary:
+	//  Gets the points of this geometry.
+	// returns: Array
+	//  The points of this geometry.
+	getPoints : function() {
+		return this.coordinates;
+	}
+
+});
+
---
+++ geo/openlayers/tests/ecr/PortRenderer.js
@@ -0,0 +1,25 @@
+// dojo.provide allows pages to use all of the types declared in this resource.
+dojo.provide("dojox.geo.openlayers.tests.ecr.PortRenderer");
+dojo.require("dojox.geo.openlayers.Point");
+
+dojo.require("dojox.geo.openlayers.tests.ecr.EcrRenderer");
+dojo.require("dojox.geo.openlayers.GeometryFeature");
+dojo.require("dojox.geo.openlayers.Geometry");
+
+dojo.declare("dojox.geo.openlayers.tests.ecr.PortRenderer", [ dojox.geo.openlayers.tests.ecr.EcrRenderer ], {
+
+  constructor : function(opts, context) {},
+
+  _renderItem : function(o, item) {
+    var gf = null;
+    if (o.type == "circle") {
+      var coords = this.getCoordinates(item);
+      var g = new dojox.geo.openlayers.Point({x:coords[0], y:coords[1]});
+      gf = new dojox.geo.openlayers.GeometryFeature(g);
+      gf.setShapeProperties({
+        r : o.radius
+      });
+    }
+    return gf;
+  }
+});
