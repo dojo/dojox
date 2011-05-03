dojo/dojox

commit 2139887bf9e5a53b393033548b97108f58d5f2d6

update to use relative module identfiers, move towards baseless dojo, whitespace cleanup for dojo styleguide !strict
git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24548 560b804f-0ae3-0310-86f3-f6aa0a117693

mobile/ComboBox.js
@@ -1,9 +1,6 @@
-define("dojox/mobile/ComboBox", ["dojo", "dijit", "dojox", "dojox/mobile/TextBox", "dojox/mobile/_ComboBoxMenu", "dijit/form/_AutoCompleterMixin"], function(dojo, dijit, dojox) {
+define(["./TextBox", "./_ComboBoxMenu", "dijit/form/_AutoCompleterMixin"], function(TextBox,ComboBoxMenu,AutoCompleterMixin) {
 
-dojo.declare(
-	"dojox.mobile.ComboBox",
-	[dojox.mobile.TextBox, dijit.form._AutoCompleterMixin],
-	{
+	return dojo.declare("dojox.mobile.ComboBox",[TextBox, AutoCompleterMixin],{
 		// summary:
 		//		A non-templated auto-completing text box widget
 		//
@@ -11,7 +8,9 @@ dojo.declare(
 		// dropDownClass: [protected extension] String
 		//		Name of the dropdown widget class used to select a date/time.
 		//		Subclasses should specify this.
-		dropDownClass: "dojox.mobile._ComboBoxMenu",
+		//
+		//dropDownClass: "dojox.mobile._ComboBoxMenu",
+		dropDownClass: ComboBoxMenu,
 
 		// initially disable selection since iphone displays selection handles that makes it hard to pick from the list
 		selectOnClick: false,
@@ -34,8 +33,5 @@ dojo.declare(
 			}
 			return ret;
 		}
-	}
-);
-
-return dojox.mobile.ComboBox;
+	});
 });

mobile/Button.js
@@ -1,8 +1,8 @@
-define("dojox/mobile/Button", ["dojo", "dijit", "dojox", "dijit/_WidgetBase", "dijit/form/_FormWidgetMixin", "dijit/form/_ButtonMixin"], function(dojo, dijit, dojox){
+define(["dojo/_base/array", "dojo/_base/html","dijit/_WidgetBase", "dijit/form/_FormWidgetMixin", "dijit/form/_ButtonMixin"], function(darray, html, WidgetBase,FormWidgetMixin,ButtonMixin){
 
-dojo.declare("dojox.mobile.Button", [dijit._WidgetBase, dijit.form._FormWidgetMixin, dijit.form._ButtonMixin], {
+	return dojo.declare("dojox.mobile.Button", [WidgetBase, FormWidgetMixin, ButtonMixin], {
 		// summary:
-		//		Non-templated BUTTON widget with a thin API wrapper for click events and setting the label
+		//	Non-templated BUTTON widget with a thin API wrapper for click events and setting the label
 		//
 		// description:
 		//              Buttons can display a label, an icon, or both.
@@ -43,7 +43,6 @@ dojo.declare("dojox.mobile.Button", [dijit._WidgetBase, dijit.form._FormWidgetMi
 			this.inherited(arguments);
 			this.connect(this.domNode, "onclick", "_onClick");
 		}
-});
+	});
 
-return dojox.mobile.Button;
 });

mobile/CheckBox.js
@@ -1,23 +1,21 @@
-define("dojox/mobile/CheckBox", ["dojo", "dijit", "dojox", "dojox/mobile/ToggleButton", "dijit/form/_CheckBoxMixin"], function(dojo, dijit, dojox) {
+define(["dojo/_base/html", "dojox/mobile/ToggleButton", "dijit/form/_CheckBoxMixin"], function(dhtml,ToggleButton,CheckBoxMixin) {
 
-dojo.declare("dojox.mobile.CheckBox", [dojox.mobile.ToggleButton, dijit.form._CheckBoxMixin], {
-	// summary:
-	//		A non-templated checkbox widget that can be in two states (checked or not).
+	return dojo.declare("dojox.mobile.CheckBox", [ToggleButton, CheckBoxMixin], {
+		// summary:
+		//		A non-templated checkbox widget that can be in two states (checked or not).
 
-	baseClass: "mblCheckBox",
+		baseClass: "mblCheckBox",
 
-	_setTypeAttr: function(){}, // cannot be changed: IE complains w/o this
+		_setTypeAttr: function(){}, // cannot be changed: IE complains w/o this
 
-	buildRendering: function(){
-		if(!this.srcNodeRef){
-			// The following doesn't work on IE < 8 if the default state is checked.
-			// You have to use "<input checked>" instead but it's not worth the bytes here.
-			this.srcNodeRef = dojo.create("input", {type: this.type});
+		buildRendering: function(){
+			if(!this.srcNodeRef){
+				// The following doesn't work on IE < 8 if the default state is checked.
+				// You have to use "<input checked>" instead but it's not worth the bytes here.
+				this.srcNodeRef = dojo.create("input", {type: this.type});
+			}
+			this.inherited(arguments);
+			this.focusNode = this.domNode;
 		}
-		this.inherited(arguments);
-		this.focusNode = this.domNode;
-	}
-});
-
-return dojox.mobile.CheckBox;
+	});
 });

mobile/ContentPane.js
@@ -1,89 +1,83 @@
-define("dojox/mobile/ContentPane", ["dojo", "dijit", "dojox", "dijit/_WidgetBase", "dijit._Container", "dijit._Contained", "dojo/_base/xhr"], function(dojo, dijit, dojox) {
+define(["dijit/_WidgetBase", "dijit._Container", "dijit._Contained", "dojo/_base/xhr","./ProgressIndicator"], function(WidgetBase,Container,Contained,xhr,ProgressIndicator) {
 
-// summary:
-//		A very simple content pane to embed an HTML fragment.
-// description:
-//		This widget embeds an HTML fragment and run the parser.
-//		onLoad() is called when parsing is done and the content is ready.
-//		"dojo/_base/xhr" is in the dependency list. Usually this is not
-//		necessary, but there is a case where dojox.mobile custom build does not
-//		contain dojo._base.xhr.
+	// summary:
+	//		A very simple content pane to embed an HTML fragment.
+	// description:
+	//		This widget embeds an HTML fragment and run the parser.
+	//		onLoad() is called when parsing is done and the content is ready.
+	//		"dojo/_base/xhr" is in the dependency list. Usually this is not
+	//		necessary, but there is a case where dojox.mobile custom build does not
+	//		contain dojo._base.xhr.
 
-dojo.declare(
-	"dojox.mobile.ContentPane",
-	[dijit._WidgetBase, dijit._Container, dijit._Contained],
-{
-	href: "",
-	content: "",
-	parseOnLoad: true,
-	prog: true, // show progress indicator
+	return dojo.declare("dojox.mobile.ContentPane", [WidgetBase,Container,Contained],{
+		href: "",
+		content: "",
+		parseOnLoad: true,
+		prog: true, // show progress indicator
 
-	startup: function(){
-		if(this._started){ return; }
-		if(this.prog){
-			this._p = dojox.mobile.ProgressIndicator.getInstance();
+		startup: function(){
+			if(this._started){ return; }
+			if(this.prog){
+				this._p = dojox.mobile.ProgressIndicator.getInstance();
+			}
+			if(this.href){
+				this.set("href", this.href);
+			}else if(this.content){
+				this.set("content", this.content);
+			}
+			var parent = this.getParent && this.getParent();
+			if(!parent || !parent.resize){ // top level widget
+				this.resize();
+			}
+			this.inherited(arguments);
+		},
+	
+		resize: function(){
+			dojo.forEach(this.getChildren(), function(child){
+				if(child.resize){ child.resize(); }
+			});
+		},
+	
+		loadHandler: function(/*String*/response){
+			this.set("content", response);
+		},
+	
+		errorHandler: function(err){
+			if(p){ p.stop(); }
+		},
+	
+		onLoad: function(){
+			// Stub method to allow the application to connect to.
+			// Called when parsing is done and the content is ready.
+		},
+	
+		_setHrefAttr: function(/*String*/href){
+			var p = this._p;
+			if(p){
+				dojo.body().appendChild(p.domNode);
+				p.start();
+			}
+			this.href = href;
+			dojo.xhrGet({
+				url: href,
+				handleAs: "text",
+				load: dojo.hitch(this, "loadHandler"),
+				error: dojo.hitch(this, "errorHandler")
+			});
+		},
+
+		_setContentAttr: function(/*String|DomNode*/data){
+			this.destroyDescendants();
+			if(typeof data === "object"){
+				this.domNode.appendChild(data);
+			}else{
+				this.domNode.innerHTML = data;
+			}
+			if(this.parseOnLoad){
+				dojo.parser.parse(this.domNode);
+			}
+			if(this._p){ this._p.stop(); }
+			this.onLoad();
 		}
-		if(this.href){
-			this.set("href", this.href);
-		}else if(this.content){
-			this.set("content", this.content);
-		}
-		var parent = this.getParent && this.getParent();
-		if(!parent || !parent.resize){ // top level widget
-			this.resize();
-		}
-		this.inherited(arguments);
-	},
-
-	resize: function(){
-		dojo.forEach(this.getChildren(), function(child){
-			if(child.resize){ child.resize(); }
-		});
-	},
-
-	loadHandler: function(/*String*/response){
-		this.set("content", response);
-	},
-
-	errorHandler: function(err){
-		if(p){ p.stop(); }
-	},
-
-	onLoad: function(){
-		// Stub method to allow the application to connect to.
-		// Called when parsing is done and the content is ready.
-	},
-
-	_setHrefAttr: function(/*String*/href){
-		var p = this._p;
-		if(p){
-			dojo.body().appendChild(p.domNode);
-			p.start();
-		}
-		this.href = href;
-		dojo.xhrGet({
-			url: href,
-			handleAs: "text",
-			load: dojo.hitch(this, "loadHandler"),
-			error: dojo.hitch(this, "errorHandler")
-		});
-	},
-
-	_setContentAttr: function(/*String|DomNode*/data){
-		this.destroyDescendants();
-		if(typeof data === "object"){
-			this.domNode.appendChild(data);
-		}else{
-			this.domNode.innerHTML = data;
-		}
-		if(this.parseOnLoad){
-			dojo.parser.parse(this.domNode);
-		}
-		if(this._p){ this._p.stop(); }
-		this.onLoad();
-	}
-});
-
-
-return dojox.mobile.ContentPane;
+	});
 });

mobile/EdgeToEdgeCategory.js
@@ -1,22 +1,8 @@
-define([
-  "dojo",
-  "dijit",
-  "dojox",
-  "dojox/mobile/RoundRectCategory"], function(dojo, dijit, dojox){
-	// module:
-	//		dojox/mobile/EdgeToEdgeCategory
-	// summary:
-	//		TODOC
-
-dojo.declare(
-	"dojox.mobile.EdgeToEdgeCategory",
-	dojox.mobile.RoundRectCategory,
-{
-	buildRendering: function(){
-		this.inherited(arguments);
-		this.domNode.className = "mblEdgeToEdgeCategory";
-	}
-});
-
-return dojox.mobile.EdgeToEdgeCategory;
+define(["./RoundRectCategory"],function(RoundRectCategory){
+	return dojo.declare("dojox.mobile.EdgeToEdgeCategory", RoundRectCategory, {
+		buildRendering: function(){
+			this.inherited(arguments);
+			this.domNode.className = "mblEdgeToEdgeCategory";
+		}
+	});
 });

mobile/EdgeToEdgeDataList.js
@@ -1,19 +1,7 @@
-define([
-  "dojo",
-  "dijit",
-  "dojox",
-  "dojox/mobile/EdgeToEdgeList",
-  "dojox/mobile/_DataListMixin"], function(dojo, dijit, dojox){
+define(["./EdgeToEdgeList", "./_DataListMixin"], function(EdgeToEdgeList,DataListMixin){
 	// module:
 	//		dojox/mobile/EdgeToEdgeDataList
 	// summary:
 	//		TODOC
-
-dojo.declare(
-	"dojox.mobile.EdgeToEdgeDataList",
-	[dojox.mobile.EdgeToEdgeList, dojox.mobile._DataListMixin],
-{
-});
-
-return dojox.mobile.EdgeToEdgeDataList;
+	return dojo.declare("dojox.mobile.EdgeToEdgeDataList", [EdgeToEdgeList,DataListMixin],{});
 });

mobile/ExpandingTextArea.js
@@ -1,6 +1,6 @@
-define("dojox/mobile/ExpandingTextArea", ["dojo", "dijit", "dojox", "dojox/mobile/TextArea", "dijit/form/_ExpandingTextAreaMixin"], function(dojo, dijit, dojox){
+define(["./TextArea", "dijit/form/_ExpandingTextAreaMixin"], function(TextArea,ExpandingTextAreaMixin){
 
-dojo.declare("dojox.mobile.ExpandingTextArea", [dojox.mobile.TextArea, dijit.form._ExpandingTextAreaMixin], {
+	return dojo.declare("dojox.mobile.ExpandingTextArea", [TextArea, ExpandingTextAreaMixin], {
 		// summary:
 		//		Non-templated TEXTAREA widget with the capability to adjust it's height according to the amount of data.
 		//
@@ -15,7 +15,5 @@ dojo.declare("dojox.mobile.ExpandingTextArea", [dojox.mobile.TextArea, dijit.for
 		// |	<textarea dojoType="dojox.mobile.ExpandingTextArea">...</textarea>
 
 		baseClass: "mblTextArea mblExpandingTextArea"
-});
-
-return dojox.mobile.ExpandingTextArea;
+	});
 });

mobile/FixedSplitter.js
@@ -1,10 +1,4 @@
-define([
-  "dojo",
-  "dijit",
-  "dojox",
-  "dijit/_WidgetBase",
-  "dijit/_Container",
-  "dijit/_Contained"], function(dojo, dijit, dojox){
+define(["dojo/_base/html","dojo/_base/lang",  "dojo/_base/array", "dijit/_WidgetBase","dijit/_Container","dijit/_Contained"],function(dhtml,dlang,darray,WidgetBase,Container,Contained){
 	// module:
 	//		dojox/mobile/FixedSplitter
 	// summary:
@@ -28,77 +22,56 @@ define([
 	// |		</div>
 	// |	</div>
 
-dojo.declare(
-	"dojox.mobile.FixedSplitter",
-	[dijit._WidgetBase, dijit._Container, dijit._Contained],
-{
-	orientation: "H", // "H" or "V"
+	return dojo.declare("dojox.mobile.FixedSplitter", [WidgetBase,Container,Contained], {
+		orientation: "H", // "H" or "V"
 
-	isContainer: true,
+		isContainer: true,
 
-	buildRendering: function(){
-		this.domNode = this.containerNode = this.srcNodeRef ? this.srcNodeRef : dojo.doc.createElement("DIV");
-		dojo.addClass(this.domNode, "mblFixedSpliter");
-	},
+		buildRendering: function(){
+			this.domNode = this.containerNode = this.srcNodeRef ? this.srcNodeRef : dojo.doc.createElement("DIV");
+			dojo.addClass(this.domNode, "mblFixedSpliter");
+		},
 
-	startup: function(){
-		if(this._started){ return; }
-		var children = dojo.filter(this.domNode.childNodes, function(node){ return node.nodeType == 1; });
-		dojo.forEach(children, function(node){
-			dojo.addClass(node, "mblFixedSplitterPane"+this.orientation);
-		}, this);
-		this.inherited(arguments);
+		startup: function(){
+			if(this._started){ return; }
+			var children = dojo.filter(this.domNode.childNodes, function(node){ return node.nodeType == 1; });
+			dojo.forEach(children, function(node){
+				dojo.addClass(node, "mblFixedSplitterPane"+this.orientation);
+			}, this);
+			this.inherited(arguments);
+	
+			var _this = this;
+			setTimeout(function(){
+				var parent = _this.getParent && _this.getParent();
+				if(!parent || !parent.resize){ // top level widget
+					_this.resize();
+				}
+			}, 0);
+		},
+	
+		resize: function(){
+			this.layout();
+		},
 
-		var _this = this;
-		setTimeout(function(){
-			var parent = _this.getParent && _this.getParent();
-			if(!parent || !parent.resize){ // top level widget
-				_this.resize();
+		layout: function(){
+			var sz = this.orientation == "H" ? "w" : "h";
+			var children = dojo.filter(this.domNode.childNodes, function(node){ return node.nodeType == 1; });
+			var offset = 0;
+			for(var i = 0; i < children.length; i++){
+				dojo.marginBox(children[i], this.orientation == "H" ? {l:offset} : {t:offset});
+				if(i < children.length - 1){
+					offset += dojo.marginBox(children[i])[sz];
+				}
 			}
-		}, 0);
-	},
-
-	resize: function(){
-		this.layout();
-	},
-
-	layout: function(){
-		var sz = this.orientation == "H" ? "w" : "h";
-		var children = dojo.filter(this.domNode.childNodes, function(node){ return node.nodeType == 1; });
-		var offset = 0;
-		for(var i = 0; i < children.length; i++){
-			dojo.marginBox(children[i], this.orientation == "H" ? {l:offset} : {t:offset});
-			if(i < children.length - 1){
-				offset += dojo.marginBox(children[i])[sz];
-			}
+	
+			var l = dojo.marginBox(this.domNode)[sz] - offset;
+			var props = {};
+			props[sz] = l;
+			dojo.marginBox(children[children.length - 1], props);
+	
+			dojo.forEach(this.getChildren(), function(child){
+				if(child.resize){ child.resize(); }
+			});
 		}
-
-		var l = dojo.marginBox(this.domNode)[sz] - offset;
-		var props = {};
-		props[sz] = l;
-		dojo.marginBox(children[children.length - 1], props);
-
-		dojo.forEach(this.getChildren(), function(child){
-			if(child.resize){ child.resize(); }
-		});
-	}
-});
-
-dojo.declare(
-	"dojox.mobile.FixedSplitterPane",
-	[dijit._WidgetBase, dijit._Container, dijit._Contained],
-{
-	buildRendering: function(){
-		this.inherited(arguments);
-		dojo.addClass(this.domNode, "mblFixedSplitterPane");
-	},
-
-	resize: function(){
-		dojo.forEach(this.getChildren(), function(child){
-			if(child.resize){ child.resize(); }
-		});
-	}
-});
-
-return dojox.mobile.FixedSplitter;
+	});
 });

commit 9b8f62d65391b9cd7c97e4fba33cd7c6d4ce7246

fix regression in dojox/mobile/ComboBox.js caused by r24548
git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24599 560b804f-0ae3-0310-86f3-f6aa0a117693

mobile/ComboBox.js
@@ -8,9 +8,7 @@ define(["./TextBox", "./_ComboBoxMenu", "dijit/form/_AutoCompleterMixin"], funct
 		// dropDownClass: [protected extension] String
 		//		Name of the dropdown widget class used to select a date/time.
 		//		Subclasses should specify this.
-		//
-		//dropDownClass: "dojox.mobile._ComboBoxMenu",
-		dropDownClass: ComboBoxMenu,
+		dropDownClass: "dojox.mobile._ComboBoxMenu",
 
 		// initially disable selection since iphone displays selection handles that makes it hard to pick from the list
 		selectOnClick: false,