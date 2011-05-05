aa56e30683d664feac4d158c782014b010d282a1
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2011-05-05 13:34:11 +0000

    Fixes #12855 !strict. Renamed the select() method to "selectItem" and moved those methods to RoundRectList.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24658 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/EdgeToEdgeList.js
+++ mobile/EdgeToEdgeList.js
@@ -1,44 +1,8 @@
-define([
-  "dojo",
-  "dijit",
-  "dojox",
-  "dojox/mobile/RoundRectList"], function(dojo, dijit, dojox){
-	// module:
-	//		dojox/mobile/EdgeToEdgeList
-	// summary:
-	//		TODOC
-
-dojo.declare(
-	"dojox.mobile.EdgeToEdgeList",
-	dojox.mobile.RoundRectList,
-{
-	stateful: false, // keep the selection state or not
-	buildRendering: function(){
-		this.inherited(arguments);
-		this.domNode.className = "mblEdgeToEdgeList";
-	},
-
-	_setStatefulAttr: function(stateful){
-		this.stateful = stateful;
-		dojo.forEach(this.getChildren(), function(child){
-			child.setArrow && child.setArrow();
-		});
-	},
-
-	deselect: function(/*ListItem*/item){
-		item.deselect();
-	},
-
-	deselectAll: function(){
-		dojo.forEach(this.getChildren(), function(child){
-			child.deselect && child.deselect();
-		});
-	},
-
-	select: function(/*ListItem*/item){
-		item.select();
-	}
-});
-
-return dojox.mobile.EdgeToEdgeList;
+define(["./RoundRectList"], function(RoundRectList){
+	return dojo.declare("dojox.mobile.EdgeToEdgeList", RoundRectList, {
+		buildRendering: function(){
+			this.inherited(arguments);
+			this.domNode.className = "mblEdgeToEdgeList";
+		}
+	});
 });
--- mobile/RoundRectList.js
+++ mobile/RoundRectList.js
@@ -9,6 +9,7 @@ define(["dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained"], function(W
 		iconBase: "",
 		iconPos: "",
 		select: "", // "single", "multiple", or ""
+		stateful: false, // keep the selection state or not
 
 		buildRendering: function(){
 			this.domNode = this.containerNode = this.srcNodeRef || dojo.doc.createElement("UL");
@@ -17,6 +18,27 @@ define(["dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained"], function(W
 	
 		onCheckStateChanged: function(/*Widget*/listItem, /*String*/newState){
 			// Stub function to connect to from your application.
+		},
+
+		_setStatefulAttr: function(stateful){
+			this.stateful = stateful;
+			dojo.forEach(this.getChildren(), function(child){
+				child.setArrow && child.setArrow();
+			});
+		},
+
+		deselectItem: function(/*ListItem*/item){
+			item.deselectItem();
+		},
+
+		deselectAll: function(){
+			dojo.forEach(this.getChildren(), function(child){
+				child.deselect && child.deselect();
+			});
+		},
+
+		selectItem: function(/*ListItem*/item){
+			item.select();
 		}
 	});
 });
