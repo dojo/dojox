030123e21ae9b80490047c6945269ee4774ba0dc
Chris Mitchell <ccmitchellusa@gmail.com>
2011-05-13 12:51:08 +0000

    fixes #12927 duplicate dojox.mobile definitions because of incorrect dojox/mobile.js define !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@24848 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile.js
+++ mobile.js
@@ -1,4 +1,4 @@
-define(["dojox/mobile/_base"], function(base){
+define([".", "dojo/_base/kernel", "dojox/mobile/_base"], function(dojox, dojo, base){
 	dojo.getObject("mobile", true, dojox);
 	dojo.experimental("dojox.mobile");
 	return dojox.mobile;
