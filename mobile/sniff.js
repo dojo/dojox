e5a4940c614a2e5e6b388339a79bf0a1735e9a67
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2011-08-25 08:40:23 +0000

    Fixes #13754 !strict. Added the 'force' option to has.add to replace the existing tests.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@26355 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/sniff.js
+++ mobile/sniff.js
@@ -6,10 +6,10 @@ define([
 	var ua = navigator.userAgent;
 
 	// BlackBerry (OS 6 or later only)
-	has.add('bb', ua.indexOf("BlackBerry") >= 0 && parseFloat(ua.split("Version/")[1]) || undefined);
+	has.add('bb', ua.indexOf("BlackBerry") >= 0 && parseFloat(ua.split("Version/")[1]) || undefined, undefined, true);
 
 	// Android
-	has.add('android', parseFloat(ua.split("Android ")[1]) || undefined);
+	has.add('android', parseFloat(ua.split("Android ")[1]) || undefined, undefined, true);
 
 	// iPhone, iPod, or iPad
 	// If iPod or iPad is detected, in addition to has('ipod') or has('ipad'),
@@ -18,13 +18,13 @@ define([
 		var p = RegExp.$1.replace(/P/,/p/);
 		var v = ua.match(/OS ([\d_]+)/) ? RegExp.$1 : "1";
 		var os = parseFloat(v.replace(/_/, '.').replace(/_/g, ''));
-		has.add(p, os);
-		has.add('iphone', os);
+		has.add(p, os, undefined, true);
+		has.add('iphone', os, undefined, true);
 	}
 
 	if(has("webkit")){
 		has.add('touch', (typeof win.doc.documentElement.ontouchstart != "undefined" &&
-			navigator.appVersion.indexOf("Mobile") != -1) || !!has('android'));
+			navigator.appVersion.indexOf("Mobile") != -1) || !!has('android'), undefined, true);
 	}
 
 	return has;
