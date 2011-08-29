c592bfa2312603c4576341cd85e3d6acce10235c
Evan Huang <evanhuangwei@gmail.com>
2011-08-29 14:32:07 +0000

    Fixes #13770, by not changing focusing when clicking at the scroll bar area(or up/down arrow)
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@26388 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/_FocusManager.js
+++ grid/_FocusManager.js
@@ -24,6 +24,8 @@ return declare("dojox.grid._FocusManager", null, {
 		this.headerMenu = this.grid.headerMenu;
 		this._connects.push(connect.connect(this.grid.domNode, "onfocus", this, "doFocus"));
 		this._connects.push(connect.connect(this.grid.domNode, "onblur", this, "doBlur"));
+		this._connects.push(connect.connect(this.grid.domNode, "mousedown", this, "_mouseDown"));
+		this._connects.push(connect.connect(this.grid.domNode, "mouseup", this, "_mouseUp"));
 		this._connects.push(connect.connect(this.grid.domNode, "oncontextmenu", this, "doContextMenu"));
 		this._connects.push(connect.connect(this.grid.lastFocusNode, "onfocus", this, "doLastNodeFocus"));
 		this._connects.push(connect.connect(this.grid.lastFocusNode, "onblur", this, "doLastNodeBlur"));
@@ -581,6 +583,10 @@ return declare("dojox.grid._FocusManager", null, {
 			event.stop(e);
 			return;
 		}
+		// don't change focus if clicking on scroller bar
+		if(this._clickFocus){
+			return;
+		}
 		// do not focus for scrolling if grid is about to blur
 		if(!this.tabbingOut){
 			this.focusHeader();
@@ -621,6 +627,15 @@ return declare("dojox.grid._FocusManager", null, {
 	},
 	doColHeaderBlur: function(e){
 		html.toggleClass(e.target, this.focusClass, false);
+	},
+	_mouseDown: function(e){
+		// a flag indicating grid is being focused by clicking
+		this._clickFocus = dojo.some(this.grid.views.views, function(v){
+			return v.scrollboxNode === e.target;
+		});
+	},
+	_mouseUp: function(e){
+		this._clickFocus = false;
 	}
 });
 });
