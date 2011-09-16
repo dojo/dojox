8ada3ff8c870662803844b4991be6363abf14acd
Douglas Hays <doughays@dojotoolkit.org>
2011-09-16 12:14:23 +0000

    Fixes #13802, #13829, #13830.  Add default z-index of Tooltip and Overlay to 2000 to match TooltipDialog in dijit.  Add full screen cover to intercept click events that can close either.  The default is to close Tooltip but not Overlay.  When onclick outside the popup fires, Opener calls widget.onBlur.  If false is returned, then the widget ignores the click.  Anything else will cause widget.hide() to be called.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@26586 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/Opener.js
+++ mobile/Opener.js
@@ -1,51 +1,62 @@
 define([
 	"dojo/_base/declare",
+	"dojo/_base/lang",
 	"dojo/_base/window",
 	"dojo/dom-class",
 	"dojo/dom-construct",
 	"dojo/dom-style",
+	"dojo/dom-geometry",
 	"./Tooltip",
 	"./Overlay"
-], function(declare, win, domClass, domConstruct, domStyle, Tooltip, Overlay){
+], function(declare, lang, win, domClass, domConstruct, domStyle, domGeometry, Tooltip, Overlay){
 
 	/*=====
 		Tooltip = dojox.mobile.Tooltip;
 		Overlay = dojox.mobile.Overlay;
 	=====*/
-	var cls = declare("dojox.mobile.Opener", domClass.contains(win.doc.documentElement, "dj_phone") ? Overlay : Tooltip, {
+	var isOverlay = domClass.contains(win.doc.documentElement, "dj_phone");
+	var cls = declare("dojox.mobile.Opener", isOverlay ? Overlay : Tooltip, {
 		// summary:
 		//		A non-templated popup widget that will use either Tooltip or Overlay depending on screen size
 		//
+		buildRendering: function(){
+			this.inherited(arguments);
+			this.cover = domConstruct.create('div', { onclick: lang.hitch(this, '_onBlur'), 'class': 'mblOpenerUnderlay', style: { top:'0px', left:'0px', width:'0px', height:'0px', position: isOverlay ? 'absolute' : 'fixed', backgroundColor:'transparent', overflow:'hidden' }}, this.domNode, 'first');
+		},
+
 		onShow: function(/*DomNode*/node){},
 		onHide: function(/*DomNode*/node, /*Anything*/v){},
+
 		show: function(node, positions){
 			this.node = node;
 			this.onShow(node);
-			if(!this.cover){
-				this.cover = domConstruct.create('div', {style: {position:'absolute', top:'0px', left:'0px', width:'100%', height:'100%', backgroundColor:'transparent' }}, this.domNode, 'before');
-				this.connect(this.cover, "onclick", "_onBlur");
+			if(isOverlay){
+				setTimeout(lang.hitch(this, function(){ // show cover after positioning popup
+					var pos = domGeometry.position(this.domNode, false);
+					var size = Math.max(pos.w+pos.x, pos.h+pos.y); // largest size in case of rotation
+					domStyle.set(this.cover, { top:-pos.y+'px', left:-pos.x+'px', width:size+'px', height:size+'px' });
+				}), 0);
+			}else{
+				var size = Math.max(win.doc.documentElement.scrollHeight, win.body().scrollHeight, win.doc.documentElement.clientHeight,
+							win.doc.documentElement.scrollWidth, win.body().scrollWidth, win.doc.documentElement.clientWidth); // largest size in case of rotation
+				domStyle.set(this.cover, { width:size+'px', height:size+'px' });
 			}
-			domStyle.set(this.cover, "visibility", "visible");
 			return this.inherited(arguments);
 		},
 
 		hide: function(/*Anything*/ val){
-			domStyle.set(this.cover, "visibility", "hidden");
 			this.inherited(arguments);
+			domStyle.set(this.cover, { height:'0px' });
 			this.onHide(this.node, val);
 		},
 
 		_onBlur: function(e){
-			if(this.onBlur(e) !== false){ // only exactly false prevents hide()
+			var ret = this.onBlur(e);
+			if(ret !== false){ // only exactly false prevents hide()
 			        this.hide(e);
 			}
-		},
-
-		destroy: function(){
-			this.inherited(arguments);
-			domConstruct.destroy(this.cover);
+			return ret;
 		}
-
 	});
 	cls.prototype.baseClass += " mblOpener"; // add to either mblOverlay or mblTooltip
 	return cls;
