caee5be6f1058b314fc85f4a67b61f504f358e2e
Rawld Gill <rgill@altoviso.com>
2011-09-27 10:15:01 +0000

    updated default profiles; refs #14005
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@26685 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox.profile.js
+++ dojox.profile.js
@@ -1,69 +1,67 @@
-(function(){
-	var testResourceRe = /\/tests\//,
+var testResourceRe = /\/tests\//,
 
-		copyOnly = function(mid){
-			var list = {
-				"dojox/dojox.profile":1,
-				"dojox/package.json":1,
-				"dojox/mobile/themes/common/compile":1
-			};
-			return (mid in list) || /^dojox\/resources\//.test(mid);
-		},
-
-		excludes = [
-			"secure",
-			"cometd",
-			"data/(demos|ItemExplorer|StoreExplorer|restListener)",
-			"drawing",
-			"editor/plugins/(ResizeTableColumn|SpellCheck)",
-			"embed/(IE)",
-			"flash",
-			"gantt",
-			"help",
-			"image/(Gallery|SlideShow|ThumbnailPicker)",
-			"jq",
-			"jsonPath",
-			"lang/(aspect|async|docs|observable|oo|typed|functional/(binrec|curry|linrec|listcomp|multirec|numrec|tailrec|util|zip))",
-			"layout/(BorderContainer|dnd|ext-dijit)",
-			"mobile/app/",
-			"rails",
-			"robot",
-			"socket/Reconnect",
-			"storage",
-			"sql",
-			"widget/(AnalogGauge|AutoRotator|BarGauge|Calendar|CalendarFx|CalendarViews|DataPresentation|DocTester|DynamicTooltip|FeedPortlet|FilePicker|FisheyeList|gauge|Iterator|Loader|Pager|Portlet|RollingList|Rotator|rotator|SortList|UpgradeBar|Wizard)",
-			"wire",
-			"xmpp",
-			"NodeList"
-		],
-
-		excludesRe = new RegExp(("^dojox/(" + excludes.join("|") + ")").replace(/\//, "\\/")),
-
-		usesDojoProvideEtAl = function(mid){
-			return excludesRe.test(mid);
+	copyOnly = function(mid){
+		var list = {
+			"dojox/dojox.profile":1,
+			"dojox/package.json":1,
+			"dojox/mobile/themes/common/compile":1
 		};
+		return (mid in list) || /^dojox\/resources\//.test(mid);
+	},
 
-	return {
-		resourceTags:{
-			test: function(filename, mid){
-				return testResourceRe.test(mid);
-			},
+	excludes = [
+		"secure",
+		"cometd",
+		"data/(demos|ItemExplorer|StoreExplorer|restListener)",
+		"drawing",
+		"editor/plugins/(ResizeTableColumn|SpellCheck)",
+		"embed/(IE)",
+		"flash",
+		"gantt",
+		"help",
+		"image/(Gallery|SlideShow|ThumbnailPicker)",
+		"jq",
+		"jsonPath",
+		"lang/(aspect|async|docs|observable|oo|typed|functional/(binrec|curry|linrec|listcomp|multirec|numrec|tailrec|util|zip))",
+		"layout/(BorderContainer|dnd|ext-dijit)",
+		"mobile/app/",
+		"rails",
+		"robot",
+		"socket/Reconnect",
+		"storage",
+		"sql",
+		"widget/(AnalogGauge|AutoRotator|BarGauge|Calendar|CalendarFx|CalendarViews|DataPresentation|DocTester|DynamicTooltip|FeedPortlet|FilePicker|FisheyeList|gauge|Iterator|Loader|Pager|Portlet|RollingList|Rotator|rotator|SortList|UpgradeBar|Wizard)",
+		"wire",
+		"xmpp",
+		"NodeList"
+	],
 
-			copyOnly: function(filename, mid){
-				return copyOnly(mid);
-			},
+	excludesRe = new RegExp(("^dojox/(" + excludes.join("|") + ")").replace(/\//, "\\/")),
 
-			amd: function(filename, mid){
-				return !testResourceRe.test(mid) && !copyOnly(mid) && !usesDojoProvideEtAl(mid) && /\.js$/.test(filename);
-			},
+	usesDojoProvideEtAl = function(mid){
+		return excludesRe.test(mid);
+	};
 
-			miniExclude: function(filename, mid){
-				return 0;
-			}
+var profile = {
+	resourceTags:{
+		test: function(filename, mid){
+			return testResourceRe.test(mid);
 		},
 
-		trees:[
-			[".", ".", /(\/\.)|(~$)/]
-		]
-	};
-})()
+		copyOnly: function(filename, mid){
+			return copyOnly(mid);
+		},
+
+		amd: function(filename, mid){
+			return !testResourceRe.test(mid) && !copyOnly(mid) && !usesDojoProvideEtAl(mid) && /\.js$/.test(filename);
+		},
+
+		miniExclude: function(filename, mid){
+			return 0;
+		}
+	},
+
+	trees:[
+		[".", ".", /(\/\.)|(~$)/]
+	]
+};
