168fb1c8987d2cb76d4f6005cdbdff8ba18ebb57
Adam Peller <github@peller.org>
2011-11-19 01:35:25 +0000

    Reuse paste, cancel strings. Refs #14207
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.7/dojox@27064 560b804f-0ae3-0310-86f3-f6aa0a117693

--- editor/plugins/SafePaste.js
+++ editor/plugins/SafePaste.js
@@ -29,6 +29,7 @@ dojo.declare("dojox.editor.plugins.SafePaste", [dojox.editor.plugins.PasteFromWo
 		this._filters = this._filters.slice(0); 
 		var strings = dojo.i18n.getLocalization("dojox.editor.plugins", "SafePaste");
 		dojo.mixin(strings, dojo.i18n.getLocalization("dijit", "common"));
+		strings.cancel = strings.buttonCancel;
 		dojo.mixin(strings, dojo.i18n.getLocalization("dijit._editor", "commands"));
 
 		this._uId = dijit.getUniqueId(this.editor.id);
