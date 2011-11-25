f4534b2660904f3895774b11c26ba0814112f167
Patrick Ruzand <pruzand@gmail.com>
2011-11-25 10:20:46 +0000

    add test_base.html
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@27126 560b804f-0ae3-0310-86f3-f6aa0a117693

--- gfx/tests/module.js
+++ gfx/tests/module.js
@@ -4,6 +4,7 @@ try{
 	dojo.require("dojox.gfx.tests.matrix");
 	dojo.require("dojox.gfx.tests.decompose");
 	doh.registerUrl("GFX: Utils", dojo.moduleUrl("dojox", "gfx/tests/test_utils.html"), 3600000);
+	doh.registerUrl("GFX: Base", dojo.moduleUrl("dojox", "gfx/tests/test_base.html"), 3600000);
 }catch(e){
 	doh.debug(e);
 }
