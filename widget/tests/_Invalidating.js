de342cd9ebea8c39faaba43eb23457049bea6c47
Christophe Jolif <cjolif@gmail.com>
2011-12-02 10:38:56 +0000

    fix test
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@27205 560b804f-0ae3-0310-86f3-f6aa0a117693

--- widget/tests/_Invalidating.js
+++ widget/tests/_Invalidating.js
@@ -4,13 +4,13 @@ define(["doh", "dojo/_base/declare", "../_Invalidating", "dijit/_WidgetBase"],
 		function test_Lifecycle(t){
 			var C = declare("MyWidget", [_WidgetBase, _Invalidating], {
 				constructor: function(){
-					this.watchedProperties = ["a"];
-					this.addWatchedProperties(["b"]);
+					this.invalidatingProperties = ["a"];
+					this.addInvalidatingProperties(["b"]);
 				}					
 			});
 			var o = new C();
 			o.startup();
-			t.is(["a", "b"], c.watchedProperties);
+			t.is(["a", "b"], o.invalidatingProperties);
 		}
 	]);
 });
