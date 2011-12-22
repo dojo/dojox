e1752eda8aaf60e2681484201808fe3ebbb1b834
Patrick Ruzand <pruzand@gmail.com>
2011-12-22 09:59:44 +0000

    fixes #14521, patch thx Marcelo Blechner (IBM,CCLA) !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@27459 560b804f-0ae3-0310-86f3-f6aa0a117693

--- gfx/fx.js
+++ gfx/fx.js
@@ -63,6 +63,17 @@ define(["dojo/_base/lang", "./_base", "./matrix", "dojo/_base/Color", "dojo/_bas
 				ret.push(this.original);
 				return;
 			}
+ 			// Adding support for custom matrices
+ 			if(t.name == "matrix"){
+ 				if((t.start instanceof m.Matrix2D) && (t.end instanceof m.Matrix2D)){
+ 					var transfMatrix = new m.Matrix2D();
+ 					for(var p in t.start) {
+ 						transfMatrix[p] = (t.end[p] - t.start[p])*r + t.start[p];
+ 					}
+ 					ret.push(transfMatrix);
+ 				}
+ 				return;
+ 			}
 			if(!(t.name in m)){ return; }
 			var f = m[t.name];
 			if(typeof f != "function"){
