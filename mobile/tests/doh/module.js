b3251c1cfb01208e17af7a119b5ab18d68b18db8
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2012-02-10 14:37:54 +0000

    Refs #14248 !strict Updated DOH tests.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@27832 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/tests/doh/module.js
+++ mobile/tests/doh/module.js
@@ -3,6 +3,10 @@ dojo.provide("dojox.mobile.tests.doh.module");
 try{
 	doh.registerUrl("dojox.mobile.tests.doh.Button", dojo.moduleUrl("dojox.mobile", "tests/doh/Button.html"),999999);
 	doh.registerUrl("dojox.mobile.tests.doh.Button", dojo.moduleUrl("dojox.mobile", "tests/doh/Button_Programmatic.html"),999999);
+	doh.registerUrl("dojox.mobile.tests.doh.Heading", dojo.moduleUrl("dojox.mobile", "tests/doh/Heading.html"),999999);
+	doh.registerUrl("dojox.mobile.tests.doh.Heading", dojo.moduleUrl("dojox.mobile", "tests/doh/Heading2.html"),999999);
+	doh.registerUrl("dojox.mobile.tests.doh.Heading", dojo.moduleUrl("dojox.mobile", "tests/doh/Heading_Programmatic.html"),999999);
+	doh.registerUrl("dojox.mobile.tests.doh.Heading", dojo.moduleUrl("dojox.mobile", "tests/doh/Heading2_Programmatic.html"),999999);
 }catch(e){
 	doh.debug(e);
 }
