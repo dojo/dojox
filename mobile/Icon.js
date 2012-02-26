75089db78a94b42fcf910d3e80c45b027fbd1b39
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2012-02-26 05:11:57 +0000

    Fixes #14892 !strict Added an iconUtils wrapper widget.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28010 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ mobile/Icon.js
@@ -0,0 +1,45 @@
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/dom-class",
+	"dojo/dom-construct",
+	"./iconUtils"
+], function(declare, lang, domClass, domConstruct, iconUtils){
+
+	// module:
+	//		dojox/mobile/Icon
+	// summary:
+	//		A wrapper for image icon, css sprite icon, or DOM Button.
+
+	return declare("dojox.mobile.Icon", null,{
+		// summary:
+		//		A DOM Button wrapper.
+		// description:
+		//		This widget creates a specified DOM Button.
+		// example:
+		//		Image icon:
+		//	|	<div data-dojo-type="dojox.mobile.Icon"
+		//	|		data-dojo-props='icon:"images/tab-icon-12h.png"'></div>
+		//
+		//		CSS sprite icon:
+		//	|	<div data-dojo-type="dojox.mobile.Icon"
+		//	|		data-dojo-props='icon:"images/tab-icons.png",iconPos:"29,116,29,29"'></div>
+		//
+		//		DOM Button:
+		//	|	<div data-dojo-type="dojox.mobile.Icon"
+		//	|		data-dojo-props='icon:"mblDomButtonBlueCircleArrow"'></div>
+
+		icon: "",
+		iconPos: "",
+		alt: "",
+		tag: "div",
+
+		constructor: function(args, node){
+			if(args){
+				lang.mixin(this, args);
+			}
+			this.domNode = node || domConstruct.create(this.tag);
+			iconUtils.createIcon(this.icon, this.iconPos, null, this.alt, this.domNode);
+		}
+	});
+});
