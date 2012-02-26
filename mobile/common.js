c3e1e00ea30dc364004ba29875a8492e586eea90
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2012-02-26 03:11:00 +0000

    Refs #14885 !strict Added resize event handling for Android. Thanks onoat for the patch.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28004 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/common.js
+++ mobile/common.js
@@ -5,11 +5,12 @@ define([
 	"dojo/_base/lang",
 	"dojo/_base/window",
 	"dojo/dom-class",
+	"dojo/dom-construct",
 	"dojo/ready",
 	"dijit/registry",
 	"./sniff",
 	"./uacss" // (no direct references)
-], function(array, config, connect, lang, win, domClass, ready, registry, has){
+], function(array, config, connect, lang, win, domClass, domConstruct, ready, registry, has){
 
 	var dm = lang.getObject("dojox.mobile", true);
 /*=====
@@ -138,6 +139,9 @@ define([
 		if(dm.disableResizeAll){ return; }
 		connect.publish("/dojox/mobile/resizeAll", [evt, root]); // back compat
 		connect.publish("/dojox/mobile/beforeResizeAll", [evt, root]);
+		if(dm._hiding === false){
+			win.body().style.minHeight = dm.getScreenSize().h + "px";
+		} 
 		dm.updateOrient();
 		dm.detectScreenSize();
 		var isTopLevel = function(w){
@@ -201,6 +205,15 @@ define([
 			if(config["mblAlwaysHideAddressBar"] === true){
 				f = dm.hideAddressBar;
 			}
+		}
+		if(has('android') && win.global.onorientationchange !== undefined){
+			var _f = f;
+			f = function(evt){
+				var _conn = connect.connect(null, "onresize", null, function(e){
+					connect.disconnect(_conn);
+					_f(e);
+				});
+			};
 		}
 		connect.connect(null, win.global.onorientationchange !== undefined
 			? "onorientationchange" : "onresize", null, f);
