3d0e3db8bac35a87f8dc7e52044a2da4d5eafa30
Kris Zyp <kriszyp@gmail.com>
2012-02-28 20:11:54 +0000

    Fix skip parameter name for CouchDBRestStore, fixes #14815 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28042 560b804f-0ae3-0310-86f3-f6aa0a117693

--- data/CouchDBRestStore.js
+++ data/CouchDBRestStore.js
@@ -30,7 +30,7 @@ dojo.declare("dojox.data.CouchDBRestStore",
 			// 		This only differs from JsonRestStore in that it, will put the query string the query part of the URL and it handles start and count
 			args.query = args.query || '_all_docs?';
 			if(args.start){
-				args.query = (args.query ? (args.query + '&') : '') + 'startkey=' + args.start;
+				args.query = (args.query ? (args.query + '&') : '') + 'skip=' + args.start;
 				delete args.start;
 			}
 			if(args.count){
