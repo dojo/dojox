b58042c8d94198596dbda2cb9eb933db26098b87
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2012-03-06 04:25:45 +0000

    Refs #14827 !strict Android workaround. Applied a patch from onoat. Thanks onoat for the patch.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28096 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/common.js
+++ mobile/common.js
@@ -87,18 +87,16 @@ define([
 		//		Internal function to hide the address bar.
 		scrollTo(0, 1);
 		dm._hidingTimer = (dm._hidingTimer == 0) ? 200 : dm._hidingTimer * 2;
-		if(dm.isAddressBarHidden() || dm._hidingTimer > dm.hideAddressBarWait){
-			// Succeeded to hide address bar, or failed but timed out 
-			win.body().style.minHeight = dm.getScreenSize().h + "px";
-			dm.resizeAll();
-			dm._hiding = false;
-		}else{
-			// Failed to hide address bar, so retry after a while
-			setTimeout(dm.hide_1, dm._hidingTimer);
-			if(has('android')){
+		setTimeout(function(){ // wait for a while for "scrollTo" to finish
+			if(dm.isAddressBarHidden() || dm._hidingTimer > dm.hideAddressBarWait){
+				// Succeeded to hide address bar, or failed but timed out 
 				dm.resizeAll();
+				dm._hiding = false;
+			}else{
+				// Failed to hide address bar, so retry after a while
+				setTimeout(dm.hide_1, dm._hidingTimer);
 			}
-		}
+		}, 50); //50ms is an experiential value
 	};
 
 	dm.hideAddressBar = function(/*Event?*/evt){
@@ -111,8 +109,28 @@ define([
 		if(dm.disableHideAddressBar || dm._hiding){ return; }
 		dm._hiding = true;
 		dm._hidingTimer = has('iphone') ? 200 : 0; // Need to wait longer in case of iPhone
-		var minH = has('android') ? (outerHeight / devicePixelRatio) : screen.availHeight;
-		win.body().style.minHeight = minH + "px"; // to ensure enough height for scrollTo to work
+		var minH = screen.availHeight;
+		if(has('android')){
+			minH = outerHeight / devicePixelRatio;
+			// On some Android devices such as Galaxy SII, minH might be 0 at this time.
+			// In that case, retry again after a while. (200ms is an experiential value)
+			if(minH == 0){
+				dm._hiding = false;
+				setTimeout(function(){ dm.hideAddressBar(); }, 200);
+			}
+			// On some Android devices such as HTC EVO, "outerHeight/devicePixelRatio"
+			// is too short to hide address bar, so make it high enough
+			if(minH <= innerHeight){ minH = outerHeight; }
+			// On Android 2.2/2.3, hiding address bar fails when "overflow:hidden" style is
+			// applied to html/body element, so force "overflow:visible" style
+			if(has('android') < 3){
+				win.doc.documentElement.style.overflow = win.body().style.overflow = "visible";
+			}
+		}
+		if(win.body().offsetHeight < minH){ // to ensure enough height for scrollTo to work
+			win.body().style.minHeight = minH + "px";
+			dm._resetMinHeight = true;
+		}
 		setTimeout(dm.hide_1, dm._hidingTimer);
 	};
 
