ac99527398bd19086cfbf9c9391314440b4a43db
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2012-03-27 06:53:20 +0000

    Fixes #15063 !strict. Fixed the setter for values to take a date in numbers in addition to strings.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28252 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/_PickerBase.js
+++ mobile/_PickerBase.js
@@ -70,7 +70,13 @@ define([
 			// summary:
 			//		Sets the slot values.
 			array.forEach(this.getSlots(), function(w, i){
-				w.set("value", a[i]);
+				var v = a[i];
+				if(!isNaN(v)){
+					var arr = [1970, 1, 1];
+					arr.splice(i, 1, v - 0);
+					v = w.format(new Date(arr));
+				}
+				w.set("value", v);
 			});
 		},
 
