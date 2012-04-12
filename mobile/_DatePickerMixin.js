44a00ba809f883427cb73a9777bdbd0b3710f6fe
Yoshiroh Kamiyama <ykami@dojotoolkit.org>
2012-04-12 01:57:30 +0000

    Refs #15063 !strict. Changed the 'new Date(arr)' constructor to 'new Date(arr[0], arr[1], arr[2])' because it is not supported at least by firefox. Thank you edurocher for finding this.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28314 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/_DatePickerMixin.js
+++ mobile/_DatePickerMixin.js
@@ -141,7 +141,7 @@ define([
 				if(!isNaN(v)){
 					var arr = [1970, 1, 1];
 					arr.splice(i, 1, v - 0);
-					v = w.format(new Date(arr));
+					v = w.format(new Date(arr[0], arr[1], arr[2]));
 				}
 				w.set("value", v);
 			});
