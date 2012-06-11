eaa4fb2a79a47712ad5fe5a4a4788d8ccb6449bd
Patrick Ruzand <pruzand@gmail.com>
2012-06-11 11:21:34 +0000

    clean up
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28789 560b804f-0ae3-0310-86f3-f6aa0a117693

--- charting/plot2d/StackedLines.js
+++ charting/plot2d/StackedLines.js
@@ -3,10 +3,9 @@ define(["dojo/_base/declare", "./Stacked"], function(declare, Stacked){
 	return declare("dojox.charting.plot2d.StackedLines", Stacked, {
 		// summary:
 		//		A convenience object to create a stacked line chart.
-		constructor: function(st){
+		constructor: function(){
 			// summary:
 			//		Force our Stacked base to be lines only.
-			// st: dojox.gfx.Stroke
 			this.opt.lines = true;
 		}
 	});
