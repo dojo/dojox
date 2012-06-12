644321bc8d7be92b0019f9f7ba4d4e755b472920
Christophe Jolif <cjolif@gmail.com>
2012-06-12 16:23:30 +0000

    refs #15499. !WidgetList support & tests. Thanks Akira Sudoh, Ed Chatelain (IBM, CCLA). !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28821 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mvc/tests/moduleFullSet.js
+++ mvc/tests/moduleFullSet.js
@@ -8,6 +8,17 @@ define([
 		var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g, "").replace(/^&/, "?");
 
 		// DOH
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_DOMNode-search-results-repeat", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_DOMNode-search-results-repeat.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_mobile-demo", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_mobile-demo.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_new_ref-set-repeat", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_new_ref-set-repeat.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_performance_search-results-repeat", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_performance_search-results-repeat.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_programmatic-repeat-store", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_programmatic-repeat-store.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_repeat_select_cancel", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_repeat_select_cancel.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_repeat_select_manualsave", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_repeat_select_manualsave.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_search-results-repeat", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_search-results-repeat.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_mvc_search-results-repeat-store", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_mvc_search-results-repeat-store.html" + userArgs), 999999);
+		doh.registerUrl("dojox.mvc.tests.WidgetList_tests.doh_new-mvc_label_and_totals", require.toUrl("dojox/mvc/tests/WidgetList_tests/doh_new-mvc_label_and_totals.html" + userArgs), 999999);
+		
 		doh.registerUrl("dojox.mvc.tests.doh_mvc_new_shipto-billto-simple", require.toUrl("dojox/mvc/tests/doh_mvc_new_shipto-billto-simple.html" + userArgs), 999999);
 		doh.registerUrl("dojox.mvc.tests.doh_mvc_new_ref-set-repeat", require.toUrl("dojox/mvc/tests/doh_mvc_new_ref-set-repeat.html" + userArgs), 999999);
 		doh.registerUrl("dojox.mvc.tests.multiattrib.doh_mvc_test_Toolbar", require.toUrl("dojox/mvc/tests/multiattrib/doh_mvc_test_Toolbar.html" + userArgs), 999999);
