337fb539725b605d78b6817fdc10cd6622b1ca31
Dylan Schiemann <dylan@dojotoolkit.org>
2012-06-15 14:40:57 +0000

    refs #14711, missed one AMD conversion, some additional minor updates
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28870 560b804f-0ae3-0310-86f3-f6aa0a117693

--- widget/CalendarFx.js
+++ widget/CalendarFx.js
@@ -1,27 +1,14 @@
-dojo.provide("dojox.widget.CalendarFx");
-dojo.require("dojox.widget.FisheyeLite");
-
-dojo.declare("dojox.widget._FisheyeFX",null, {
+define([
+	"dojo/_base/kernel",
+	"dojo/_base/declare",
+	"dojox/widget/CalendarFisheye",
+	"dojox/widget/_FisheyeFX",
+], function(kernel, declare, CalendarFisheye, _FisheyeFX){
+	kernel.experimental("dojox/widget/CalendarFx");
 	// summary:
-	//		A mixin to add a FisheyeLite effect to the calendar
-	addFx: function(query, fromNode) {
-		//Use the query and base node passed from the calendar view mixin
-		//to select the nodes to attach the event to.
-		dojo.query(query, fromNode).forEach(function(node){
-			new dojox.widget.FisheyeLite({
-				properties: {
-					fontSize: 1.1
-				}
-			}, node);
-		});
-	}
+	//		The Root class for Calendar effects
+	return declare("dojox.widget.CalendarFx", [CalendarFisheye, _FisheyeFX], {
+		// summary:
+		//	The visual effects extensions for dojox/widget/Calendar.
+	});
 });
-
-dojo.declare("dojox.widget.CalendarFisheye",
-	[dojox.widget.Calendar,
-	 dojox.widget._FisheyeFX], {
-	 	// summary:
-	 	//		The standard Calendar. It includes day, month and year views.
-		//		FisheyeLite effects are included.
-	 }
-);
--- widget/CalendarViews.js
+++ widget/CalendarViews.js
@@ -2,5 +2,5 @@ define([
 	"dojo/_base/kernel",
 	"dojox/widget/Calendar"
 ], function(kernel, Calendar){
-	kernel.experimental("dojox.widget.CalendarViews");
+	kernel.experimental("dojox/widget/CalendarViews");
 });
