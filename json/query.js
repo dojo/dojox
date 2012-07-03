a33833f78706dcb778e7215a34b7efb58dab2b0d
Bill Keese <bill@dojotoolkit.org>
2012-07-03 02:02:09 +0000

    another list fix, refs #13101 !strict
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@29112 560b804f-0ae3-0310-86f3-f6aa0a117693

--- json/query.js
+++ json/query.js
@@ -105,51 +105,50 @@ define(["dojo/_base/kernel", "dojo/_base/lang", "dojox", "dojo/_base/array"], fu
 		//		the starting object, various operators can be successively applied, each operating
 		//		on the result of the last operation.
 		//
-		//		Supported Operators:
+		//		Supported Operators
 		//		--------------------
-		//		* .property - This will return the provided property of the object, behaving exactly
+		//
+		//		- .property - This will return the provided property of the object, behaving exactly
 		//		like JavaScript.
-		//		* [expression] - This returns the property name/index defined by the evaluation of
+		//		- [expression] - This returns the property name/index defined by the evaluation of
 		//		the provided expression, behaving exactly like JavaScript.
-		//		* [?expression] - This will perform a filter operation on an array, returning all the
+		//		- [?expression] - This will perform a filter operation on an array, returning all the
 		//		items in an array that match the provided expression. This operator does not
 		//		need to be in brackets, you can simply use ?expression, but since it does not
 		//		have any containment, no operators can be used afterwards when used
 		//		without brackets.
-		//		* [^?expression] - This will perform a distinct filter operation on an array. This behaves
+		//		- [^?expression] - This will perform a distinct filter operation on an array. This behaves
 		//		as [?expression] except that it will remove any duplicate values/objects from the
 		//		result set.
-		//		* [/expression], [\expression], [/expression, /expression] - This performs a sort
+		//		- [/expression], [\expression], [/expression, /expression] - This performs a sort
 		//		operation on an array, with sort based on the provide expression. Multiple comma delimited sort
 		//		expressions can be provided for multiple sort orders (first being highest priority). /
 		//		indicates ascending order and \ indicates descending order
-		//		* [=expression] - This performs a map operation on an array, creating a new array
+		//		- [=expression] - This performs a map operation on an array, creating a new array
 		//		with each item being the evaluation of the expression for each item in the source array.
-		//		* [start:end:step] - This performs an array slice/range operation, returning the elements
+		//		- [start:end:step] - This performs an array slice/range operation, returning the elements
 		//		from the optional start index to the optional end index, stepping by the optional step number.
-		//		* [expr,expr] - This a union operator, returning an array of all the property/index values from
+		//		- [expr,expr] - This a union operator, returning an array of all the property/index values from
 		//		the evaluation of the comma delimited expressions.
-		//		* .* or [*] - This returns the values of all the properties of the current object.
-		//		* $ - This is the root object, If a JSONQuery expression does not being with a $,
+		//		- .* or [*] - This returns the values of all the properties of the current object.
+		//		- $ - This is the root object, If a JSONQuery expression does not being with a $,
 		//		it will be auto-inserted at the beginning.
-		//		* @ - This is the current object in filter, sort, and map expressions. This is generally
+		//		- @ - This is the current object in filter, sort, and map expressions. This is generally
 		//		not necessary, names are auto-converted to property references of the current object
 		//		in expressions.
-		//		*	..property - Performs a recursive search for the given property name, returning
+		//		- ..property - Performs a recursive search for the given property name, returning
 		//		an array of all values with such a property name in the current object and any subobjects
-		//		* expr = expr - Performs a comparison (like JS's ==). When comparing to
+		//		- expr = expr - Performs a comparison (like JS's ==). When comparing to
 		//		a string, the comparison string may contain wildcards * (matches any number of
 		//		characters) and ? (matches any single character).
-		//		* expr ~ expr - Performs a string comparison with case insensitivity.
-		//		* ..[?expression] - This will perform a deep search filter operation on all the objects and
+		//		- expr ~ expr - Performs a string comparison with case insensitivity.
+		//		- ..[?expression] - This will perform a deep search filter operation on all the objects and
 		//		subobjects of the current data. Rather than only searching an array, this will search
 		//		property values, arrays, and their children.
-		//		* $1,$2,$3, etc. - These are references to extra parameters passed to the query
+		//		- $1,$2,$3, etc. - These are references to extra parameters passed to the query
 		//		function or the evaluator function.
-		//		* +, -, /, *, &, |, %, (, ), <, >, <=, >=, != - These operators behave just as they do
+		//		- +, -, /, *, &, |, %, (, ), <, >, <=, >=, != - These operators behave just as they do
 		//		in JavaScript.
-		//
-		//
 		//
 		// 	|	dojox.json.query(queryString,object)
 		//		and
