169d4ce8570fdd37e7f9e0033dd6d4cebf4a474f
Evan Huang <evanhuangwei@gmail.com>
2012-07-16 07:17:31 +0000

    Fixes #11572 !strict, do not apply edit if the row does not exist anymore, also back port to 1.7.x branch
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.7/dojox@29251 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/cells/_base.js
+++ grid/cells/_base.js
@@ -143,7 +143,9 @@ define([
 		},
 		// edit support
 		applyEdit: function(inValue, inRowIndex){
-			this.grid.edit.applyCellEdit(inValue, this, inRowIndex);
+			if(inValue){
+				this.grid.edit.applyCellEdit(inValue, this, inRowIndex);
+			}
 		},
 		cancelEdit: function(inRowIndex){
 			this.grid.doCancelEdit(inRowIndex);
