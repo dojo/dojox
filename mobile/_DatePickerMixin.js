de3e265935a5e01c0cb22e919579d6690afbe96b
Christophe Jolif <cjolif@gmail.com>
2012-07-16 09:14:29 +0000

    fixes #15677. Workarounding a !BlackBerry bug when setting dates. Thanks Soyhy Lim (IBM, CCLA). !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@29253 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/_DatePickerMixin.js
+++ mobile/_DatePickerMixin.js
@@ -33,7 +33,8 @@ define([
 	var monthSlotMixin = lang.mixin({
 		initLabels: function(){
 			this.labels = [];
-			var d = new Date(2000, 0, 1);
+			// On certain BlackBerry devices, we need to init to 16 not 1 to avoid some devices bugs (see #15677)
+			var d = new Date(2000, 0, 16);
 			for(var i = 0; i < 12; i++){
 				d.setMonth(i);
 				this.labels.push(this.format(d));
