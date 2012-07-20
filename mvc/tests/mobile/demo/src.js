61d0171434376f367efbc2e6039a829b25acf45d
Christophe Jolif <cjolif@gmail.com>
2012-07-20 14:42:23 +0000

    refs #15721. Update to the demos and to its test (only) to use !WidgetList instead of deprecated Repeater. Thanks Ed Chatelain (IBM , CCLA).
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@29356 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mvc/tests/mobile/demo/src.js
+++ mvc/tests/mobile/demo/src.js
@@ -1,205 +1,218 @@
-var listCtl, repeatModel, setRef, nextIndexToAdd, selectedIndex, nameCtl, genmodel;
-var setDetailsContext, updateView, updateModel, addEmpty, remove, forceFocus;
+define("dojox/mvc/MobileDemoContactModel", ["dojox/mvc/getStateful"], function(getStateful){
+	// module:
+	//		dojox/mvc/MobileDemoContactModel
+	// summary:
+	//		The data model of contact info for this demo.
 
-require(['dojo/has',
-	'dojox/mobile/parser',
-	//'dojo/parser',
-	'dojo/ready',
-	'dijit/registry', 
-	'dojox/mvc/at',
-	'dojox/mvc/getStateful',
-	'dojox/mvc/EditStoreRefListController',
-	"dojo/store/Memory",
-	"dojo/when",
-	'dojox/mobile',
-	'dojox/mobile/ScrollableView',
-	'dojox/mobile/Button',
-	'dojox/mobile/TextArea',
-	'dojox/mvc/Group',
-	'dojox/mvc/Output',
-	'dojox/mvc/Generate',
-	'dojox/mvc/Repeat',
-	'dojox/mobile/TextBox',
-	'dojox/mobile/deviceTheme',
-	'dojox/mobile/Heading',
-	'dojo/_base/json',
-	'dojo/dom'
-], function(has, parser, ready, registry, at, getStateful, EditStoreRefListController, 
-		Memory, when, mobile, ScrollableView, Button, TextArea, Group, Output, Generate, 
-		Repeat, TextBox, deviceTheme, Heading, json, dom){
+	return getStateful({
+		Serial: "360324",
+		First: "John",
+		Last: "Doe",
+		Email: "jdoe@us.ibm.com",
+		ShipTo: {
+			Street: "123 Valley Rd",
+			City: "Katonah",
+			State: "NY",
+			Zip: "10536"
+		},
+		BillTo: {
+			Street: "17 Skyline Dr",
+			City: "Hawthorne",
+			State: "NY",
+			Zip: "10532"
+		}
+	});
+});
 
+define("dojox/mvc/MobileDemoContactListModel", ["dojox/mvc/getStateful"], function(getStateful){
+	// module:
+	//		dojox/mvc/MobileDemoContactListModel
+	// summary:
+	//		The data model of contact list info for this demo.
+
+	return getStateful([ 
+		{
+			uniqueId: "0",
+			First: "Chad",
+			Last: "Chapman",
+			Location: "CA",
+			Office: "1278",
+			Email: "c.c@test.com",
+			Tel: "408-764-8237",
+			Fax: "408-764-8228"
+		},
+		{
+			uniqueId: "1",
+			First: "Irene",
+			Last: "Ira",
+			Location: "NJ",
+			Office: "F09",
+			Email: "i.i@test.com",
+			Tel: "514-764-6532",
+			Fax: "514-764-7300"
+		},
+		{
+			uniqueId: "2",
+			First: "John",
+			Last: "Jacklin",
+			Location: "CA",
+			Office: "6701",
+			Email: "j.j@test.com",
+			Tel: "408-764-1234",
+			Fax: "408-764-4321"
+		}
+	]);
+});
+
+define("dojox/mvc/MobileDemoContactController", [
+	"dojo/_base/declare",
+	"dojox/mvc/EditModelRefController",
+	"dojox/mvc/ListController"
+], function(declare, EditModelRefController, ListController){
+	// module:
+	//		dojox/mvc/MobileDemoContactController
+	// summary:
+	//		The controller for contact info for this demo.
+
+	return declare([EditModelRefController, ListController]);
+});
+
+define("dojox/mvc/MobileDemoContactListController", [
+	"dojo/_base/declare",
+	"dijit/registry",
+	"dojox/mvc/getStateful",
+	"dojox/mvc/ListController",
+	"dojox/mvc/StoreRefController"
+], function(declare, registry, getStateful, ListController, StoreRefController){
+	// module:
+	//		dojox/mvc/MobileDemoContactListController
+	// summary:
+	//		The controller for contact list info for this demo.
+
+	return declare([ListController, StoreRefController], {
+		// summaryScrollableViewId: String
+		//		The ID of the scrollable view representing summary (in list).
+		summaryScrollableViewId: "",
+
+		// detailScrollableViewId: String
+		//		The ID of the scrollable view representing detail (in form).
+		detailScrollableViewId: "",
+
+		// initialFocusElementId: String
+		//		The ID of the element that should get the focus when list selection switches, etc.
+		initialFocusElementId: "",
+
+		setDetailsContext: function(/*String*/ uniqueId){
+			// summary:
+			//		Called to move to the repeatdetails page when an item is selected on the WidgetList Data Binding page. 
+			// uniqueId: String
+			//		The ID of the row. 
+
+			this.set("cursorId", uniqueId);
+			registry.byId(this.initialFocusElementId).focus();
+		},
+
+		addEmpty: function(){
+			// summary:
+			//		Called to add an empty item when the white plus icon is pressed on the WidgetList Data Binding page. 
+
+			var payload = getStateful({
+				uniqueId: "" + Math.random(),
+				First: "",
+				Last: "",
+				Location: "CA",
+				Office: "",
+				Email: "",
+				Tel: "",
+				Fax: ""
+			});
+			this[this._refInModelProp].push(payload);
+			this.set("cursor", payload);
+			registry.byId(this.summaryScrollableViewId).performTransition(this.detailScrollableViewId, 1, "none");
+			registry.byId(this.initialFocusElementId).focus();
+		},
+
+		remove: function(/*String*/ uniqueId){
+			// summary:
+			//		Called to remove an item when the red circle minus icon is pressed on the WidgetList Data Binding page. 
+
+			for(var model = this[this._refInModelProp], i = 0; i < model.length; i++){
+				if(model[i][this.idProperty] == uniqueId){
+					model.splice(i, 1);
+					if(i < this[this._refInModelProp].get("length")){
+						this.set("cursorIndex", i);
+					}
+					return;
+				}
+			}
+		}
+	});
+});
+
+define("dojox/mvc/MobileDemoGenerateActions", [
+	"dojo/dom",
+	"dojo/json",
+	"dijit/registry"
+], function(dom, json, registry){
+	// module:
+	//		dojox/mvc/MobileDemoGenerateActions
+	// summary:
+	//		The action handlers for Generate example of this demo.
+
+	return {
+		switchToData: function(){
+			// summary:
+			//		Called when the "Update Model" button is pressed on the Generate View page. 
+
+			dom.byId("outerModelArea").style.display = "";
+			try {
+				dom.byId("modelArea").focus(); // hack: do this to force focus off of the textbox, bug on mobile?
+				dom.byId("viewArea").style.display = "none";
+				registry.byId("modelArea").set("value", json.stringify(registry.byId("view").get("children")));
+			} catch(e) {
+				console.log(e);
+			}
+		},
+
+		switchToGenerated: function(){
+			// summary:
+			//		Called when the "Update View" button is pressed on the Generate Simple Form. 
+
+			dom.byId("outerModelArea").style.display = "none";
+			dom.byId("viewArea").style.display = "";              		
+		}
+	};
+});
+
+require([
+	"dojo/dom",
+	"dojo/has",
+	"dojox/mobile/parser",
+	"dojox/mvc/at",
+	"dojox/mvc/Generate",
+	"dojox/mvc/Group",
+	"dojox/mvc/Output",
+	"dojox/mvc/Templated",
+	"dojox/mvc/WidgetList",
+	"dojox/mvc/_InlineTemplateMixin",
+	"dojox/mvc/MobileDemoContactModel",
+	"dojox/mvc/MobileDemoContactListModel",
+	"dojox/mvc/MobileDemoContactController",
+	"dojox/mvc/MobileDemoContactListController",
+	"dojox/mvc/MobileDemoGenerateActions",
+	"dojox/mobile",
+	"dojox/mobile/deviceTheme",
+	"dojox/mobile/Button",
+	"dojox/mobile/Heading",
+	"dojox/mobile/ScrollableView",
+	"dojox/mobile/TextArea",
+	"dojox/mobile/TextBox",
+	"dojo/domReady!"
+], function(dom, has, parser, at){
 	if(!has("webkit")){
 		require(["dojox/mobile/compat"]);
 	}
-
 	window.at = at;
-	
-	var names = [{
-	"id" 	 : "360324",
-	"Serial" : "360324",
-	"First"  : "John",
-	"Last"   : "Doe",
-	"Email"  : "jdoe@us.ibm.com",
-	"ShipTo" : {
-		"Street" : "123 Valley Rd",
-		"City"   : "Katonah",
-		"State"  : "NY",
-		"Zip"    : "10536"
-	},
-	"BillTo" : {
-		"Street" : "17 Skyline Dr",
-		"City"   : "Hawthorne",
-		"State"  : "NY",
-		"Zip"    : "10532"
-	}
-}];
-
-// Initial repeat data used in the Repeat Data binding demo
-var repeatData = [ 
-	{
-		"First"   : "Chad",
-		"Last"    : "Chapman",
-		"Location": "CA",
-		"Office"  : "1278",
-		"Email"   : "c.c@test.com",
-		"Tel"     : "408-764-8237",
-		"Fax"     : "408-764-8228"
-	},
-	{
-		"First"   : "Irene",
-		"Last"    : "Ira",
-		"Location": "NJ",
-		"Office"  : "F09",
-		"Email"   : "i.i@test.com",
-		"Tel"     : "514-764-6532",
-		"Fax"     : "514-764-7300"
-	},
-	{
-		"First"   : "John",
-		"Last"    : "Jacklin",
-		"Location": "CA",
-		"Office"  : "6701",
-		"Email"   : "j.j@test.com",
-		"Tel"     : "408-764-1234",
-		"Fax"     : "408-764-4321"
-	}
-];
-
-	selectedIndex = 0;
-
-	nameCtl = new EditStoreRefListController({store: new Memory({data: names})});
-	nameCtl.getStore("360324");
-	model = nameCtl.model;
-	
-	listCtl = new EditStoreRefListController({store: new Memory({data: repeatData}), cursorIndex: 0});
-	when(listCtl.queryStore(), function(model){
-		repeatmodel = model;
-		nextIndexToAdd = repeatmodel.length;
-	});
-	
-	
-	setDetailsContext=function(index){
-		// summary:
-		//		Called to move to the repeatdetails page when an item is selected on the Repeat Data Binding page. 
-		//
-		// index: string
-		//		The index of the item to show in the repeatdetails page. 
-		//
-		listCtl.set("cursorIndex", index);
-		registry.byId("firstInput").focus();
-	};
-
-
-	addEmpty = function(){
-		// summary:
-		//		Called to add an empty item when the white plus icon is pressed on the Repeat Data Binding page. 
-		//
-		var data = {id:Math.random(), "First": "", "Last": "", "Location": "CA", "Office": "", "Email": "",
-					"Tel": "", "Fax": ""};
-		repeatmodel.push(new getStateful(data));
-		var r = registry.byId("repeat");
-		r.performTransition("repeatdetails", 1, "none");
-		setDetailsContext(repeatmodel.length-1);
-	},
-
-	remove = function(idx){
-		// summary:
-		//		Called to remove an item when the red circle minus icon is pressed on the Repeat Data Binding page. 
-		//
-		// idx: string
-		//		The index of the item to remove. 
-		//
-		repeatmodel.splice(idx, 1);
-		if(listCtl.get("cursorIndex") > repeatmodel.length-1){
-			listCtl.set("cursorIndex", repeatmodel.length - 1);
-		}
-	},
-
-	forceFocus = function(){
-		// summary:
-		//		Called to set the focus to force the update of the field when going back to the repeat list Repeat Data Binding page. 
-		//
-		registry.byId("telInput").focus();
-	},
-	
-	updateView = function() {
-		// summary:
-		//		Called when the "Update View" button is pressed on the Generate Simple Form. 
-		//
-		try {
-			registry.byId("view").set("children", at('widget:modelArea', 'value').direction(at.from).transform({format: dojo.fromJson}));
-			dom.byId("outerModelArea").style.display = "none";
-			dom.byId("viewArea").style.display = "";              		
-		}catch(err){
-			console.error("Error parsing json from model: "+err);
-		}
-	};
-
-	// used in the Generate View demo
-	updateModel = function() {
-		// summary:
-		//		Called when the "Update Model" button is pressed on the Generate View page. 
-		//
-		dom.byId("outerModelArea").style.display = "";
-		try {
-			dom.byId("modelArea").focus(); // hack: do this to force focus off of the textbox, bug on mobile?
-			dom.byId("viewArea").style.display = "none";
-			var test = registry.byId("view");
-			registry.byId("modelArea").set("value",(json.toJson(test.get("children"), true)));
-		} catch(e) {
-			console.log(e);
-		};
-	};
-
-
-	// when "dojo/ready" is ready call parse
-	ready(function(){
-		parser.parse();
-	});
-
-	// when domReady! is ready show the page 
-	require(['dojo/domReady!'], function(){
-		dom.byId("wholepage").style.display = "";
-	});
-
-}); // end function
-
-function setRef(id, model, attr) {
-		// summary:
-		//		Called when the "Ship To" or "Bill To" button is pressed on the Simple Data binding. 
-		//
-		// id: string
-		//		The id of the widget, in our test it will be 'addrGroup' 
-		// model: string
-		//		The name of the "model", in our test it will be 'rel:' since it is a relative binding
-		// attr: string
-		//		The attr to bind, in our test it will be 'ShipTo' or 'BillTo'
-		//
-	require([
-	         "dijit/registry",
-	         "dojox/mvc/at"
-	         ], function(registry, at){
-					var widget = registry.byId(id);
-					widget.set("target", at(model,attr));
-				});
-};
+	parser.parse();
+	dom.byId("wholepage").style.display = "";
+});
