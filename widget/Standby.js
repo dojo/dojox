8b8c86d97e0e197a4103b7c2b1cc0ae18642ead8
Mark Hayes <haysmark@us.ibm.com>
2012-08-13 19:52:12 +0000

    Backport [29477]. Refs #15811.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/1.8/dojox@29478 560b804f-0ae3-0310-86f3-f6aa0a117693

--- widget/Standby.js
+++ widget/Standby.js
@@ -698,7 +698,7 @@ return declare("dojox.widget.Standby", [_Widget, _TemplatedMixin],{
 			 this._overflowDisabled = true;
 			 var body = baseWindow.body();
 			 if(body.style && body.style.overflow){
-				 this._oldOverflow = domStyle.set(body, "overflow");
+				 this._oldOverflow = domStyle.get(body, "overflow");
 			 }else{
 				 this._oldOverflow = "";
 			 }
@@ -712,7 +712,7 @@ return declare("dojox.widget.Standby", [_Widget, _TemplatedMixin],{
 					 this._oldBodyParentOverflow = body.parentNode.style.overflow;
 				 }else{
 					 try{
-						this._oldBodyParentOverflow = domStyle.set(body.parentNode, "overflow");
+						this._oldBodyParentOverflow = domStyle.get(body.parentNode, "overflow");
 					 }catch(e){
 						 this._oldBodyParentOverflow = "scroll";
 					 }
