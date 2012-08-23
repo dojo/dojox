a6ffe5dbb27df22b40fc5dc284703e4b77dd37fb
Evan Huang <evanhuangwei@gmail.com>
2012-08-23 14:34:11 +0000

    Fixes #15452, EDG.IndirectSelect is not fully compatible with columnReordering, having the column of checkbox moved to other positions may bring potential issues when running with other EDG plugins, the better options would be using EDG.plugin.DnD to get a better compliance.
    Adding a small fix so that IndirectSelct can run well with columnReordering when no other plugins are turned on
    
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@29536 560b804f-0ae3-0310-86f3-f6aa0a117693

--- grid/enhanced/plugins/IndirectSelection.js
+++ grid/enhanced/plugins/IndirectSelection.js
@@ -267,7 +267,7 @@ var SingleRowSelector = declare("dojox.grid.cells.SingleRowSelector", RowSelecto
 		//		Event fired on the target row
 		var index = e.rowIndex;
 		if(this.disabledMap[index]){ return; }
-		this._focusEndingCell(index, 0);
+		this._focusEndingCell(index, e.cellIndex);
 		this._nativeSelect(index, !this.grid.selection.selected[index]);
 	}
 });
@@ -462,7 +462,7 @@ var MultipleRowSelector = declare("dojox.grid.cells.MultipleRowSelector", RowSel
 		var rowIndex = e.rowIndex;
 		if(this.disabledMap[rowIndex]){ return; }
 		evt.stop(e);
-		this._focusEndingCell(rowIndex, 0);
+		this._focusEndingCell(rowIndex, e.cellIndex);
 		
 		var delta = rowIndex - this.lastClickRowIdx;
 		var newValue = !this.grid.selection.selected[rowIndex];
