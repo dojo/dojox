20427ec84d25a32f7c45167e06c1b94734774ecb
Dylan Schiemann <dylan@dojotoolkit.org>
2012-10-24 14:40:06 +0000

    fixes #16229, dojox/analytics should use dojo/request
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@29870 560b804f-0ae3-0310-86f3-f6aa0a117693

--- analytics/_base.js
+++ analytics/_base.js
@@ -86,7 +86,7 @@ define(["dojo/_base/lang", "dojo/_base/config", "dojo/ready", "dojo/_base/unload
 						break;
 				}
 				promise.then(function(response){
-					this.onPushComplete();
+					lang.hitch(this, this.onPushComplete);
 				});
 				return promise;
 			}
