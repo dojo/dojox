cea195175e048509326c887062d8a3a7afed35d7
Mike Wilcox <mwilcox@dojotoolkit.org>
2013-01-10 19:20:28 +0000

    Fixes #15285 - using addOnWindowUnload to avoid IE bugs
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@30330 560b804f-0ae3-0310-86f3-f6aa0a117693

--- embed/Flash.js
+++ embed/Flash.js
@@ -87,7 +87,8 @@ define(["dojo"], function(dojo) {
 		})();
 
 		//	attach some cleanup for IE, thanks to deconcept :)
-		dojo.addOnUnload(function(){
+		dojo.addOnWindowUnload(function(){
+			console.warn('***************UNLOAD');
 			var dummy = function(){};
 			var objs = dojo.query("object").
 				reverse().
