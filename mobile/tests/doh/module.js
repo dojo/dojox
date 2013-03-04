7f7b714454dcf4f3e59ef379fbe20869df704004
Christophe Jolif <cjolif@gmail.com>
2013-03-04 16:25:06 +0000

    refs #16793. First half of test refactoring. Create sub modules for testing each class. Reference those sub modules. Thanks Sebastien Brunot (IBM, CCLA). !strict.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@30749 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/tests/doh/module.js
+++ mobile/tests/doh/module.js
@@ -1,98 +1,51 @@
-define(["doh/runner", "dojo/sniff", "require"], function(doh, has, require){
+define([
+     "doh/runner",
+     "dojo/sniff",
+     "require",
+     "./accordion/module",
+     "./badge/module",
+     "./button/module",
+     "./checkbox/module",
+     "./combobox/module",
+     "./contentpane/module",
+     "./edgetoedgecategory/module",
+     "./edgetoedgedatalist/module",
+     "./edgetoedgelist/module",
+     "./edgetoedgestorelist/module",
+     "./expandingtextarea/module",
+     "./filteredlistmixin/module",
+     "./fixedsplitter/module",
+     "./heading/module",
+     "./iconcontainer/module",
+     "./iconmenu/module",
+     "./listitem/module",
+     "./longlistmixin/module",
+     "./pageindicator/module",
+     "./progressindicator/module",
+     "./radiobutton/module",
+     "./roundrect/module",
+     "./roundrectdatalist/module",
+     "./roundrectlist/module",
+     "./roundrectstorelist/module",
+     "./slider/module",
+     "./spinwheel/module",
+     "./spinwheeldatepicker/module",
+     "./swapview/module",
+     "./switch/module",
+     "./tabbar/module",
+     "./templating/module",
+     "./textarea/module",
+     "./textbox/module",
+     "./toolbarbutton/module",
+     "./transition/module",
+     "./valuepickerdatepicker/module",
+     "./valuepickertimepicker/module",
+     "./view/module"
+], function(doh, has, require){
 	try{
-		doh.registerUrl("dojox.mobile.tests.doh.Button", require.toUrl("./Button.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Button", require.toUrl("./Button_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.DatePickerIso", require.toUrl("./DatePickerIso.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeCategory", require.toUrl("./EdgeToEdgeCategory.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeDataList", require.toUrl("./EdgeToEdgeDataList.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeDataList", require.toUrl("./EdgeToEdgeDataList_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeStoreList", require.toUrl("./EdgeToEdgeStoreList.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeStoreList", require.toUrl("./EdgeToEdgeStoreList_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Heading", require.toUrl("./Heading.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Heading", require.toUrl("./Heading2.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Heading", require.toUrl("./Heading_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Heading", require.toUrl("./Heading2_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.IconContainer", require.toUrl("./IconContainer.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.IconContainer", require.toUrl("./IconContainer2.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.IconContainer", require.toUrl("./IconContainer3.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.IconContainer", require.toUrl("./IconContainer_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.IconContainer", require.toUrl("./View.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectList", require.toUrl("./RoundRect.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItem.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItem2.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItem_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItem2_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItem3_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Progress-indicator", require.toUrl("./progress-indicator.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRect", require.toUrl("./RoundRect.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRect", require.toUrl("./RoundRect_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectDataList", require.toUrl("./RoundRectDataList.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectDataList", require.toUrl("./RoundRectDataList_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectStoreList", require.toUrl("./RoundRectStoreList.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectStoreList", require.toUrl("./RoundRectStoreList_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectList", require.toUrl("./RoundRectList.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Switch", require.toUrl("./Switch.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Switch", require.toUrl("./Switch_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.TabBar", require.toUrl("./TabBar.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.TabBar", require.toUrl("./TabBar_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ToolBarButton", require.toUrl("./ToolBarButton.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ToolBarButton", require.toUrl("./ToolBarButton_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ToolBarButton", require.toUrl("./ToolBarButtonSetter.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.View", require.toUrl("./View.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.View", require.toUrl("./View_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.View", require.toUrl("./View2_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.View", require.toUrl("./View3_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.View", require.toUrl("./View-demo.html"),999999);
 		doh.registerUrl("dojox.mobile.tests.doh.URLProperty", require.toUrl("./TestURLProp.html"),999999);
 		doh.registerUrl("dojox.mobile.tests.doh.URLProperty", require.toUrl("./TestURLProp2.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.SpinWheel", require.toUrl("./SpinWheel_Programmatic.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.SpinWheelDatePicker", require.toUrl("./SpinWheelDatePicker.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ValuePickerDatePicker", require.toUrl("./ValuePickerDatePicker.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ValuePickerTimePickerTests", require.toUrl("./ValuePickerTimePickerTests.html"),999999);
-
-		doh.registerUrl("dojox.mobile.tests.doh.Button", require.toUrl("./ButtonTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.CheckBox", require.toUrl("./CheckBoxTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ComboBoxTests", require.toUrl("./ComboBoxTests.html"),999999);
-		if(!has("ie")){
-			doh.registerUrl("dojox.mobile.tests.doh.ContentPane", require.toUrl("./ContentPaneTests.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeDataList", require.toUrl("./EdgeToEdgeDataListTests.html"),999999);
-		}
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeCategory", require.toUrl("./EdgeToEdgeCategoryTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeList", require.toUrl("./EdgeToEdgeListTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.ExpandingTextArea", require.toUrl("./ExpandingTextAreaTests.html"),999999);
-		if(!has("ie")){
-			doh.registerUrl("dojox.mobile.tests.doh.FixedSplitter", require.toUrl("./FixedSplitterTests1.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.FixedSplitter", require.toUrl("./FixedSplitterTests2.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.FixedSplitter", require.toUrl("./FixedSplitterTests3.html"),999999);
-		}
-		doh.registerUrl("dojox.mobile.tests.doh.Heading", require.toUrl("./HeadingTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.IconContainer", require.toUrl("./IconContainerTests.html"),999999);
-		if(!has("ie")){
-			doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItemTests1.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.ListItem", require.toUrl("./ListItemTests2.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.PageIndicator", require.toUrl("./PageIndicatorTests1.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.PageIndicator", require.toUrl("./PageIndicatorTests2.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.PageIndicator", require.toUrl("./PageIndicatorTests3.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.RoundRectDataList", require.toUrl("./RoundRectDataListTests.html"),999999);
-		}
-		doh.registerUrl("dojox.mobile.tests.doh.RadioButton", require.toUrl("./RadioButtonTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.RoundRectList", require.toUrl("./RoundRectListTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.TextArea", require.toUrl("./TextAreaTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.TextBox", require.toUrl("./TextBoxTests.html"),999999);
-		if(!has("ie")){
-			doh.registerUrl("dojox.mobile.tests.doh.TabBar", require.toUrl("./TabBarTests.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.SwapView", require.toUrl("./SwapViewTests1.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.SwapView", require.toUrl("./SwapViewTests2.html"),999999);
-			doh.registerUrl("dojox.mobile.tests.doh.SwapView", require.toUrl("./SwapViewTests3.html"),999999);
-		}
-		doh.registerUrl("dojox.mobile.tests.doh.Slider", require.toUrl("./SliderTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.Switch", require.toUrl("./SwitchTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.FilteredListTests", require.toUrl("./FilteredListTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.TemplatedWidgetsTests", require.toUrl("./TemplatedWidgetsTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.MblCSS3TransitionTests", require.toUrl("./MblCSS3TransitionTests.html"),999999);
 		doh.registerUrl("dojox.mobile.tests.doh.CustomFixedBarsTests", require.toUrl("./CustomFixedBarsTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.HeadingFixedBarsTests", require.toUrl("./HeadingFixedBarsTests.html"),999999);
-		doh.registerUrl("dojox.mobile.tests.doh.LongListMixin", require.toUrl("./LongListMixin.html"),999999);
 	}catch(e){
 		doh.debug(e);
 	}
