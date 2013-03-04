219f3aa39b9adbf7947d48d7d4b1079328962e06
Christophe Jolif <cjolif@gmail.com>
2013-03-04 15:50:53 +0000

    refs #16010. Add a columns property to control the layout.  Thanks Damien Mandrioli (IBM, CCLA).
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@30748 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/FormLayout.js
+++ mobile/FormLayout.js
@@ -11,15 +11,16 @@ define([
 		// summary:
 		//		A responsive container to create mobile forms.
 		// description:
-		//		This container layouts form widgets according to the screen size and the value of the rightAlign property.
+		//		This container layouts form widgets according to the screen size.
 		//		Each row of a form is made of a <label> and a <fieldset> that contains one or more form widgets.
-		//		If the width of the screen if greater than 500px, the label and the fieldset are placed side by side.
-		//		Otherwise they are stacked vertically.
+		//		By default, if the width of the screen if greater than 500px, the <label> and the <fieldset> are positioned on the same line.
+		//		Otherwise they are stacked vertically. You can force how a <label> and its <fieldset> are positioned using the
+		//		'columns' property.
 		//		Form controls are: "dojox/mobile/Button", "dojox/mobile/CheckBox", "dojox/mobile/ComboBox",
 		//		"dojox/mobile/RadioButton", "dojox/mobile/Slider", "dojox/mobile/TextBox", "dojox/mobile/SearchBox",
 		//		"dojox/mobile/ExpandingTextArea", "dojox/mobile/ToggleButton".
 		// example:
-		// |	<div data-dojo-type="dojox/mobile/FormLayout">
+		// |	<div data-dojo-type="dojox/mobile/FormLayout" data-dojo-props="columns:'two', rightAlign:true">
 		// |		<div>
 		// |			<label>Name:</label>
 		// |			<fieldset>
@@ -36,8 +37,21 @@ define([
 		// |		</div>
 		// |	</div>
 
+
+		// columns: "auto" | "single" | "two"
+		//		This property controls how a <label> and its <fieldset> are positioned. The <label> can be on the same line
+		//		than its <fieldset> (two columns) or on top of it (single column).
+		//		If set to "auto", the number of columns depends on the width of the screen: Two columns
+		//		if the width of the screen is larger than 500px, one column otherwise. The width of the screen is determined using CSS
+		//		Media Queries.
+		//		Setting this property to "single" or "two" allows to force the layout used whatever the width of the screen.
+		//		Default value for this property is "auto".
+		columns: "auto",
+
 		// rightAlign: Boolean
-		//		If true and screen width larger than 500px, widgets are aligned on the right. Default is false.
+		//		This property controls the horizontal position of control(s) in a <fieldset>. It applies only
+		//		to forms that have two columns (see 'columns' property).
+		//		Default value for this property is false.
 		rightAlign: false,
 
 		/* internal properties */
@@ -48,6 +62,13 @@ define([
 
 		buildRendering: function(){
 			this.inherited(arguments);
+			if(this.columns == "auto"){
+				domClass.add(this.domNode, "mblFormLayoutAuto");
+			}else if(this.columns == "single"){
+				domClass.add(this.domNode, "mblFormLayoutSingleCol");
+			}else if(this.columns == "two"){
+				domClass.add(this.domNode, "mblFormLayoutTwoCol");
+			}
 			if(this.rightAlign){
 				domClass.add(this.domNode, "mblFormLayoutRightAlign");
 			}
