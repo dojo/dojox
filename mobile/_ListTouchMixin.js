c70a27ce46c59248b658a0257d46f6c3ed3d16e5
Eric Durocher <edurocher0@gmail.com>
2013-04-18 15:21:15 +0000

    refs #16175, fix dojox/mobile/ComboBox on IE10/Windows 7 to not use MSGesture, which throws error.
    git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@31296 560b804f-0ae3-0310-86f3-f6aa0a117693

--- mobile/_ListTouchMixin.js
+++ mobile/_ListTouchMixin.js
@@ -19,7 +19,7 @@ define([
 
 			// For some reason in IE click event is fired immideatly after user scrolled combobox control and released
 			// his/her finger. As a fix we replace click with tap event that is fired correctly.
-			if(!(has('ie') >= 10)){
+			if(!(has("ie") >= 10 && has("touch"))){
 				this._listConnect("click", "_onClick");
 			}else{
 				this._listConnect(touch.press, "_onPress");
