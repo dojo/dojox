876e5e9419611def01071da55e51493cb889ac66
Sebastien Pereira <spereira@javalive.net>
2013-05-30 11:56:41 +0200

    #15913 new enhancement. Enhance touch feedback of dojox/mobile/Button.

--- mobile/Button.js
+++ mobile/Button.js
@@ -1,15 +1,19 @@
 define([
 	"dojo/_base/array",
 	"dojo/_base/declare",
+	"dojo/_base/event",
+	"dojo/_base/window",
+	"dojo/dom",
 	"dojo/dom-class",
 	"dojo/dom-construct",
+	"dojo/touch",
 	"dijit/_WidgetBase",
 	"dijit/form/_ButtonMixin",
 	"dijit/form/_FormWidgetMixin",
 	"dojo/has",
 	"dojo/has!dojo-bidi?dojox/mobile/bidi/Button"
 	],
-	function(array, declare, domClass, domConstruct, WidgetBase, ButtonMixin, FormWidgetMixin, has, BidiButton){
+	function(array, declare, event, win, dom, domClass, domConstruct, touch, WidgetBase, ButtonMixin, FormWidgetMixin, has, BidiButton){
 
 	var Button = declare(has("dojo-bidi") ? "dojox.mobile.NonBidiButton" : "dojox.mobile.Button", [WidgetBase, FormWidgetMixin, ButtonMixin], {
 		// summary:
@@ -34,7 +38,7 @@ define([
 
 		// duration: Number
 		//		The duration of selection, in milliseconds, or -1 for no post-click CSS styling.
-		duration: 1000,
+		duration: 0,
 
 		/*=====
 		// label: String
@@ -78,7 +82,62 @@ define([
 
 		postCreate: function(){
 			this.inherited(arguments);
+
+			// we need to set the area to make sure the synthetic click is 
+			// emitted by touch.doClicks when we move before we release.
+			// Note for 2.0: compare target elements, not the area.
+			this.domNode.dojoClick = { x: 120, y: 30 };
+
+			var _this = this;
+
+			// handle touch.press event
+			this.on(touch.press, function(e){
+				event.stop(e);
+
+				if(_this.domNode.disabled){return;}
+				_this._press(true); 
+
+				// change button state when moving outside the button
+				var alwaysInside = true;
+				_this._moveh = on(win.doc, touch.move, function(e){
+					event.stop(e);
+					var inside = false;
+					for(var t = e.target; t; t = t.parentNode){
+						if(t == _this.domNode){
+							inside = true;
+							break;
+						}
+					}
+					alwaysInside = alwaysInside && inside;
+					inside = alwaysInside && inside;
+					_this._press(inside);
+				});
+
+				// handle touch.release and triggers synthetic click when 
+				// touch is released on the widget.
+				_this._endh = on(win.doc, touch.release, function(e){
+					console.log("touch.release\n");
+					event.stop(e);
+					_this._press(false);
+					_this._moveh.remove();
+					_this._endh.remove();
+				});
+			});
+
+			dom.setSelectable(this.focusNode, false);
 			this.connect(this.domNode, "onclick", "_onClick");
+		},
+
+		_press: function(pressed){
+			// tags:
+			//		private
+			if(pressed != this._pressed){
+				this._pressed = pressed;
+				var button = this.focusNode || this.domNode;
+				var newStateClasses = (this.baseClass+' '+this["class"]).split(" ");
+				newStateClasses = array.map(newStateClasses, function(c){ return c+"Selected"; });
+				(pressed?domClass.add:domClass.remove)(button, newStateClasses);
+			}
 		},
 
 		_setLabelAttr: function(/*String*/ content){
