2f1a1005db07bcdb648b47fb39db422319a380b1
Sebastien Pereira <spereira@javalive.net>
2013-06-06 11:17:24 +0200

    #15913 new enhancement. Enhance touch feedback of dojox/mobile/Button.
    Requires dojoClick useTarget feature from dojo/touch.

--- mobile/Button.js
+++ mobile/Button.js
@@ -7,13 +7,14 @@ define([
 	"dojo/dom-class",
 	"dojo/dom-construct",
 	"dojo/touch",
+	"dojo/on",
 	"dijit/_WidgetBase",
 	"dijit/form/_ButtonMixin",
 	"dijit/form/_FormWidgetMixin",
 	"dojo/has",
 	"dojo/has!dojo-bidi?dojox/mobile/bidi/Button"
 	],
-	function(array, declare, event, win, dom, domClass, domConstruct, touch, WidgetBase, ButtonMixin, FormWidgetMixin, has, BidiButton){
+	function(array, declare, event, win, dom, domClass, domConstruct, touch, on, WidgetBase, ButtonMixin, FormWidgetMixin, has, BidiButton){
 
 	var Button = declare(has("dojo-bidi") ? "dojox.mobile.NonBidiButton" : "dojox.mobile.Button", [WidgetBase, FormWidgetMixin, ButtonMixin], {
 		// summary:
@@ -49,17 +50,7 @@ define([
 		_onClick: function(e){
 			// tags:
 			//		private
-			var ret = this.inherited(arguments);
-			if(ret && this.duration >= 0){ // if its not a button with a state, then emulate press styles
-				var button = this.focusNode || this.domNode;
-				var newStateClasses = (this.baseClass+' '+this["class"]).split(" ");
-				newStateClasses = array.map(newStateClasses, function(c){ return c+"Selected"; });
-				domClass.add(button, newStateClasses);
-				this.defer(function(){
-					domClass.remove(button, newStateClasses);
-				}, this.duration);
-			}
-			return ret;
+			return this.inherited(arguments);
 		},
 
 		isFocusable: function(){ 
@@ -83,10 +74,10 @@ define([
 		postCreate: function(){
 			this.inherited(arguments);
 
-			// we need to set the area to make sure the synthetic click is 
-			// emitted by touch.doClicks when we move before we release.
-			// Note for 2.0: compare target elements, not the area.
-			this.domNode.dojoClick = { x: 120, y: 30 };
+			// we need to ensure the synthetic click is emitted by 
+			// touch.doClicks even if we moved (inside or outside) before we 
+			// released in the button area.
+			this.domNode.dojoClick = "useTarget";
 
 			var _this = this;
 
@@ -97,10 +88,18 @@ define([
 				if(_this.domNode.disabled){return;}
 				_this._press(true); 
 
-				// change button state when moving outside the button
-				var alwaysInside = true;
+				// change button state depending on where we are
+				var isFirstMoveDone = true;
 				_this._moveh = on(win.doc, touch.move, function(e){
-					event.stop(e);
+					if(this.isFirstMoveDone){
+						// #17220: preventDefault may not have any effect.
+						// causing minor impact on some android 
+						// (Galaxy Tab 2 with stock browser 4.1.1) where button
+						// display does not reflect the actual button state 
+						// when user moves back and forth from the button area.
+						e.preventDefault();
+						this.isFirstMoveDone = false;
+					}
 					var inside = false;
 					for(var t = e.target; t; t = t.parentNode){
 						if(t == _this.domNode){
@@ -108,15 +107,11 @@ define([
 							break;
 						}
 					}
-					alwaysInside = alwaysInside && inside;
-					inside = alwaysInside && inside;
 					_this._press(inside);
 				});
 
-				// handle touch.release and triggers synthetic click when 
-				// touch is released on the widget.
+				// handle touch.release 
 				_this._endh = on(win.doc, touch.release, function(e){
-					console.log("touch.release\n");
 					event.stop(e);
 					_this._press(false);
 					_this._moveh.remove();
