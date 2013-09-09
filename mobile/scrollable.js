4769e08449c74a86384565241da3517cc86b9078
Sebastien Brunot <sbrunot@gmail.com>
2013-09-09 18:35:29 +0200

    Fixes #17416. Fix detection of finger move after change of direction of scrolling. Thanks Sebastien Brunot (IBM, CCLA) (cherry picked from commit c895d8004e4d05a6fec50edd36abab33a46b43fe)

--- mobile/scrollable.js
+++ mobile/scrollable.js
@@ -122,9 +122,15 @@ define([
 		//		- 0: use default value (2 in case of Android < 3, 3 if iOS6, otherwise 1)
 		scrollType: 0,
 		
-		// for Tooltip.js
+		// _parentPadBorderExtentsBottom: [private] Number
+		//		For Tooltip.js.
 		_parentPadBorderExtentsBottom: 0,
-		
+
+		// _moved: [private] Boolean
+		//		Flag that signals if the user have moved in (one of) the scroll
+		//		direction(s) since touch start (a move under the threshold is ignored).
+		_moved: false,
+
 		init: function(/*Object?*/params){
 			// summary:
 			//		Initialize according to the given params.
@@ -465,6 +471,7 @@ define([
 			this._posX = [this.touchStartX];
 			this._posY = [this.touchStartY];
 			this._locked = false;
+			this._moved = false;
 
 			if(!this.isFormElement(e.target)){
 				this.propagatable ? e.preventDefault() : event.stop(e);
@@ -494,15 +501,16 @@ define([
 				}
 				if(this._v && this._h){ // scrollDir="hv"
 					if(dy < this.threshold &&
-					   dx < this.threshold){
+						dx < this.threshold){
 						return;
 					}
 				}else{
 					if(this._v && dy < this.threshold ||
-					   (this._h || this._f) && dx < this.threshold){
+						(this._h || this._f) && dx < this.threshold){
 						return;
 					}
 				}
+				this._moved = true;
 				this.addCover();
 				this.showScrollBar();
 			}
@@ -535,6 +543,7 @@ define([
 			var max = 10;
 			var n = this._time.length; // # of samples
 			if(n >= 2){
+				this._moved = true;
 				// Check the direction of the finger move.
 				// If the direction has been changed, discard the old data.
 				var d0, d1;
@@ -582,17 +591,6 @@ define([
 			}
 		},
 
-		_fingerMovedSinceTouchStart: function(){
-			// summary:
-			//		Return true if the "finger" has moved since the touchStart, false otherwise.
-			var n = this._time.length; // # of samples
-			if(n <= 1 || (n == 2 && Math.abs(this._posY[1] - this._posY[0]) < 4 && has('touch'))){
-				return false;
-			}else{
-				return true;
-			}
-		},
-
 		onTouchEnd: function(/*Event*/e){
 			// summary:
 			//		User-defined function to handle touchEnd events.
@@ -609,7 +607,7 @@ define([
 				this._conn = null;
 
 				var clicked = false;
-				if(!this._aborted && !this._fingerMovedSinceTouchStart()){
+				if(!this._aborted && !this._moved){
 					clicked = true;
 				}
 				if(clicked){ // clicked, not dragged or flicked
@@ -1284,7 +1282,7 @@ define([
 				// | dojo.require("dojo.fx.easing");
 				//
 				// This module itself does not make dependency on them.
-				// TODO: for 2.0 the dojo global is going away.   Use require("dojo/fx") and require("dojo/fx/easing") instead.
+				// TODO: for 2.0 the dojo global is going away. Use require("dojo/fx") and require("dojo/fx/easing") instead.
 				var s = dojo.fx.slideTo({
 					node: node,
 					duration: duration*1000,
