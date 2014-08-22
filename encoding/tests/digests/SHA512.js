e96cd063fe5d77fbf3cb01cb2d10cfb51da2b4ed
Jason Cheatham <j.cheatham@gmail.com>
2014-08-21 23:14:37 -0400

    Refactor SHA2 algorithms, add basic tests
    The algorithms now share most of their implementation code, and more
    importantly, they generate correct output.
    
    Test data is taken from RFC 4321: "Identifiers and Test Vectors for
    HMAC-SHA-224, HMAC-SHA-256, HMAC-SHA-384, and HMAC-SHA-512"
    
    Fixes #18221, #17733

--- encoding/tests/digests/SHA512.js
+++ encoding/tests/digests/SHA512.js
@@ -10,6 +10,9 @@ define(['doh', '../../digests/_base', '../../digests/SHA512'],
 
 	var base64="3a81oZNherrMQXNJriBBMRLm+k6JqX6iCp7u5ktV05ohkpkqJ0/BqDa6PCOj/uu9RU1EI2Q86A4qmslPpUyknw==";
 	var hex="ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f";
+	var hmacKey="Jefe";
+	var hmacData="what do ya want for nothing?";
+	var hmacHex="164b7a7bfcf819e2e395fbe73b56e0a387bd64222e831fd610270cd7ea2505549758bf75c05a994a6d034f65f8f0e6fdcaeab1a34d4a6b4b636e070a38bce737";
 
 	doh.register("dojox.encoding.tests.digests.SHA512", [
 		function testBase64Compute(t){
@@ -17,6 +20,9 @@ define(['doh', '../../digests/_base', '../../digests/SHA512'],
 		},
 		function testHexCompute(t){
 			t.assertEqual(hex, SHA512(message, ded.outputTypes.Hex));
+		},
+		function testHmacCompute(t){
+			t.assertEqual(hmacHex, SHA512.hmac(hmacData, hmacKey, ded.outputTypes.Hex));
 		}
 	]);
 });
