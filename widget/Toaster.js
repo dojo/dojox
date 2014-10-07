7616ec88ec8a3c7e7355870ac152203c8fda0e55
Nicolas R <rudasn@gmail.com>
2014-10-07 13:41:33 +0300

    #18302 - dojox/widget/toaster - Do not override existing element ID on IE

--- widget/Toaster.js
+++ widget/Toaster.js
@@ -251,7 +251,7 @@ define([
 			style.clip = "rect(0px, " + nodeSize.w + "px, " + nodeSize.h + "px, 0px)";
 			if(has("ie")){
 				if(!this.bgIframe){
-					this.clipNode.id = registry.getUniqueId("dojox_widget_Toaster_clipNode");
+					this.clipNode.id || (this.clipNode.id = registry.getUniqueId("dojox_widget_Toaster_clipNode"));
 					this.bgIframe = new BackgroundIframe(this.clipNode);
 				}
 				var iframe = this.bgIframe.iframe;
