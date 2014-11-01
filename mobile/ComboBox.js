930cfcdacf4c6e3e983fea8541af7e262ddc735f
Nick Nisi <nick@nisi.org>
2014-11-01 05:33:12 -0700

    fixes #18317, display jumping on iOS8

--- mobile/ComboBox.js
+++ mobile/ComboBox.js
@@ -151,7 +151,7 @@ define([
 				domAttr.set(this.domNode, "aria-owns", dropDown.id);
 			}
 
-			if(has("touch")){
+			if(has("touch") && (!has("ios") || has("ios") < 8)){
 				win.global.scrollBy(0, domGeometry.position(aroundNode, false).y); // don't call scrollIntoView since it messes up ScrollableView
 			}
 
