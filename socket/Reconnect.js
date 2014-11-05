b2bd953b45348e4afdd61114f7f5c9304e76c3b5
Jared Jurkiewicz <jared.jurkiewicz@gmail.com>
2014-11-05 10:39:58 -0600

    socket: fix issue with baseURI and port Reconnect to AMD
    fixes #17661
    
    (cherry picked from commit fe64cc6e45f5f15924e2d60bdec0bdfc600d6397)
    
    Conflicts:
    	socket.js
    	socket/Reconnect.js

--- socket/Reconnect.js
+++ socket/Reconnect.js
@@ -1,54 +1,59 @@
-dojo.provide("dojox.socket.Reconnect");
+define([
+	"dojox/socket",
+	"dojo/aspect"
+], function(dxSocket, aspect) {
 
-dojox.socket.Reconnect = function(socket, options){
-	// summary:
-	//		Provides auto-reconnection to a websocket after it has been closed
-	//	socket:
-	//		Socket to add reconnection support to.
-	// returns:
-	// 		An object that implements the WebSocket API
-	// example:
-	//		You can use the Reconnect module:
-	//		| dojo.require("dojox.socket");
-	//		| dojo.require("dojox.socket.Reconnect");
-	//		| var socket = dojox.socket({url:"/comet"});
-	//		| // add auto-reconnect support
-	//		| socket = dojox.socket.Reconnect(socket);
-	options = options || {};
-	var reconnectTime = options.reconnectTime || 10000;
-	
-	var connectHandle = dojo.connect(socket, "onclose", function(event){
-		clearTimeout(checkForOpen);
-		if(!event.wasClean){
-			socket.disconnected(function(){
-				dojox.socket.replace(socket, newSocket = socket.reconnect());
-			});
+	dxSocket.Reconnect = function(socket, options){
+		// summary:
+		//		Provides auto-reconnection to a websocket after it has been closed
+		// socket:
+		//		Socket to add reconnection support to.
+		// returns:
+		//		An object that implements the WebSocket API
+		// example:
+		//		You can use the Reconnect module:
+		//		| require["dojox/socket", "dojox/socket/Reconnect"], function(dxSocket, reconnect){
+		//		|    var socket = dxSocket({url:"/comet"});
+		//		|    // add auto-reconnect support
+		//		|    socket = reconnect(socket);
+		var reconnectTime = options.reconnectTime || 10000;
+		var checkForOpen, newSocket;
+		options = options || {};
+
+		aspect.after(socket, "onclose", function(event){
+			clearTimeout(checkForOpen);
+			if(!event.wasClean){
+				socket.disconnected(function(){
+					dxSocket.replace(socket, newSocket = socket.reconnect());
+				});
+			}
+		}, true);
+		if(!socket.disconnected){
+			// add a default impl if it doesn't exist
+			socket.disconnected = function(reconnect){
+				setTimeout(function(){
+					reconnect();
+					checkForOpen = setTimeout(function(){
+						//reset the backoff
+						if(newSocket.readyState < 2){
+							reconnectTime = options.reconnectTime || 10000;
+						}
+					}, 10000);
+				}, reconnectTime);
+				// backoff each time
+				reconnectTime *= options.backoffRate || 2;
+			};
 		}
-	});
-	var checkForOpen, newSocket;
-	if(!socket.disconnected){
-		// add a default impl if it doesn't exist
-		socket.disconnected = function(reconnect){
-			setTimeout(function(){
-				reconnect();
-				checkForOpen = setTimeout(function(){
-					//reset the backoff
-					if(newSocket.readyState < 2){
-						reconnectTime = options.reconnectTime || 10000;
-					}
-				}, 10000);
-			}, reconnectTime);
-			// backoff each time
-			reconnectTime *= options.backoffRate || 2;
-		};
-	}
-	if(!socket.reconnect){
-		// add a default impl if it doesn't exist
-		socket.reconnect = function(){
-			return socket.args ?
-				dojox.socket.LongPoll(socket.args) :
-				dojox.socket.WebSocket({url: socket.URL || socket.url}); // different cases for different impls
-		};
-	}
-	return socket;
-};
+		if(!socket.reconnect){
+			// add a default impl if it doesn't exist
+			socket.reconnect = function(){
+				return socket.args ?
+					dxSocket.LongPoll(socket.args) :
+					dxSocket.WebSocket({url: socket.URL || socket.url}); // different cases for different impls
+			};
+		}
+		return socket;
+	};
+
+	return dxSocket.Reconnect;
+});
