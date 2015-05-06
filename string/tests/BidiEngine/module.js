096bce69082600baacef672b0589a8899e7a486a
Semion Chichelnitsky <semion@il.ibm.com>
2015-05-06 12:54:19 +0300

    Support maps in BidiEngine

--- string/tests/BidiEngine/module.js
+++ string/tests/BidiEngine/module.js
@@ -2,9 +2,8 @@ dojo.provide("dojox.string.tests.BidiEngine.module");
 
 try{
    	dojo.require("dojox.string.tests.BidiEngine.BidiEngineTest");
-   	
    	dojo.require("dojox.string.tests.BidiEngine.BidiEngineTestLayouts");
-
-}catch(e){
+   	dojo.require("dojox.string.tests.BidiEngine.BidiEngineMapsTest");
+}catch(e) {
      doh.debug(e);
 }
