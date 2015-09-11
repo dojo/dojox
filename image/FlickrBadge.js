123c6766a5e3d43e01100e2728a4520eaebd2695
Dylan Schiemann <dylan@dojotoolkit.org>
2015-09-11 15:56:36 -0700

    fixes #18305, update Flickr API calls to require SSL
    (cherry picked from commit daff4f675185f95ad635ac4221753d42646f27b0)

--- image/FlickrBadge.js
+++ image/FlickrBadge.js
@@ -35,7 +35,7 @@ define(["dojo", "dojox/main", "dojox/image/Badge", "dojox/data/FlickrRestStore"]
 		postCreate: function(){
 			if(this.username && !this.userid){
 				var def = dojo.io.script.get({
-					url: "http://www.flickr.com/services/rest/",
+					url: "https://www.flickr.com/services/rest/",
 					preventCache: true,
 					content: {
 						format: "json",
