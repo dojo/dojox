dojo/dojox

commit dcb1f0ca39e56c6be4c0986863f828ed44fe1b8a

convert dojox.drawing to AMD
!strict


git-svn-id: http://svn.dojotoolkit.org/src/dojox/trunk@28466 560b804f-0ae3-0310-86f3-f6aa0a117693

drawing/tools/custom/Vector.js
@@ -1,8 +1,7 @@
-dojo.provide("dojox.drawing.tools.custom.Vector");
-dojo.require("dojox.drawing.tools.Arrow");
-dojo.require("dojox.drawing.util.positioning");
+define(["dojo", "../../util/oo", "../../manager/_registry", "../../util/positioning", "../Arrow"],
+function(dojo, oo, registry, positioning, Arrow){
 
-dojox.drawing.tools.custom.Vector = dojox.drawing.util.oo.declare(
+var Vector = oo.declare(
 	// summary:
 	//		Creates a Vector Stencil.
 	// description:
@@ -11,7 +10,7 @@ dojox.drawing.tools.custom.Vector = dojox.drawing.util.oo.declare(
 	//		to allow for a 'zero vector' - one with no length.
 	//
 	//
-	dojox.drawing.tools.Arrow,
+	Arrow,
 	function(options){
 		this.minimumSize = this.style.arrows.length;
 		this.addShadow({size:3, mult:2});
@@ -163,7 +162,7 @@ dojox.drawing.tools.custom.Vector = dojox.drawing.util.oo.declare(
 				};
 			}
 			var radius = this.util.length(obj);
-			var angle = this.util.angle(obj);
+			var angle = positioning.angle(obj);
 			angle<0 ? angle = 360 + angle : angle;
 			
 			angle = angle > 135 && angle < 315 ? this.style.zAngle : this.util.oppAngle(this.style.zAngle);
@@ -179,7 +178,7 @@ dojox.drawing.tools.custom.Vector = dojox.drawing.util.oo.declare(
 			var obj = {start:{x:p[0].x, y:p[0].y}, x:p[1].x, y:p[1].y};
 			if(this.style.zAxis && (this.util.length(obj)>this.minimumSize)){
 				
-				var angle = this.util.angle(obj);
+				var angle = positioning.angle(obj);
 				angle<0 ? angle = 360 + angle : angle;
 				cosphi = angle > 135 && angle < 315 ? 1 : -1;
 			}
@@ -281,7 +280,8 @@ dojox.drawing.tools.custom.Vector = dojox.drawing.util.oo.declare(
 	
 );
 
-dojox.drawing.tools.custom.Vector.setup = {
+dojo.setObject("dojox.drawing.tools.custom.Vector", Vector);
+Vector.setup = {
 	// summary: See stencil._Base ToolsSetup
 	//
 	name:"dojox.drawing.tools.custom.Vector",
@@ -289,7 +289,8 @@ dojox.drawing.tools.custom.Vector.setup = {
 	iconClass:"iconVector"
 };
 
-if(dojox.drawing.defaults.zAxisEnabled){
+//TODO
+if(0 && dojox.drawing.defaults.zAxisEnabled){
 	dojox.drawing.tools.custom.Vector.setup.secondary = {
 		// summary:
 		//		Creates a secondary tool for the Vector Stencil.
@@ -385,4 +386,6 @@ if(dojox.drawing.defaults.zAxisEnabled){
 		}
 	};
 }
-dojox.drawing.register(dojox.drawing.tools.custom.Vector.setup, "tool");
+registry.register(Vector.setup, "tool");
+return Vector;
+});

dojox.profile.js
@@ -14,7 +14,6 @@ var profile = (function(){
 			"secure",
 			"cometd",
 			"data/(demos|ItemExplorer|StoreExplorer|restListener)",
-			"drawing",
 			"editor/plugins/(ResizeTableColumn|SpellCheck)",
 			"embed/(IE)",
 			"flash",

drawing.js
@@ -1,2 +1,3 @@
-dojo.provide("dojox.drawing");
-dojo.require("dojox.drawing._base");
+//dojo.provide("dojox.drawing");dojo.require("dojox.drawing._base");
+define(['./drawing/_base'],function(){
+});

drawing/Drawing.js
@@ -1,10 +1,8 @@
-dojo.provide("dojox.drawing.Drawing");
-
-(function(){
-
-	var _plugsInitialized = false;
-
-	dojo.declare("dojox.drawing.Drawing", [], {
+define(["dojo", "./defaults", "./manager/_registry", "./manager/keys", "./manager/Mouse",
+"./manager/Canvas","./manager/Undo","./manager/Anchors","./manager/Stencil","./manager/StencilUI",
+  "./util/common"], 
+function(dojo, defaults, registry, keys, Mouse, Canvas, Undo, Anchors, Stencil, StencilUI, utilCommon){
+	return dojo.declare("dojox.drawing.Drawing", [], {
 		// summary:
 		//		Drawing is a project that sits on top of DojoX GFX and uses SVG and
 		//		VML vector graphics to draw and display.
@@ -109,21 +107,16 @@ dojo.provide("dojox.drawing.Drawing");
 			//			Will change when Drawing can be created programmatically.
 			//
 			var def = dojo.attr(node, "defaults");
-			if(def){
-				dojox.drawing.defaults =  dojo.getObject(def);
-			}
-			this.defaults =  dojox.drawing.defaults;
+			this.defaults =  def ? (typeof def === 'string' ? dojo.getObject(def) : def) : defaults;
 
 			this.id = node.id;
-			dojox.drawing.register(this, "drawing");
+			registry.register(this, "drawing");
 			this.mode = (props.mode || dojo.attr(node, "mode") || "").toLowerCase();
 			var box = dojo.contentBox(node);
 			this.width = box.w;
 			this.height = box.h;
-			this.util = dojox.drawing.util.common;
-			this.util.register(this); // So Toolbar can find this Drawing DEPRECATED
-			this.keys = dojox.drawing.manager.keys;
-			this.mouse = new dojox.drawing.manager.Mouse({util:this.util, keys:this.keys, id:this.mode=="ui"?"MUI":"mse"});
+			utilCommon.register(this); // So Toolbar can find this Drawing DEPRECATED
+			this.mouse = new Mouse({util:utilCommon, keys:keys, id:this.mode=="ui"?"MUI":"mse"});
 			this.mouse.setEventMode(this.mode);
 
 			this.tools = {};
@@ -159,15 +152,15 @@ dojo.provide("dojox.drawing.Drawing");
 				dijit.registry.add(this);
 			}
 
-			var stencils = dojox.drawing.getRegistered("stencil");
+			var stencils = registry.getRegistered("stencil");
 			for(var nm in stencils){
 				this.registerTool(stencils[nm].name);
 			}
-			var tools = dojox.drawing.getRegistered("tool");
+			var tools = registry.getRegistered("tool");
 			for(nm in tools){
 				this.registerTool(tools[nm].name);
 			}
-			var plugs = dojox.drawing.getRegistered("plugin");
+			var plugs = registry.getRegistered("plugin");
 			for(nm in plugs){
 				this.registerTool(plugs[nm].name);
 			}
@@ -177,9 +170,9 @@ dojo.provide("dojox.drawing.Drawing");
 
 		_createCanvas: function(){
 			console.info("drawing create canvas...");
-			this.canvas = new dojox.drawing.manager.Canvas({
+			this.canvas = new Canvas({
 				srcRefNode:this.domNode,
-				util:this.util,
+				util:utilCommon,
 				mouse:this.mouse,
 				callback: dojo.hitch(this, "onSurfaceReady")
 			});
@@ -216,8 +209,8 @@ dojo.provide("dojox.drawing.Drawing");
 			var ui = this.mode=="ui" || mode=="ui";
 			return dojo.mixin({
 				container: ui && !surface ? this.canvas.overlay.createGroup() : this.canvas.surface.createGroup(),
-				util:this.util,
-				keys:this.keys,
+				util:utilCommon,
+				keys:keys,
 				mouse:this.mouse,
 				drawing:this,
 				drawingType: ui && !surface ? "ui" : "stencil",
@@ -249,8 +242,8 @@ dojo.provide("dojox.drawing.Drawing");
 			}
 			dojo.forEach(this.plugins, function(p, i){
 				var props = dojo.mixin({
-					util:this.util,
-					keys:this.keys,
+					util:utilCommon,
+					keys:keys,
 					mouse:this.mouse,
 					drawing:this,
 					stencils:this.stencils,
@@ -266,7 +259,6 @@ dojo.provide("dojox.drawing.Drawing");
 				}
 			}, this);
 			this.plugins = [];
-			_plugsInitialized = true;
 			// In IE, because the timing is different we have to get the
 			// canvas position after everything has drawn. *sigh*
 			this.mouse.setCanvas();
@@ -280,17 +272,17 @@ dojo.provide("dojox.drawing.Drawing");
 			this.ready = true;
 			//console.info("Surface ready")
 			this.mouse.init(this.canvas.domNode);
-			this.undo = new dojox.drawing.manager.Undo({keys:this.keys});
-			this.anchors = new dojox.drawing.manager.Anchors({drawing:this, mouse:this.mouse, undo:this.undo, util:this.util});
+			this.undo = new Undo({keys:keys});
+			this.anchors = new Anchors({drawing:this, mouse:this.mouse, undo:this.undo, util:utilCommon});
 			if(this.mode == "ui"){
-				this.uiStencils = new dojox.drawing.manager.StencilUI({canvas:this.canvas, surface:this.canvas.surface, mouse:this.mouse, keys:this.keys});
+				this.uiStencils = new StencilUI({canvas:this.canvas, surface:this.canvas.surface, mouse:this.mouse, keys:keys});
 			}else{
-				this.stencils = new dojox.drawing.manager.Stencil({canvas:this.canvas, surface:this.canvas.surface, mouse:this.mouse, undo:this.undo, keys:this.keys, anchors:this.anchors});
-				this.uiStencils = new dojox.drawing.manager.StencilUI({canvas:this.canvas, surface:this.canvas.surface, mouse:this.mouse, keys:this.keys});
+				this.stencils = new Stencil({canvas:this.canvas, surface:this.canvas.surface, mouse:this.mouse, undo:this.undo, keys:keys, anchors:this.anchors});
+				this.uiStencils = new StencilUI({canvas:this.canvas, surface:this.canvas.surface, mouse:this.mouse, keys:keys});
 			}
 			if(dojox.gfx.renderer=="silverlight"){
 				try{
-				new dojox.drawing.plugins.drawing.Silverlight({util:this.util, mouse:this.mouse, stencils:this.stencils, anchors:this.anchors, canvas:this.canvas});
+				new dojox.drawing.plugins.drawing.Silverlight({util:utilCommon, mouse:this.mouse, stencils:this.stencils, anchors:this.anchors, canvas:this.canvas});
 				}catch(e){
 					throw new Error("Attempted to install the Silverlight plugin, but it was not found.");
 				}
@@ -496,7 +488,7 @@ dojo.provide("dojox.drawing.Drawing");
 			var constr = dojo.getObject(type);
 			//console.log("constr:", type)
 			this.tools[type] = constr;
-			var abbr = this.util.abbr(type);
+			var abbr = utilCommon.abbr(type);
 			this.stencilTypes[abbr] = constr;
 			this.stencilTypeMap[abbr] = type;
 		},
@@ -530,7 +522,7 @@ dojo.provide("dojox.drawing.Drawing");
 			//console.log("new tool arg:", type, "curr:", this.currentType, "mode:", this.mode, "tools:", this.tools)
 
 			try{
-				this.currentStencil = new this.tools[this.currentType]({container:this.canvas.surface.createGroup(), util:this.util, mouse:this.mouse, keys:this.keys});
+				this.currentStencil = new this.tools[this.currentType]({container:this.canvas.surface.createGroup(), util:utilCommon, mouse:this.mouse, keys:keys});
 				console.log("new tool is:", this.currentStencil.id, this.currentStencil);
 				if(this.defaults.clickMode){ this.defaults.clickable = false; }
 				this.currentStencil.connect(this.currentStencil, "onRender", this, "onRenderStencil");
@@ -563,4 +555,4 @@ dojo.provide("dojox.drawing.Drawing");
 		}
 	});
 
-})();
+});

drawing/_base.js
@@ -1,36 +1,6 @@
-dojo.provide("dojox.drawing._base");
+define(["dojo", "./annotations/Label", "./Drawing"
+  ],function(dojo, Drawing){
 dojo.experimental("dojox.drawing");
+return Drawing;
+});
 
-dojo.require("dojox.drawing.manager._registry");
-dojo.require("dojox.gfx");
-dojo.require("dojox.drawing.Drawing");
-dojo.require("dojox.drawing.util.oo");
-dojo.require("dojox.drawing.util.common");
-dojo.require("dojox.drawing.util.typeset");
-dojo.require("dojox.drawing.defaults");
-dojo.require("dojox.drawing.manager.Canvas");
-
-// interactive managers
-dojo.require("dojox.drawing.manager.Undo");
-dojo.require("dojox.drawing.manager.keys");
-dojo.require("dojox.drawing.manager.Mouse");
-dojo.require("dojox.drawing.manager.Stencil");
-dojo.require("dojox.drawing.manager.StencilUI"); // plugin? or as a require? good here? in toolbar?
-dojo.require("dojox.drawing.manager.Anchors");
-
-// standard stencils
-dojo.require("dojox.drawing.stencil._Base");
-dojo.require("dojox.drawing.stencil.Line");
-dojo.require("dojox.drawing.stencil.Rect");
-dojo.require("dojox.drawing.stencil.Ellipse");
-dojo.require("dojox.drawing.stencil.Path");
-dojo.require("dojox.drawing.stencil.Text");
-dojo.require("dojox.drawing.stencil.Image");
-
-// annotations are built within stencil/_Base.js
-// would like to optionally include them, but for
-// now it's mandatory.
-dojo.require("dojox.drawing.annotations.Label");
-dojo.require("dojox.drawing.annotations.Angle");
-dojo.require("dojox.drawing.annotations.Arrow");
-dojo.require("dojox.drawing.annotations.BoxShadow");

drawing/annotations/Angle.js
@@ -1,6 +1,8 @@
-dojo.provide("dojox.drawing.annotations.Angle");
+define(["dojo", "../util/oo", "../util/positioning"], 
+function(dojo, oo, positioning){
 
-dojox.drawing.annotations.Angle = dojox.drawing.util.oo.declare(
+//dojox.drawing.annotations.Angle = 
+return oo.declare(
 	// summary:
 	//	When initiated, an HTML box will hover near the Stencil,
 	//	displaying it's angle while drawn or modified. Currently
@@ -43,7 +45,7 @@ dojox.drawing.annotations.Angle = dojox.drawing.util.oo.declare(
 			}
 			var node = this.getAngleNode();
 			var d = this.stencil.pointsToData();
-			var pt = dojox.drawing.util.positioning.angle({x:d.x1,y:d.y1},{x:d.x2,y:d.y2});
+			var pt = positioning.angle({x:d.x1,y:d.y1},{x:d.x2,y:d.y2});
 			var sc = this.mouse.scrollOffset();
 			var mx = this.stencil.getTransform();
 			var dx = mx.dx / this.mouse.zoom;
@@ -102,3 +104,4 @@ dojox.drawing.annotations.Angle = dojox.drawing.util.oo.declare(
 	}
 
 );
+});

drawing/annotations/Arrow.js
@@ -1,14 +1,15 @@
-dojo.provide("dojox.drawing.annotations.Arrow");
-dojo.require("dojox.drawing.stencil.Path");
+define(["../util/oo", "../stencil/Path"], 
+function(oo, Path){
 
-dojox.drawing.annotations.Arrow = dojox.drawing.util.oo.declare(
+//dojox.drawing.annotations.Arrow = 
+return oo.declare(
 	// summary:
 	//	An annotation called internally to put an arrowhead
 	//	on ether end of a Line. Initiated in Arrow (and Vector)
 	//	with the optional params: arrowStart and arrowEnd. Both
 	//	default true for Axes.
 	//
-	dojox.drawing.stencil.Path,
+	Path,
 	function(/* dojox.__stencilArgs */options){
 		// arguments: See stencil._Base
 		this.stencil.connectMult([
@@ -69,3 +70,4 @@ dojox.drawing.annotations.Arrow = dojox.drawing.util.oo.declare(
 		
 	}
 );
+});

drawing/annotations/BoxShadow.js
@@ -1,6 +1,7 @@
-dojo.provide("dojox.drawing.annotations.BoxShadow");
+define(["dojo", "dojo/_base/Color", "../util/oo"], 
+function(dojo, Color, oo){
 
-dojox.drawing.annotations.BoxShadow = dojox.drawing.util.oo.declare(
+return oo.declare(
 	// summary:
 	//		Creates a box shadow under solid objects. Can change the
 	//		shadow direction, color, size, and intensity. Can center
@@ -56,7 +57,7 @@ dojox.drawing.annotations.BoxShadow = dojox.drawing.util.oo.declare(
 		
 		delete options.stencil;
 		this.options = dojo.mixin(shadowDefaults, options);
-		this.options.color = new dojo.Color(this.options.color)
+		this.options.color = new Color(this.options.color)
 		this.options.color.a = this.options.alpha;
 		switch(this.stencil.shortType){
 			case "image":
@@ -305,3 +306,5 @@ dojox.drawing.annotations.BoxShadow = dojox.drawing.util.oo.declare(
 		}
 	}
 );
+
+});

commit 188e0c347b81c9a372b68b761ff3a74ce8c27652

Add AMD conversion for axial (z-axis) vectors.  Fix incorrect call to angle function introduced in commit    dcb1f0ca39e56c6be4c0986863f828ed44fe1b8a This fixes bug https://bugs.dojotoolkit.org/ticket/17652

drawing/tools/custom/Vector.js
@@ -1,5 +1,5 @@
-define(["dojo", "../../util/oo", "../../manager/_registry", "../../util/positioning", "../Arrow"],
-function(dojo, oo, registry, positioning, Arrow){
+define(["dojo", "../../util/oo", "../../manager/_registry", "../Arrow", "../../defaults"],
+function(dojo, oo, registry, Arrow, defaults){
 
 var Vector = oo.declare(
 	Arrow,
@@ -142,7 +142,7 @@ var Vector = oo.declare(
 			var test = x<0 ? x>-y : x<-y;
 			var dx = test ? x : -y/Math.tan(radians);
 			var dy = !test ? y : -Math.tan(radians)*x;
-			return {x:dx, y:dy}
+			return {x:dx, y:dy};
 		},
 		
 		zPoint: function(obj){
@@ -162,8 +162,8 @@ var Vector = oo.declare(
 				};
 			}
 			var radius = this.util.length(obj);
-			var angle = positioning.angle(obj.start, obj);
-			angle<0 ? angle = 360 + angle : angle;
+			var angle = this.util.angle(obj);
+			angle = angle<0 ? 360 + angle : angle;
 			
 			angle = angle > 135 && angle < 315 ? this.style.zAngle : this.util.oppAngle(this.style.zAngle);
 			
@@ -178,8 +178,8 @@ var Vector = oo.declare(
 			var obj = {start:{x:p[0].x, y:p[0].y}, x:p[1].x, y:p[1].y};
 			if(this.style.zAxis && (this.util.length(obj)>this.minimumSize)){
 				
-				var angle = positioning.angle(obj.start, obj);
-				angle<0 ? angle = 360 + angle : angle;
+				var angle = this.util.angle(obj);
+				angle = angle<0 ? 360 + angle : angle;
 				cosphi = angle > 135 && angle < 315 ? 1 : -1;
 			}
 			this.data = {
@@ -212,7 +212,7 @@ var Vector = oo.declare(
 					x2:pt.x,
 					y2:pt.y,
 					cosphi:cosphi
-				}
+				};
 				
 			}
 			this.points = [
@@ -291,9 +291,8 @@ Vector.setup = {
 	iconClass:"iconVector"
 };
 
-//TODO
-if(0 && dojox.drawing.defaults.zAxisEnabled){
-	dojox.drawing.tools.custom.Vector.setup.secondary = {
+if(defaults.zAxisEnabled){
+	Vector.setup.secondary = {
 		// summary:
 		//		Creates a secondary tool for the Vector Stencil.
 		// description:
@@ -313,7 +312,7 @@ if(0 && dojox.drawing.defaults.zAxisEnabled){
 			
 			var stencils = this.drawing.stencils.selectedStencils;
 			for(var nm in stencils){
-				if(stencils[nm].shortType == "vector" && (stencils[nm].style.zAxis != dojox.drawing.defaults.zAxis)){
+				if(stencils[nm].shortType == "vector" && (stencils[nm].style.zAxis != defaults.zAxis)){
 					var s = stencils[nm];
 					s.changeAxis();
 					//Reset anchors
@@ -328,11 +327,11 @@ if(0 && dojox.drawing.defaults.zAxisEnabled){
 			//		are treated as if they were added directly to toolbar.
 			//		They are included with the tool because secondary buttons
 			//		are tool specific.
-			var zAxis = dojox.drawing.defaults.zAxis;
+			var zAxis = defaults.zAxis;
 			this.zSelect = function(button){
 				if(!button.enabled){ return; }
 				zAxis = true;
-				dojox.drawing.defaults.zAxis = true;
+				defaults.zAxis = true;
 				button.select();
 				this.vectorTest();
 				this.zSelected = button;
@@ -340,7 +339,7 @@ if(0 && dojox.drawing.defaults.zAxisEnabled){
 			this.zDeselect = function(button){
 				if(!button.enabled){ return; }
 				zAxis = false;
-				dojox.drawing.defaults.zAxis = false;
+				defaults.zAxis = false;
 				button.deselect();
 				this.vectorTest();
 				this.zSelected = null;
@@ -352,7 +351,11 @@ if(0 && dojox.drawing.defaults.zAxisEnabled){
 					}
 				},this);
 			};
-			dojo.connect(this, "onRenderStencil", this, function(){ if(this.zSelected){ this.zDeselect(this.zSelected)}});
+			dojo.connect(this, "onRenderStencil", this, function(){ 
+			    if(this.zSelected){ 
+				this.zDeselect(this.zSelected);
+			    }
+			});
 			var c = dojo.connect(this.drawing, "onSurfaceReady", this, function(){
 				dojo.disconnect(c);
 				dojo.connect(this.drawing.stencils, "onSelect", this, function(stencil){
@@ -383,8 +386,8 @@ if(0 && dojox.drawing.defaults.zAxisEnabled){
 			//		extra functionality for some of the basic functions.
 			//		Post is passed the button so those connections can
 			//		be made.
-			dojo.connect(btn, "enable", function(){ dojox.drawing.defaults.zAxisEnabled = true; });
-			dojo.connect(btn, "disable", function(){ dojox.drawing.defaults.zAxisEnabled = false; });
+			dojo.connect(btn, "enable", function(){ defaults.zAxisEnabled = true; });
+			dojo.connect(btn, "disable", function(){ defaults.zAxisEnabled = false; });
 		}
 	};
 }