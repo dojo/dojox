b4541221fc7e69ead7f6da5cbef6461eb16d0db3
Jason Cheatham <j.cheatham@gmail.com>
2017-11-30 16:05:47 -0700

    fix several issues with the Rotator widget
    (cherry picked from commit 8b7a9756c673a2900ac83fcd8d186c8b8dcb9747)

--- widget/Rotator.js
+++ widget/Rotator.js
@@ -185,6 +185,9 @@ define([
 					}
 					_t._domNode.removeChild(removed.node);
 				}
+				if (_t.idx > idx) {
+					_t.idx--;
+				}
 			}
 
 			var index,
@@ -196,7 +199,7 @@ define([
 			}
 			else {
 				for (var i = 0; i < panes.length; i++) {
-					if (panes.node === nodeOrIndex) {
+					if (panes[i].node === nodeOrIndex) {
 						index = i;
 						break;
 					}
@@ -220,9 +223,6 @@ define([
 			}
 			else {
 				removeFromPanes(index);
-				if (this.idx > index) {
-					this.idx--;
-				}
 			}
 		},
 
